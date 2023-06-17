d1888ae82fbacac8fe126376d3d9c72c300762ee
sam caldwell <mail@samcaldwell.net>
2023-06-17 07:52:17 -0500

    add IsValidYaml() to projects/fs/file

---
+++ projects/fs/file/IsValidYaml.go
@@ -0,0 +1,22 @@
+package file
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"os"
+)
+
+// IsValidYaml - Return boolean if YAML can be opened and unmarshalled without error
+func IsValidYaml(filename string) error {
+	yamlFile, err := os.ReadFile(filename)
+	if err != nil {
+		return fmt.Errorf("error reading YAML file '%s': %v", filename, err)
+	}
+
+	var data any
+	err = yaml.Unmarshal(yamlFile, &data)
+	if err != nil {
+		return fmt.Errorf("error unmarshaling YAML file '%s': %v", filename, err)
+	}
+	return nil
+}
