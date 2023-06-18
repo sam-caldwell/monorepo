0f50872c48b18bb8f36d565707410c528e5f86ea
sam caldwell <mail@samcaldwell.net>
2023-06-18 17:09:44 -0500

    add placeholder for windows

--- Makefile.d/list.build.projects.mk
+++ Makefile.d/list.build.projects.mk
@@ -3,11 +3,17 @@
 # For build autoamation docs, see docs/builds/README.md
 #
 list/build/projects:
+ifeq ($(OPSYS),windows)
+	@echo off
+	@echo "Windows fucking sucks.  If you are reading this, you've bought into the line that it's better.  It's not."
+	@echo "I might fix this when I have time.  But...well...that would be enabling your dumb ass."
+else
 	@(\
 		echo ""; \
 		echo "current binary projects (enabled):"; \
-		for PROJECT in $(PROJECTS); do \
+		for PROJECT in $(BUILD_PROJECTS); do \
 		  echo " - $${PROJECT}"; \
 	    done; \
 		echo ""; \
 	)
+endif
