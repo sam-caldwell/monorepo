0fa33586e2628bfc66e2452bcefa3e37998532c4
sam caldwell <mail@samcaldwell.net>
2023-06-18 16:39:46 -0500

    make things a bit more friendly for windows

--- Makefile.d/self-tests/setup.tests.fakeProject.mk
+++ Makefile.d/self-tests/setup.tests.fakeProject.mk
@@ -3,7 +3,7 @@
 # (c) 2023 Sam Caldwell.  See License.txt
 #
 test/Makefile/setup.fakeProject:
-	@echo "\033[32m>starting $@\033[0m"
-	@mkdir -p $(FAKE_COMMAND_DIR) || echo -e "\033[31m>FAILED:$@\033[0m"
-	@mkdir -p $(FAKE_PROJECT_DIR) || echo -e "\033[31m>FAILED:$@\033[0m"
-	@echo "\033[32m>done:$@\033[0m"
+	@echo "starting $@"
+	@$(MKDIR) -p $(FAKE_COMMAND_DIR) || { echo "FAILED:$@";exit 1; }
+	@$(MKDIR) -p $(FAKE_PROJECT_DIR) || { echo "FAILED:$@";exit 1; }
+	@echo "done: $@"
--- Makefile.d/self-tests/teardown.tests.fakeProject.mk
+++ Makefile.d/self-tests/teardown.tests.fakeProject.mk
@@ -3,7 +3,7 @@
 # (c) 2023 Sam Caldwell.  See License.txt
 #
 test/Makefile/teardown.fakeProject:
-	@echo "\033[32m>starting $@\033[0m"
-	@rm -rf cmd/fakeProject &> /dev/null || true
-	@rm -rf projects/fakeProject &> /dev/null || true
-	@echo "\033[32m>done:$@\033[0m"
+	@echo "starting $@"
+	@$(RM_RF) cmd/fakeProject &> /dev/null || true
+	@$(RM_RF)projects/fakeProject &> /dev/null || true
+	@echo "done: $@"
