8de516bfaaf3cf0d0747a1cc33b73f1bbfcd2683
sam caldwell <mail@samcaldwell.net>
2023-06-18 17:06:48 -0500

    fixing find parameter (windows)

--- Makefile.d/parameters.build_projects.mk
+++ Makefile.d/parameters.build_projects.mk
@@ -1,5 +1,5 @@
 ifeq ($(OPSYS),windows)
-BUILD_PROJECTS := $(shell for /r "cmd" %G in (.) do @(where /q "%%~G\build.disabled" || echo %%~dpG))
+BUILD_PROJECTS := $(shell powershell -NoProfile -Command "Get-ChildItem -Path 'cmd' -Recurse -Directory | ForEach-Object { if (-not (Test-Path -Path (Join-Path $_.FullName 'build.disabled'))) { $_.FullName } }")
 else
 BUILD_PROJECTS := $(shell find cmd -mindepth 2 -maxdepth 3 -type d -execdir test ! -f "build.disabled" \; -print)
 endif
