c7e021e42f4ffdb7cb41ea160e5eabeed1e51c51
sam caldwell <mail@samcaldwell.net>
2023-06-18 14:51:59 -0500

    add colors to clean

--- Makefile.d/clean.mk
+++ Makefile.d/clean.mk
@@ -1,4 +1,6 @@
 clean:
-	@make test/Makefile/teardown.fakeProject
-	@rm -rf ./build
-	@mkdir -p ./build
+	@echo "\033[32m>starting $@\033[0m"
+	@make test/Makefile/teardown.fakeProject &> /dev/null
+	@rm -rf ./build || { echo -e "\033[31m>>FAILED:$@\033[0m";exit 1; }
+	@mkdir -p ./build || { echo -e "\033[31m>>FAILED:$@\033[0m";exit 1; }
+	@echo "\033[32m>clean...done\033[0m"
