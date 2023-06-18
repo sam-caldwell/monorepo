cf6731ccf0e2469421a7eab01a5b9c9dd4bd4280
sam caldwell <mail@samcaldwell.net>
2023-06-18 17:06:11 -0500

    fixing find parameter (windows)

--- Makefile.d/parameters.build_projects.mk
+++ Makefile.d/parameters.build_projects.mk
@@ -1,5 +1,5 @@
 ifeq ($(OPSYS),windows)
-BUILD_PROJECTS := $(shell for /r "cmd" %G in (.) do @(dir /b /ad "%%~G" | findstr /r /c:"\\[^\\]*\\[^\\]*$$" >nul || echo %%~dpG))
+BUILD_PROJECTS := $(shell for /r "cmd" %G in (.) do @(where /q "%%~G\build.disabled" || echo %%~dpG))
 else
 BUILD_PROJECTS := $(shell find cmd -mindepth 2 -maxdepth 3 -type d -execdir test ! -f "build.disabled" \; -print)
 endif
