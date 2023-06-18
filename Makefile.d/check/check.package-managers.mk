333c9af060fa5fe6fd1017cbc6d092774709378a
sam caldwell <mail@samcaldwell.net>
2023-06-18 18:05:30 -0500

    more Makefile.d restructuring

--- Makefile.d/check/check.package-managers.mk
+++ Makefile.d/check/check.package-managers.mk
@@ -1,21 +1,11 @@
 #
 # PACKAGE MANAGERS
 #
-# mac/linux package managers
-HAS_BREW=$(shell go run cmd/tools/hasExecutable/main.go brew)
-# linux: debian/ubuntu
-HAS_APT=$(shell go run cmd/tools/hasExecutable/main.go apt-get)
-HAS_DPKG=$(shell go run cmd/tools/hasExecutable/main.go dpkg)
 
-# linux: redhat/centos/fedora
-HAS_YUM=$(shell go run cmd/tools/hasExecutable/main.go yum)
 
-# linux: redhat/centos/fedora/sles
-HAS_RPM=$(shell go run cmd/tools/hasExecutable/main.go rpm)
 
-# windows:
-HAS_WINGET=$(shell go run cmd/tools/hasExecutable/main.go winget)
-HAS_CHOCO=$(shell go run cmd/tools/hasExecutable/main.go choco)
+
+
 
 #
 # POSIX OPERATING SYSTEMS
@@ -29,19 +19,3 @@ check/package-managers:
 	[[ "$${HAS_WINGET}_" =="yes_" ]] && {echo "winget" && exit 0;}; \
 	[[ "$${HAS_CHOCO}_" =="yes_" ]] && {echo "choco" && exit 0;}; \
 
-
-check/package-managers/list:
-	@echo "\033[32m detected package managers: \033[0m"
-	@echo "\033[34m   macos/linux: \033[0m"
-	@echo "\033[34m\t     HAS_BREW: $(HAS_BREW) \033[0m"
-	@echo "\033[34m   linux: debian/ubuntu: \033[0m"
-	@echo "\033[34m\t      HAS_APT: $(HAS_APT) \033[0m"
-	@echo "\033[34m\t     HAS_DPKG: $(HAS_DPKG) \033[0m"
-	@echo "\033[34m   linux: redhat/centos/fedora: \033[0m"
-	@echo "\033[34m\t      HAS_YUM: $(HAS_YUM) \033[0m"
-	@echo "\033[34m   linux: redhat/centos/fedora/suse: \033[0m"
-	@echo "\033[34m\t      HAS_RPM: $(HAS_RPM) \033[0m"
-	@echo "\033[34m   windows: \033[0m"
-	@echo "\033[34m\t   HAS_WINGET: $(HAS_WINGET) \033[0m"
-	@echo "\033[34m\t    HAS_CHOCO: $(HAS_CHOCO) \033[0m"
-	@echo "\033[32m --- \033[0m"
