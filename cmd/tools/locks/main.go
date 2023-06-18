14ddb0f61d27f700ca634cec01414272bfa5d1a3
sam caldwell <mail@samcaldwell.net>
2023-06-17 20:12:56 -0500

    add a simple file lock system

---
+++ cmd/tools/locks/main.go
@@ -0,0 +1,116 @@
+package main
+
+/*
+ * locks command
+ * (c) 2023 Sam Caldwell.  All Rights Reserved.
+ *
+ * This is a cross-platform lock tool for our Makefiles or anything really.
+ * When executed, this tool will create a context-specific lock file.
+ *
+ * Note: By design this is a very trusting locking mechanism.  Do not use this
+ *       for security purposes.
+ *
+ * lock create
+ * 					<- creates a lock with the context and print the context string to stdout
+ * 					<- if 'foo' exists, the program will fail (exit 1)
+ * 				    <- if 'foo' does not exist, the lock is created (exit 0)
+ *
+ * lock check <contextId>
+ *  				<- check to see if the lock exists:
+ *								yes: exit 1 - locked,
+ * 								 no: exit 0 - unlocked
+ *
+ * lock free <contextId>
+ * 					<- delete the given context if it exists (exit 0)
+ */
+import (
+	"fmt"
+	"github.com/sam-caldwell/go/v2/projects/exit"
+	"github.com/sam-caldwell/go/v2/projects/fs/file"
+	"github.com/sam-caldwell/go/v2/projects/lock"
+	"github.com/sam-caldwell/go/v2/projects/misc/words"
+	"os"
+	"regexp"
+	"strings"
+)
+
+const (
+	cmdUsage = "\n\n" +
+		"Usage:\n" +
+		"\tlock %s            - creates a new lock file and returns its contextId\n" +
+		"\tlock %s <contextId> - indicates if lock exists (exit code 0: no, exit code 1: yes)\n" +
+		"\tlock %s <contextId>  - removes a given lock\n" +
+		"\n"
+
+	contextIdRegex = `^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$`
+	osArgCommand   = 1
+	osArgContextId = 2
+
+	lockExists = 1
+	lockFree   = 0
+)
+
+func main() {
+	exit.OnCondition(
+		len(os.Args) < osArgCommand+1,
+		exit.MissingArg,
+		fmt.Sprintf(exit.ErrMissingArgCommand, words.Command),
+		fmt.Sprintf(cmdUsage, words.Create, words.Check, words.Free))
+
+	getCheckContext := func() (string, error) {
+		exit.OnCondition(
+			len(os.Args) < osArgContextId+1,
+			exit.MissingArg,
+			exit.ErrMissingContextId,
+			fmt.Sprintf(cmdUsage, words.Create, words.Check, words.Free))
+		contextId := strings.ToLower(strings.TrimSpace(os.Args[osArgContextId]))
+		// Sanitize our contextId
+		if regexp.MustCompile(contextIdRegex).MatchString(contextId) {
+			return contextId, nil
+		}
+		return contextId, fmt.Errorf(exit.ErrInvalidContextId, contextId)
+	}
+
+	var lock lock.File
+
+	switch command := strings.ToLower(strings.TrimSpace(os.Args[osArgCommand])); command {
+	case words.Create:
+		contextId, err := lock.Create()
+		if err != nil {
+			exit.OnError(err, exit.LockCreateFailed, words.EmptyString)
+		}
+		fmt.Println(contextId)
+	case words.Check:
+		contextId, err := getCheckContext()
+		if err != nil {
+			exit.OnError(err, exit.InvalidInput, cmdUsage)
+		}
+		if err := lock.Check(contextId); err != nil {
+			if err.Error() == exit.ErrLockCheckFailed {
+				exit.OnError(err, exit.GeneralError, cmdUsage)
+			}
+			os.Exit(lockExists)
+		}
+		os.Exit(lockFree)
+	case words.Free:
+		contextId, err := getCheckContext()
+		if err != nil {
+			exit.OnError(err, exit.InvalidInput, cmdUsage)
+		}
+		if err := lock.Free(contextId); err != nil {
+			exit.OnError(err, exit.FailedToFreeLock, words.EmptyString)
+		}
+
+	default:
+		exit.OnError(fmt.Errorf(exit.ErrInvalidCommand, command), exit.InvalidCommand, cmdUsage)
+	}
+
+	tempFilePath, err := file.CreateTempFile()
+	if err != nil {
+		fmt.Printf("Error creating temporary file: %s\n", err)
+		return
+	}
+
+	fmt.Printf("Temporary file created: %s\n", tempFilePath)
+	defer os.Remove(tempFilePath) // Clean up the temporary file when done
+}
