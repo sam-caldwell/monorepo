a797d1c359e4e991aa93fbbdb81933ea70c74d89
sam caldwell <mail@samcaldwell.net>
2023-06-18 16:29:43 -0500

    working on opsys-specific ansi escape codes

--- Makefile.d/tools/tools.mk
+++ Makefile.d/tools/tools.mk
@@ -1,1 +1,24 @@
 COLOR:=./bin/color
+
+
+ifeq ($(OPSYS),windows)
+    # Windows
+    RM = del /Q /F
+    MKDIR = mkdir
+
+    ANSI_RED="^[[31m"
+    ANSI_GREEN="^[[32m"
+    ANSI_BLUE="^[[34m"
+    ANSI_RESET="^[[0m"
+else
+    # Linux or other Unix-like systems
+    RM = rm -f
+    RM_RF = rm -rf
+    MKDIR = mkdir -p
+
+	ANSI_RED="\\033[31m"
+	ANSI_GREEN="\\033[32m"
+	ANSI_BLUE="\\033[34m"
+	ANSI_RESET="\\033[0m"
+endif
+
