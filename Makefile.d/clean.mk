c04fc1c8fb9caa6d0402c60a27d89deda66bb40e
sam caldwell <mail@samcaldwell.net>
2023-06-18 16:47:45 -0500

    add debugging line

--- Makefile.d/clean.mk
+++ Makefile.d/clean.mk
@@ -1,4 +1,5 @@
 clean:
+	@echo "clean start $(OPSYS: $(OPSYS))"
 	@make test/Makefile/teardown.fakeProject &> /dev/null
 	@$(RM_RF) ./bin || { echo "FAILED:$@";exit 1; }
 	@$(MKDIR) ./bin || { echo "FAILED:$@";exit 1; }
