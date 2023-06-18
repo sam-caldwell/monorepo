20808be731523965a7ed20a7b8e075ba1b294870
sam caldwell <mail@samcaldwell.net>
2023-06-18 16:37:11 -0500

    Update clean command

--- Makefile.d/clean.mk
+++ Makefile.d/clean.mk
@@ -1,11 +1,7 @@
 clean:
-	@echo "$(ANSI_GREEN)>starting$(ANSI_RESET)"
 	@make test/Makefile/teardown.fakeProject &> /dev/null
-	@$(RM_RF) ./bin || { echo -e "$(ANSI_RED)>>FAILED:$(ANSI_RESET)";exit 1; }
-	@$(MKDIR) ./bin || { echo -e "$(ANSI_RED)>>FAILED:$(ANSI_RESET)";exit 1; }
-	@$(RM_RF) ./build || { echo -e "$(ANSI_RED)>>FAILED:$(ANSI_RESET)";exit 1; }
-	@$(MKDIR) ./build || { echo -e "$(ANSI_RED)>>FAILED:$(ANSI_RESET)";exit 1; }
-	@echo "$(ANSI_GREEN)>clean...done$(ANSI_RESET)"
-
-this:
-	@$(ECHO) $(ANSI_GREEN)"test"$(ANSI_RESET)
+	@$(RM_RF) ./bin || { echo "FAILED:$@";exit 1; }
+	@$(MKDIR) ./bin || { echo "FAILED:$@";exit 1; }
+	@$(RM_RF) ./build || { echo "FAILED:$@";exit 1; }
+	@$(MKDIR) ./build || { echo "FAILED:$@";exit 1; }
+	@echo "clean...done"
