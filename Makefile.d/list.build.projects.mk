ea1d8d7d10927ea31c608b8a50ded83d895d4d3d
sam caldwell <mail@samcaldwell.net>
2023-06-18 17:44:40 -0500

    abandoning windows...It's just too much effort to support the people who use it.

--- Makefile.d/list.build.projects.mk
+++ Makefile.d/list.build.projects.mk
@@ -3,16 +3,17 @@
 # For build autoamation docs, see docs/builds/README.md
 #
 list/build/projects:
-ifeq ($$(OPSYS),windows)
-	@$(NOT_SUPPORTED)
-	exit 1
-endif
 	@$(PRINT_START)
+ifeq ($$(OPSYS),windows)
+	@echo $(NOT_SUPPORTED)
+	exit 1
+else
 	@echo ""; \
 	echo "current binary projects (enabled):"; \
 	for PROJECT in $(BUILD_PROJECTS); do \
 		echo " - $$PROJECT"; \
 	done; \
 	echo "";
+endif
 	@$(PRINT_DONE)
 
