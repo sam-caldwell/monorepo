515891ecb43b8034860842a51d79890af650fd70
sam caldwell <mail@samcaldwell.net>
2023-06-18 17:27:54 -0500

    attempting a different solution to satisfy one edgecase (windows)

--- Makefile.d/list.build.projects.mk
+++ Makefile.d/list.build.projects.mk
@@ -2,18 +2,29 @@
 # list the projects that build artifacts.
 # For build autoamation docs, see docs/builds/README.md
 #
-list/build/projects:
-	@$(PRINT_START)
 ifeq ($(OPSYS),windows)
-	@powershell -NoProfile -Command "Write-Host 'current binary projects (enabled):'; $(foreach PROJECT,$(BUILD_PROJECTS), Write-Host ' - $(PROJECT)')"
+	# Windows
+	WINDOWS_LIST_PROJECTS = \
+		echo ""; \
+		echo "current binary projects (enabled):"; \
+		for %%I in ($(BUILD_PROJECTS)) do echo " - %%~I"; \
+		echo "";
 else
-	@( \
+	# Unix-like systems
+	UNIX_LIST_PROJECTS = \
 		echo ""; \
 		echo "current binary projects (enabled):"; \
 		for PROJECT in $(BUILD_PROJECTS); do \
 			echo " - $$PROJECT"; \
 		done; \
-		echo ""; \
-	)
+		echo "";
+endif
+
+list/build/projects:
+	@$(PRINT_START)
+ifeq ($(OPSYS),windows)
+	@$(WINDOWS_LIST_PROJECTS)
+else
+	@$(UNIX_LIST_PROJECTS)
 endif
 	@$(PRINT_DONE)
