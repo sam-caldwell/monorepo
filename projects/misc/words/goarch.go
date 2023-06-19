19aa323b8bec1140ec7dc5349d03f16183b6667c
sam caldwell <mail@samcaldwell.net>
2023-06-19 15:19:46 -0500

    add words/goarch.go

---
+++ projects/misc/words/goarch.go
@@ -0,0 +1,18 @@
+package words
+
+const (
+	Intel386 = "386"
+	Amd64    = "amd64"
+	Arm      = "arm"
+	Arm64    = "arm64"
+	Loong64  = "loong64"
+	Mips     = "mips"
+	Mips64   = "mips64"
+	Mips64le = "mips64le"
+	Mipsle   = "mipsle"
+	ppc64    = "ppc64"
+	ppc64le  = "ppc64le"
+	riscv64  = "riscv64"
+	s390x    = "s390x"
+	wasm     = "wasm"
+)
