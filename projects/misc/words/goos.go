357a357b8a39d54b3b3e3b5f480e6268aca362e8
sam caldwell <mail@samcaldwell.net>
2023-06-19 15:19:32 -0500

    add words/goos.go

---
+++ projects/misc/words/goos.go
@@ -0,0 +1,18 @@
+package words
+
+const (
+	Aix       = "aix"
+	Android   = "android"
+	Darwin    = "darwin"
+	Dragonfly = "dragonfly"
+	FreeBsd   = "freebsd"
+	Illumos   = "illumos"
+	Ios       = "ios"
+	Js        = "js"
+	Linux     = "linux"
+	NetBsd    = "netbsd"
+	OpenBsd   = "openbsd"
+	Plan9     = "plan9"
+	Solaris   = "solaris"
+	Windows   = "windows"
+)
