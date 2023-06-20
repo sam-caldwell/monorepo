bfa124df15082c28735cfbd324d179eed4beb7e5
sam caldwell <mail@samcaldwell.net>
2023-06-20 11:35:44 -0500

    minor formatting and such

--- projects/systemrecon/opsys/OpSys.go
+++ projects/systemrecon/opsys/OpSys.go
@@ -4,5 +4,7 @@ import "runtime"
 
 // OpSys - Return the operating system (e.g. windows, darwin, linux)
 func OpSys() (string, error) {
+
 	return runtime.GOOS, nil
+
 }
--- projects/systemrecon/opsys/OpSysVersion.go
+++ projects/systemrecon/opsys/OpSysVersion.go
@@ -1,21 +1,29 @@
 package systemrecon
 
 import (
+	"fmt"
 	"github.com/sam-caldwell/go/v2/projects/exit/errors"
 	"github.com/sam-caldwell/go/v2/projects/misc/words"
 	"runtime"
 )
 
 // OpSysVersion - Return the operating system version
-func OpSysVersion() (string, error) {
-	switch runtime.GOOS {
-	case words.Darwin:
-		return GetVersion()
-	case words.Linux:
-		return GetVersion()
-	case words.Windows:
-		return GetVersion()
+func OpSysVersion() (version string, err error) {
 
+	switch runtime.GOOS {
+
+	case words.Darwin:
+		version, err = GetVersion()
+
+	case words.Linux:
+		version, err = GetVersion()
+
+	case words.Windows:
+		version, err = GetVersion()
+
+	default:
+		version, err = words.EmptyString, fmt.Errorf(errors.UnsupportedOpsys)
 	}
-	return errors.UnsupportedOpsys, nil
+
+	return version, err
 }
