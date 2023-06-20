f861d25de039e7759a3a2fbb02fb5226ef0fe70c
sam caldwell <mail@samcaldwell.net>
2023-06-20 14:12:52 -0500

    a faster LeftPad()

--- projects/misc/LeftPad.go
+++ projects/misc/LeftPad.go
@@ -1,6 +1,8 @@
 package misc
 
+import "fmt"
+
 // LeftPad - pad the left side of string (s) with char (c) for total output size (sz) chars
 func LeftPad(c rune, s string, sz int) string {
-	return RepeatChars(string(c), sz-len(s)) + s
+	return fmt.Sprintf("%*s", sz-len(s), string(c))
 }
