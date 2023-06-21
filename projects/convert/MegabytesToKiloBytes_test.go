68a5fcc0f7cb24897557e7584bfb1fddc8ed03a5
sam caldwell <mail@samcaldwell.net>
2023-06-21 09:26:55 -0500

    add unit test for MegabytesToKiloBytes()

---
+++ projects/convert/MegabytesToKiloBytes_test.go
@@ -0,0 +1,37 @@
+package convert
+
+import (
+	"testing"
+)
+
+func TestMegabytesToKiloBytes(t *testing.T) {
+	tests := []struct {
+		name string
+		arg  int
+		want int
+	}{
+		{
+			name: "1 megabyte to kilobytes",
+			arg:  1,
+			want: 1024,
+		},
+		{
+			name: "0 megabytes to kilobytes",
+			arg:  0,
+			want: 0,
+		},
+		{
+			name: "negative megabytes to kilobytes",
+			arg:  -1,
+			want: -1024,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := MegabytesToKiloBytes(tt.arg); got != tt.want {
+				t.Errorf("MegabytesToKiloBytes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
