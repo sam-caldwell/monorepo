a0404c2251a5cbf4c6caf28a7754c336b02080e9
sam caldwell <mail@samcaldwell.net>
2023-06-24 15:14:45 -0500

    linter fix

--- projects/exit/errors/IgnoreError.go
+++ projects/exit/errors/IgnoreError.go
@@ -1,6 +1,4 @@
 package errors
 
 // Swallow - Used in cases where you want to ignore the error returned but be explicit that you are doing so.
-func Swallow(err error) {
-	return
-}
+func Swallow(err error) {}
