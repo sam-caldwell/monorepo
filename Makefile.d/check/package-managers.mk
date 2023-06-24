4f2c75770aabffd40076f717ae01fd00bbffbfbc
sam caldwell <mail@samcaldwell.net>
2023-06-24 12:43:02 -0500

    fix bugs in 'make check/package-managers

--- Makefile.d/check/package-managers.mk
+++ Makefile.d/check/package-managers.mk
@@ -2,11 +2,28 @@
 # Identify detected packet managers.
 #
 check/package-managers:
-	[[ "$${HAS_BREW}_" =="yes_" ]] && {echo "brew" && exit 0;}; \
-	[[ "$${HAS_APT}_" =="yes_" ]] && {echo "apt" && exit 0;}; \
-	[[ "$${HAS_YUM}_" =="yes_" ]] && {echo "yum" && exit 0;}; \
-	[[ "$${HAS_DPKG}_" =="yes_" ]] && {echo "dpkg" && exit 0;}; \
-	[[ "$${HAS_RPM}_" =="yes_" ]] && {echo "rpm" && exit 0;}; \
-	[[ "$${HAS_WINGET}_" =="yes_" ]] && {echo "winget" && exit 0;}; \
-	[[ "$${HAS_CHOCO}_" =="yes_" ]] && {echo "choco" && exit 0;}; \
+	@(\
+		[[ "$(HAS_BREW)_" == "yes_" ]] && { \
+			echo "brew" && exit 0;\
+		}; \
+		[[ "$(HAS_APT)_" == "yes_" ]] && { \
+			echo "apt" && exit 0;\
+		}; \
+		[[ "$(HAS_YUM)_" == "yes_" ]] && { \
+			echo "yum" && exit 0; \
+		}; \
+		[[ "$(HAS_DPKG)_" == "yes_" ]] && { \
+		  echo "dpkg" && exit 0; \
+		}; \
+		[[ "$(HAS_RPM)_" == "yes_" ]] && { \
+		  echo "rpm" && exit 0; \
+		}; \
+		[[ "$(HAS_WINGET)_" == "yes_" ]] && { \
+		  echo "winget" && exit 0; \
+		}; \
+		[[ "$(HAS_CHOCO)_" == "yes_" ]] && { \
+		  echo "choco" && exit 0; \
+		};\
+		exit 0;\
+	)
 
