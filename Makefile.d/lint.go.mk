6e64a5132e1aec5422f73afe07bdaf5053631afe
sam caldwell <mail@samcaldwell.net>
2023-06-28 16:22:16 -0500

    start vetting multi opsys

--- Makefile.d/lint.go.mk
+++ Makefile.d/lint.go.mk
@@ -17,17 +17,17 @@ lint/vet:
 		GOOS=darwin go vet ./... || { \
 			echo "\033[31m>failed on darwin [$@]\033[0m";\
 			exit 1;\
-		};\
+		} || exit 1;\
 		echo "\033[32m>darwin: ok $@\033[0m"; \
 		GOOS=linux go vet ./... || { \
 			echo "\033[31m>failed on linux [$@]\033[0m";\
 			exit 1;\
-		};\
+		} || exit 1;\
 		echo "\033[32m>linux: ok $@\033[0m"; \
 		GOOS=windows go vet ./... || { \
 			echo "\033[31m>failed on windows [$@]\033[0m";\
 			exit 1;\
-		}; \
+		} || exit 1; \
 		echo "\033[32m>windows: ok $@\033[0m"; \
 	);\
 	echo "\033[32m>ok $@\033[0m"
