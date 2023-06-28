52aba3bd67eb8fe8e51baf5ff7a64111d81d8855
sam caldwell <mail@samcaldwell.net>
2023-06-28 16:21:19 -0500

    start vetting multi opsys

--- Makefile.d/lint.go.mk
+++ Makefile.d/lint.go.mk
@@ -13,10 +13,23 @@ lint/go/setup:
 
 lint/vet:
 	@echo "\033[34m>starting $@\033[0m"
-	@go vet ./... || { \
-		echo "\033[31m>failed $@\033[0m";\
-		exit 1;\
-	}; \
+	@(\
+		GOOS=darwin go vet ./... || { \
+			echo "\033[31m>failed on darwin [$@]\033[0m";\
+			exit 1;\
+		};\
+		echo "\033[32m>darwin: ok $@\033[0m"; \
+		GOOS=linux go vet ./... || { \
+			echo "\033[31m>failed on linux [$@]\033[0m";\
+			exit 1;\
+		};\
+		echo "\033[32m>linux: ok $@\033[0m"; \
+		GOOS=windows go vet ./... || { \
+			echo "\033[31m>failed on windows [$@]\033[0m";\
+			exit 1;\
+		}; \
+		echo "\033[32m>windows: ok $@\033[0m"; \
+	);\
 	echo "\033[32m>ok $@\033[0m"
 
 lint/staticcheck:
