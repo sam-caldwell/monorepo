9950e6ace7d07d4994501e905fa103cda3194a8f
sam caldwell <mail@samcaldwell.net>
2023-06-29 09:36:07 -0500

    debugging staticcheck in github actions

--- Makefile.d/lint/lint.staticcheck.mk
+++ Makefile.d/lint/lint.staticcheck.mk
@@ -2,9 +2,14 @@ lint/install/staticcheck:
 	@(\
 		command -v staticcheck &> /dev/null || { \
 			echo "\033[34m>starting $@\033[0m"; \
-			go get honnef.co/go/tools/cmd/staticcheck || { \
-				echo "\033[31m>(install failed) failed $@\033[0m";\
-				exit 1;\
+			export STATICCHECK_CACHE="${{ runner.temp }}/staticcheck"; \
+			go get -u honnef.co/go/tools; \
+			go get honnef.co/go/tools/cmd/staticcheck; \
+			go install "honnef.co/go/tools/cmd/staticcheck@2023.1.3"; \
+			echo "Verify install..."; \
+			command -v staticcheck || { \
+				echo 'install failed.'; \
+				exit 1; \
 			}; \
 			command -v staticcheck &> /dev/null || { \
 				echo "\033[31m>(file not found) failed $@\033[0m";\
