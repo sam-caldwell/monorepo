b47b1a9c61b21f07052586129dd88e03d5cefa7a
sam caldwell <mail@samcaldwell.net>
2023-06-29 08:11:17 -0500

    debugging staticcheck execution in github actions

--- Makefile.d/lint/lint.staticcheck.mk
+++ Makefile.d/lint/lint.staticcheck.mk
@@ -1,21 +1,19 @@
 lint/install/staticcheck:
-	@command -v staticcheck &> /dev/null || { \
-		echo "\033[34m>starting $@\033[0m"; \
-		go get honnef.co/go/tools/cmd/staticcheck|| { \
-			echo "\033[31m>(install failed) failed $@\033[0m";\
-			exit 1;\
-		}; \
+	@(\
 		command -v staticcheck &> /dev/null || { \
-			echo "\033[31m>(file not found) failed $@\033[0m";\
-			echo "GOPATH: '$(go env GOPATH)'";\
-			echo "  PATH: '$(PATH)'";\
-			ls -la /home/runner/go/bin; \
-			ls -la /opt/hostedtoolcache/go/1.20.5/x64/bin; \
-ls -la /home/runner/.local/bin; \
-			exit 1;\
-		}; \
-		echo "\033[32m>ok $@\033[0m";\
-  	}
+			echo "\033[34m>starting $@\033[0m"; \
+			go get honnef.co/go/tools/cmd/staticcheck || { \
+				echo "\033[31m>(install failed) failed $@\033[0m";\
+				exit 1;\
+			}; \
+			command -v staticcheck &> /dev/null || { \
+				echo "\033[31m>(file not found) failed $@\033[0m";\
+				ls -la /home/runner/.local/bin; \
+				exit 1;\
+			}; \
+			echo "\033[32m>ok $@\033[0m";\
+		};\
+	)
 
 lint/staticcheck/%:
 	@echo "\033[34m>starting $@\033[0m"
