4eef68aae34c9015a8ab481ec9e29a807305bbf1
sam caldwell <mail@samcaldwell.net>
2023-06-29 01:55:00 -0500

    adding more debugging for lint/staticcheck to debug github actions failures

--- Makefile.d/lint/lint.staticcheck.mk
+++ Makefile.d/lint/lint.staticcheck.mk
@@ -4,6 +4,10 @@ lint/install/staticcheck:
 		echo "\033[31m>failed $@\033[0m";\
 		exit 1;\
 	}; \
+	command -v staticcheck || { \
+		echo "\033[31m>(file not found) failed $@\033[0m";\
+		exit 1;\
+	}; \
 	echo "\033[32m>ok $@\033[0m"
 
 lint/staticcheck/%:
