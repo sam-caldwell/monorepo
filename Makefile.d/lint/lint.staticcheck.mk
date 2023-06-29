01de771a0b5c838c2722f10e3b3ece4c8ec895aa
sam caldwell <mail@samcaldwell.net>
2023-06-29 07:43:26 -0500

    debugging github actions

--- Makefile.d/lint/lint.staticcheck.mk
+++ Makefile.d/lint/lint.staticcheck.mk
@@ -7,6 +7,9 @@ lint/install/staticcheck:
 		}; \
 		command -v staticcheck &> /dev/null || { \
 			echo "\033[31m>(file not found) failed $@\033[0m";\
+			ls -la /home/runner/go/bin; \
+			ls -la /opt/hostedtoolcache/go/1.20.5/x64/bin; \
+ls -la /home/runner/.local/bin; \
 			exit 1;\
 		}; \
 		echo "\033[32m>ok $@\033[0m";\
