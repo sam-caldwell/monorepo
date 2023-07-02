be9ad5f32e00675ea47da8a86648bb3b305de112
sam caldwell <mail@samcaldwell.net>
2023-07-01 21:27:53 -0500

    disable self-tests (intended to test makefiles)

--- Makefile.d/test.mk
+++ Makefile.d/test.mk
@@ -1,5 +1,5 @@
 test: lint test/simple
 	echo "$@ done"
 
-test/simple: test/Makefile.d/self-tests/run
-	go test -v -count=1 ./...
+test/simple: # test/Makefile.d/self-tests/run
+	go test -failfast -v -count=1 ./...
