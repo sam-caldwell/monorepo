61a46ae1340aaa6e99639d1c28c331f244ffe25e
sam caldwell <mail@samcaldwell.net>
2023-07-01 21:47:26 -0500

    add color coding and a nice exit for make test

--- Makefile.d/test.mk
+++ Makefile.d/test.mk
@@ -1,5 +1,10 @@
 test: lint test/simple
-	echo "$@ done"
+	@echo "\033[32m>done:$@\033[0m"
 
 test/simple: # test/Makefile.d/self-tests/run
-	go test -failfast -v -count=1 -vet=all ./...
+	@echo "\033[32m>starting $@\033[0m"
+	go test -failfast -v -count=1 -vet=all ./... || { \
+  		echo "\033[31m>failed:$@\033[0m"; \
+  		exit 1; \
+  	}; \
+	echo "\033[32m>done:$@\033[0m"
