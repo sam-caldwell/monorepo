ce4f23f01b371753fe1ac7768119473b0cf17749
sam caldwell <mail@samcaldwell.net>
2023-07-05 17:05:58 -0500

    PIPELINES-19: works on mac, doesn't work on linux...testing

--- cmd/tools/hasExecutable/main.go
+++ cmd/tools/hasExecutable/main.go
@@ -1,10 +1,5 @@
 package main
 
-/*
- * This utility program can run on multiple operating systems and answer the question
- * "Is this 'executable' installed?" and it will only return a 'yes' or 'no' response.
- * If any error occurs, it replies 'no'
- */
 import (
 	"fmt"
 	"github.com/sam-caldwell/go/v2/projects/misc/words"
@@ -19,10 +14,13 @@ const (
 	shellCode      = "%s"
 )
 
-func runCommand(shell string, command []string) (exitCode int, response string) {
+func runCommand(shell string, command string, args []string) (exitCode int, response string) {
 	response = words.No
-	fmt.Printf("shell: %s command: %v\n", shell, command)
-	cmd := exec.Command(shell, command...)
+	fmt.Printf("shell: %s command: %s args: %v\n", shell, command, args)
+
+	args = append([]string{command}, args...)
+	cmd := exec.Command(shell, args...)
+
 	if out, err := cmd.Output(); err == nil {
 		fmt.Printf("out: '%s'\n", string(out))
 		exitCode = 0
@@ -32,17 +30,18 @@ func runCommand(shell string, command []string) (exitCode int, response string) 
 		exitCode = 1
 		response = words.No
 	}
+
 	return exitCode, response
 }
 
 func hasCommand(targetCommand string) (exitCode int, answer string) {
 	switch goos := runtime.GOOS; goos {
 	case "windows":
-		return runCommand("powershell", []string{"-Command", fmt.Sprintf(powershellCode, targetCommand)})
+		return runCommand("powershell", "-Command", []string{fmt.Sprintf(powershellCode, targetCommand)})
 	case "darwin":
-		return runCommand("command", []string{"-v", fmt.Sprintf(shellCode, targetCommand)})
+		return runCommand("command", "-v", []string{fmt.Sprintf(shellCode, targetCommand)})
 	case "linux":
-		return runCommand("command", []string{"-v", fmt.Sprintf(shellCode, targetCommand)})
+		return runCommand("command", "-v", []string{fmt.Sprintf(shellCode, targetCommand)})
 	}
 	//unsupported operating system
 	return 2, words.No
@@ -53,14 +52,15 @@ func main() {
 		fmt.Println("no")
 		os.Exit(0)
 	}
+
 	targetCommand := strings.TrimSpace(os.Args[1])
 	exitCode, answer := hasCommand(targetCommand)
 	fmt.Println(answer)
+
 	if len(os.Args) == 3 {
 		if strings.TrimSpace(strings.ToLower(os.Args[2])) == "--exitcode" {
 			fmt.Println("exit_code:", exitCode)
 			os.Exit(exitCode)
 		}
 	}
-
 }
