dc9dc7a081ed50e825f2543c93ade2e54dbcaf1b
sam caldwell <mail@samcaldwell.net>
2023-07-05 18:27:18 -0500

    fix lint/shell for linux

--- Makefile.d/lint/lint.shell.mk
+++ Makefile.d/lint/lint.shell.mk
@@ -1,13 +1,12 @@
 lint/shell: lint/install/shellcheck
 	@echo "\033[34m>starting $@\033[0m"
 	@shellcheck Makefile.d/scripts/*/*
-	@find . -type f -name "*.sh" -print0 | while IFS= read -r -d $$'\0' file; do \
-	    echo "\033[34m>>Checking $$file\033[0m"; \
-	    shellcheck "$$file"; \
-	    status=$$?; \
-	    if [ $$status -ne 0 ]; then \
-			echo "\033[32m>failed on $$file $@\033[0m"; \
-	        exit $$status; \
-	    fi; \
-	done
-	@echo "\033[32m>ok $@\033[0m"
+	@( \
+		for file in $(find . -type f -name "*.sh" -print0); do \
+			shellcheck $${file} || {\
+				echo "\033[32m>failed on $${file} $@\033[0m"; \
+				exit $$status; \
+			};\
+	  	done;\
+	) || exit 1;\
+	echo "\033[32m>ok $@\033[0m"
