a44796ba723441cb9232c8c01a62883d998c25de
sam caldwell <mail@samcaldwell.net>
2023-07-07 23:29:03 -0500

    add assemblyJmpToFunction() for Amd64 test with endianness awareness.  Code cleanup

--- projects/testing/hijack/assemblyJmpToFunction.Amd64_test.go
+++ projects/testing/hijack/assemblyJmpToFunction.Amd64_test.go
@@ -5,36 +5,57 @@ package hijack
 
 import (
 	"encoding/binary"
+	endianness "github.com/sam-caldwell/go/v2/projects/convert/Endianness"
 	"reflect"
 	"testing"
 	"unsafe"
 )
 
 func TestAssemblyJmpToFunction(t *testing.T) {
-	const MOV = 0xBA // Assembly instruction MOV
-	const JMP = 0xFF // Assembly instruction JMP
-	const RDX = 0x22 // CPU RDX Register
+	const (
+		ptr32bit = 4 // 32bits / 8 bits per byte = 4
+		ptr64bit = 8 // 64bits / 8 bits per byte = 8
+
+		MOV = 0xBA // Assembly instruction MOV
+		JMP = 0xFF // Assembly instruction JMP
+		RDX = 0x22 // CPU RDX Register
+	)
+
 	// Create our fake target and its address (destination)
 	targetFunc := func() {}
 	// Determine the address of our destination address (32/64-bit)
 	destination := reflect.ValueOf(targetFunc).Pointer()
 	// Get the size of our destination
-	sz := unsafe.Sizeof(destination)
+	AddressSize := unsafe.Sizeof(destination)
 	// Convert destination to a []byte slice
-	byteAddress := make([]byte, sz)
-	switch sz {
-	case 4:
-		binary.LittleEndian.PutUint32(byteAddress, uint32(destination))
-	case 8:
-		binary.LittleEndian.PutUint64(byteAddress, uint64(destination))
+	byteAddress := make([]byte, AddressSize)
+	switch AddressSize {
+	case ptr32bit: //We are dealing with 32-bits...cool
+		if endianness.LittleEndian {
+			binary.LittleEndian.PutUint32(byteAddress, uint32(destination))
+		} else {
+			binary.BigEndian.PutUint32(byteAddress, uint32(destination))
+		}
+	case ptr64bit: //We are dealing with 64-bits...how modern of ya?
+		if endianness.LittleEndian {
+			binary.LittleEndian.PutUint64(byteAddress, uint64(destination))
+		} else {
+			binary.BigEndian.PutUint64(byteAddress, uint64(destination))
+		}
 	default:
-		t.Fatalf("TestAssemblyJmpToFunction does not support size %d", sz)
+		// We're in the future!  I told you not to take the delorean past 88mph...but did you listen? No...
+		t.Fatalf("TestAssemblyJmpToFunction does not support size %d", AddressSize)
 	}
-	expectedInstructions := make([]byte, sz+3)
+
+	expectedInstructions := make([]byte, AddressSize+3)
+
 	expectedInstructions[0] = MOV
+
 	copy(expectedInstructions[1:], byteAddress)
-	expectedInstructions[sz+1] = JMP
-	expectedInstructions[sz+2] = RDX
+
+	expectedInstructions[AddressSize+1] = JMP
+
+	expectedInstructions[AddressSize+2] = RDX
 
 	actualInstructions := assemblyJmpToFunction(destination)
 
@@ -46,14 +67,4 @@ func TestAssemblyJmpToFunction(t *testing.T) {
 			"  actualInstructions: %0x\n",
 			destination, expectedInstructions, actualInstructions)
 	}
-
-	// Test case: Destination is 0 (null address)
-	//destination = uintptr(0)
-	//expectedInstructions = []byte{0xBA, 0x00, 0x00, 0x00, 0x00, 0xFF, 0x22}
-	//instructions = assemblyJmpToFunction(destination)
-
-	// Verify that the generated instructions match the expected instructions
-	//if !reflect.DeepEqual(instructions, expectedInstructions) {
-	//	t.Errorf("Generated instructions do not match expected instructions")
-	//}
 }
