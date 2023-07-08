1b9dc99def74a7ac8975f5287c63145adf91a320
sam caldwell <mail@samcaldwell.net>
2023-07-07 23:53:04 -0500

    add more tests for peek()

--- projects/testing/hijack/peek_test.go
+++ projects/testing/hijack/peek_test.go
@@ -1,31 +1,53 @@
 package hijack
 
 import (
-	"reflect"
 	"testing"
 	"unsafe"
 )
 
 func TestPeek(t *testing.T) {
-	// Test case: Peek into a memory address with valid data
-	data := []byte{0x12, 0x34, 0x56, 0x78}
-	addr := uintptr(unsafe.Pointer(&data[0]))
-	length := len(data)
-	result := peek(addr, length)
+	const (
+		NULL          = 0x00
+		expectedValue = "This is a test string of several words"
+	)
 
-	// Verify that the peeked data matches the original data
-	if !reflect.DeepEqual(result, data) {
-		t.Errorf("Peeked data does not match the original data")
-	}
+	func() {
+		// Test case: Peek into a memory address with valid data
 
-	// Test case: Peek into a memory address with zero length
-	var emptyData []byte
-	emptyAddr := uintptr(unsafe.Pointer(&emptyData))
-	emptyLength := len(emptyData)
-	emptyResult := peek(emptyAddr, emptyLength)
+		data := []byte(expectedValue)
+		addr := uintptr(unsafe.Pointer(&data[0]))
+		length := len(data)
+		actualValue := peek(addr, length)
+		// Verify that the peeked data matches the original data
+		if string(actualValue) != expectedValue {
+			t.Errorf("Peeked data does not match the original data")
+		}
+	}()
 
-	// Verify that peeking into an empty memory address returns an empty slice
-	if len(emptyResult) != 0 {
-		t.Errorf("Peeked data is not empty for an empty memory address")
-	}
+	func() {
+		// Test case: Peek into a memory address with zero length
+		var emptyData []byte
+		emptyAddr := uintptr(unsafe.Pointer(&emptyData))
+		emptyLength := len(emptyData)
+		actualValue := peek(emptyAddr, emptyLength)
+
+		// Verify that peeking into an empty memory address returns an empty slice
+		if len(actualValue) != emptyLength {
+			t.Errorf("Peeked data is not empty for an empty memory address")
+		}
+	}()
+
+	func() {
+		// Test case: send in a null pointer and zero length
+		emptyAddr := uintptr(NULL)
+		emptyLength := 10
+		actualValue := peek(emptyAddr, emptyLength)
+
+		// Verify that peeking into an empty memory address returns an empty slice
+		if len(actualValue) != emptyLength {
+			t.Errorf("Peeked data is not empty for an empty memory address\n"+
+				"actualValue size:  %d\n"+
+				"     actualValue: '%0x'", len(actualValue), actualValue)
+		}
+	}()
 }
