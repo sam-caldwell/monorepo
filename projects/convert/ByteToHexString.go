88882b7c3c3685516c5ed6885dc6099041ed198f
sam caldwell <mail@samcaldwell.net>
2023-07-11 08:17:04 -0500

    adding HexPrintFormat() and ByteToHexString to convert package

--- projects/convert/ByteToHexString.go
+++ projects/convert/ByteToHexString.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func ByteToHexString(b []byte) (out string) {
 	for i := 0; i < len(b); i++ {
-		out += fmt.Sprintf("%00x ", b[i])
+		out += fmt.Sprintf("%02x ", b[i])
 	}
 	return out
 }
