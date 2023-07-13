4c6593a29696007de1ec451be90e04606499cca7
sam caldwell <mail@samcaldwell.net>
2023-07-13 10:48:26 -0500

    bug fix for negative numbers and test fix

--- projects/moremath/CountDigitsInt.go
+++ projects/moremath/CountDigitsInt.go
@@ -1,16 +1,17 @@
 package moremath
 
 func CountDigitsInt(n int) int {
+	count := 0
+
 	if n == 0 {
 		return 1
 	}
-
-	count := 0
-	for n != 0 {
-		n /= 10
+	//account for - in negative number
+	if n < 0 {
 		count++
 	}
-	if n < 0 {
+	for n != 0 {
+		n /= 10
 		count++
 	}
 
