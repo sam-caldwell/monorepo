e868538597a9587a3ea08e182d0f63f3fed9e65e
sam caldwell <mail@samcaldwell.net>
2023-07-13 08:37:47 -0500

    Add documentation to CalcMaxColumnWidth()

--- projects/misc/CalcMaxColumnWidth.go
+++ projects/misc/CalcMaxColumnWidth.go
@@ -1,5 +1,17 @@
 package misc
 
+/*
+ * CalcMaxColumnWidth
+ * (c) 2023 Sam Caldwell.  See LICENSE.txt
+ *
+ * This file creates a function which will take a list of
+ * strings and calculate the width of the longest string,
+ * returning the same as an int.
+ *
+ * See README.md
+ */
+
+// CalcMaxColumnWidth - given a list calculate the maximum width of the strings in the list.
 func CalcMaxColumnWidth(list *[]string) (maxWidth int) {
 	if list != nil {
 		for _, line := range *list {
