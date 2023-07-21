b0b13bbe19756755ab202a4e7f58e0c9f14874fc
sam caldwell <mail@samcaldwell.net>
2023-07-21 11:05:33 -0500

    adding lint-repo tool with minimum viable code (no linter functions implemented)

--- cmd/tools/lint-repo/main.go
+++ cmd/tools/lint-repo/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/sam-caldwell/go/v2/projects/exit"
+import (
+	"github.com/sam-caldwell/go/v2/projects/ansi"
+	"github.com/sam-caldwell/go/v2/projects/exit"
+	repolinter "github.com/sam-caldwell/go/v2/projects/repotools/linter"
+	"github.com/sam-caldwell/go/v2/projects/simpleArgs"
+)
 
 /*
  * lint-repo
@@ -11,6 +16,12 @@ import "github.com/sam-caldwell/go/v2/projects/exit"
  *
  * See README.md
  */
+
+import (
+	"fmt"
+	"os"
+)
+
 const (
 	commandUsage = `
 lint-repo -h|--help 
@@ -24,8 +35,42 @@ lint-repo [-color]
 
 // main - lint-repo main function
 func main() {
+	var useColor = simpleArgs.UseColor()
 
 	exit.IfHelpRequested(commandUsage)
 	exit.IfVersionRequested()
 
+	fail := func(name string, err error) {
+		const format = "Linting on [FAIL](%s): %s"
+		if useColor {
+			ansi.Blue().Printf(format, name, err).LF().Reset()
+		} else {
+			fmt.Printf(format, name, err)
+		}
+	}
+
+	pass := func(name string) {
+		const format = "Linting [PASS](%s)"
+		if useColor {
+			ansi.Blue().Printf(format, name).LF().Reset()
+		} else {
+			fmt.Printf(format, name)
+		}
+	}
+
+	if useColor {
+		ansi.Blue().Printf("Running Linter").LF().Reset()
+	} else {
+		fmt.Printf("Running Linter\n")
+	}
+
+	if err := repolinter.LinterMaster(pass, fail); err != nil {
+		if useColor {
+			ansi.Red().Printf("Linter failed (%s)", err).LF().Reset()
+			os.Exit(exit.Success)
+		} else {
+			fmt.Printf("Linter failed (%s)\n", err)
+			os.Exit(exit.GeneralError)
+		}
+	}
 }
