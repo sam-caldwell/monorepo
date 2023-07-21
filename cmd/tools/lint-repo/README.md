605e9cdc905a0ed1a6a5edf78e8e2cb9e7d2ec04
sam caldwell <mail@samcaldwell.net>
2023-07-21 08:36:25 -0500

    REPOTOOL-9: fixup command MANIFESTs

---
+++ cmd/tools/lint-repo/README.md
@@ -0,0 +1,16 @@
+lint-repo
+=========
+
+## Description
+This program will run the linter against every
+file in the repository, stopping on the first error.
+
+## Usage
+```text
+lint-repo -h|--help 
+   Show usage
+   
+lint-repo [-color]
+   Run linter and if -color is present show
+   the output in ANSI color.
+```
---
+++ cmd/tools/lint-repo/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "github.com/sam-caldwell/go/v2/projects/exit"
+
+/*
+ * lint-repo
+ * (c) 2023 Sam Caldwell.  See LICENSE.txt
+ *
+ * This program creates will lint our repository
+ * and its various file formats.
+ *
+ * See README.md
+ */
+const (
+	commandUsage = `
+lint-repo -h|--help 
+   Show usage
+   
+lint-repo [-color]
+   Run linter and if -color is present show
+   the output in ANSI color.
+`
+)
+
+// main - lint-repo main function
+func main() {
+
+	exit.IfHelpRequested(commandUsage)
+
+}
