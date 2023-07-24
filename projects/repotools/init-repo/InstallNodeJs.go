9ea05ba51961c06f93cb7897fce15f7de8481a9d
sam caldwell <mail@samcaldwell.net>
2023-07-24 11:44:03 -0500

    adding debug line for ubuntu linux

--- projects/repotools/init-repo/InstallNodeJs.go
+++ projects/repotools/init-repo/InstallNodeJs.go
@@ -2,6 +2,7 @@ package repotools
 
 import (
 	"fmt"
+	"github.com/sam-caldwell/go/v2/projects/ansi"
 	"github.com/sam-caldwell/go/v2/projects/exit"
 	"github.com/sam-caldwell/go/v2/projects/exit/errors"
 	"github.com/sam-caldwell/go/v2/projects/misc/words"
@@ -15,17 +16,15 @@ func InstallNodeJs(commandUsage string) string {
 		return words.EmptyString
 	case words.Linux:
 		distro, err := systemrecon.GetVersion()
+		ansi.Blue().Printf("Linux distro: %s", distro).LF().Reset()
 		exit.OnError(err, exit.GeneralError, commandUsage)
 
 		switch distro {
-
 		case words.Ubuntu:
 			return "command -v node || sudo apt install nodejs npm -y"
-
 		case words.Centos:
 			exit.OnError(fmt.Errorf(errors.NotImplemented), exit.GeneralError, commandUsage)
 			return words.EmptyString
-
 		default:
 			exit.OnError(fmt.Errorf(errors.UnsupportedOpsys+errors.Details, distro), exit.GeneralError, commandUsage)
 		}
