cbd5abfac0d789a1046a7158cec5eb08d772ab7b
sam caldwell <mail@samcaldwell.net>
2023-07-23 19:57:53 -0500

    make runTest() function so we can start the process of supporting multiple languages

--- projects/repotools/tester/Setup.go
+++ projects/repotools/tester/Setup.go
@@ -20,6 +20,19 @@ func Setup(
 	skip repocli.SkipMessagePrintFunc,
 	fail repocli.FailMessagePrintFunc) func(projectType string) (err error) {
 
+	runTest := func(projectPath string, manifest *projectmanifest.Manifest) error {
+		command := fmt.Sprintf("go test -failfast -v -count=2 -vet=all %s/...", projectPath)
+		out, err := runcommand.ShellExecute(command)
+		notice(out)
+		if err == nil {
+			pass(manifest.Name, projectPath)
+		} else {
+			err = fmt.Errorf("out:%s\nerr:%s", out, err)
+			fail(manifest.Name, projectPath, err)
+		}
+		return err
+	}
+
 	// Run - Run all tests for the repo or specific project
 	return func(projectType string) (err error) {
 
@@ -45,14 +58,7 @@ func Setup(
 			}
 			if manifest.IsTestEnabled() {
 				d := filepath.Dir(path)
-				command := fmt.Sprintf("go test -failfast -v -count=2 -vet=all %s/...", d)
-				out, err := runcommand.ShellExecute(command)
-				if err == nil {
-					pass(manifest.Name, path)
-				} else {
-					err = fmt.Errorf("out:%s\nerr:%s", out, err)
-					fail(manifest.Name, path, err)
-				}
+				err = runTest(d, &manifest)
 			} else {
 				skip(manifest.Name, path, "test not enabled")
 			}
