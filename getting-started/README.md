29f60d27dadf1927aa08bfbf37207193602bce11
sam caldwell <mail@samcaldwell.net>
2023-07-24 18:52:56 -0500

    add getting-started scripts

---
+++ getting-started/README.md
@@ -0,0 +1,5 @@
+Getting Started Scripts
+=======================
+
+## Description
+This directory contains scripts for bootstrapping a new dev environment.
---
+++ getting-started/ubuntu-linux.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -e
+
+command -v git ||{
+  echo "git must be installed first"
+  echo "run sudo apt-get install git -y"
+  echo "Then clone this repo...how did you get this far?"
+  exit 1
+}
+
+apt-get update -y --fix-missing
+
+sudo apt-get update -y --fix-missing
+
+sudo apt-get install build-essential curl nodejs npm python3 python3-pip shellcheck yamllint flake8 -y
+
+# shellcheck disable=SC2016
+echo 'export PATH=${PATH}:${HOME}/.local/bin' > ${HOME}/.bashrc
+
+# shellcheck disable=SC1090
+source "${HOME}/.bashrc"
+
+pip3 install virtualenv
+
+npm install -g npm
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+# shellcheck disable=SC1090
+source "${HOME}/.bashrc"
+
+nvm install "$(nvm ls-remote | tail -n 1)"
+
+npm install npm
+
+npm install -g snyk
