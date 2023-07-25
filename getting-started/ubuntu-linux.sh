978f9cb9574d2d7c528e4d5c0ed6ad924b625c90
sam caldwell <mail@samcaldwell.net>
2023-07-24 20:04:39 -0500

    update the getting-started/ubuntu-linux.sh

--- getting-started/ubuntu-linux.sh
+++ getting-started/ubuntu-linux.sh
@@ -7,41 +7,57 @@ command -v git ||{
   exit 1
 }
 
-sudo apt-get update -y --fix-missing
+[[ "$(whoami)" == "root" ]] && {
+  echo "cannot run as root"
+  exit 1
+}
 
-sudo apt-get update -y --fix-missing
+create_directories(){
+  mkdir -p "${HOME}/go/bin"
+  mkdir -p "${HOME}/.local/bin"
+}
+append_bashrc_paths(){
+  # shellcheck disable=SC2016
+  echo 'export PATH=${PATH}:${HOME}/.local/bin/:${HOME}/go/bin' > "${HOME}/.bashrc"
+}
+update_apt(){
+  sudo apt-get update -y --fix-missing
+  sudo apt-get install build-essential curl nodejs npm python3 python3-pip shellcheck yamllint flake8 -y
+}
+install_virtualenv(){
+  command -v virtualenv || pip3 install virtualenv
+  source "${HOME}/.bashrc"
+}
+install_nvm(){
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+  LATEST_NODE_VERSION="$(nvm ls-remote | tail -n 1 | awk '{print $1}')"
+  nvm install "${LATEST_NODE_VERSION}"
+}
 
-sudo apt-get install build-essential curl nodejs npm python3 python3-pip shellcheck yamllint flake8 -y
-
+create_directories
+append_bashrc_paths
+update_apt
+# shellcheck disable=SC1090
+source "${HOME}/.bashrc"
 flake8 --version
 yamllint --version
 shellcheck --version
-
-# shellcheck disable=SC2016
-echo 'export PATH=${PATH}:${HOME}/.local/bin' > ${HOME}/.bashrc
-
 # shellcheck disable=SC1090
 source "${HOME}/.bashrc"
-
-command -v virtualenv || pip3 install virtualenv
-source "${HOME}/.bashrc"
-virtualenv --version
-
+install_virtualenv
 npm --version
 node --version
-
+virtualenv --version
 npm install
-
-command -v nvm || {
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-}
 # shellcheck disable=SC1090
 source "${HOME}/.bashrc"
-
-LATEST_NODE_VERSION="$(nvm ls-remote | tail -n 1 | awk '{print $1}')"
-nvm install "${LATEST_NODE_VERSION}"
-
-# npm --version
-# node --version
-
+install_nvm
+# shellcheck disable=SC1090
+source "${HOME}/.bashrc"
+npm --version
+node --version
 command -v snyk || npm install snyk
+pip3 install -r ./requirements.txt
+go mod tidy
+go install honnef.co/go/tools/cmd/staticcheck@latest
+
