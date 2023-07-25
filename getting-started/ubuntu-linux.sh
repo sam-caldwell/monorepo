796008680077ac9a6c13e734b0661f0948433c00
sam caldwell <mail@samcaldwell.net>
2023-07-24 19:07:44 -0500

    disabling scripts for debugging

--- getting-started/ubuntu-linux.sh
+++ getting-started/ubuntu-linux.sh
@@ -14,22 +14,22 @@ sudo apt-get update -y --fix-missing
 sudo apt-get install build-essential curl nodejs npm python3 python3-pip shellcheck yamllint flake8 -y
 
 # shellcheck disable=SC2016
-echo 'export PATH=${PATH}:${HOME}/.local/bin' > ${HOME}/.bashrc
+#echo 'export PATH=${PATH}:${HOME}/.local/bin' > ${HOME}/.bashrc
 
 # shellcheck disable=SC1090
-source "${HOME}/.bashrc"
+#source "${HOME}/.bashrc"
 
-pip3 install virtualenv
+#pip3 install virtualenv
 
-npm install -g npm
+#npm install -g npm
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
 # shellcheck disable=SC1090
-source "${HOME}/.bashrc"
-
-nvm install "$(nvm ls-remote | tail -n 1)"
-
-npm install npm
-
-npm install -g snyk
+#source "${HOME}/.bashrc"
+#
+#nvm install "$(nvm ls-remote | tail -n 1)"
+#
+#npm install npm
+#
+#npm install -g snyk
