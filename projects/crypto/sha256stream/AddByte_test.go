9d05c13f615fd66166ec73bc31c778d5d3b6c560
sam caldwell <mail@samcaldwell.net>
2023-08-02 11:19:08 -0500

    fix tests for AddByte()

--- projects/crypto/sha256stream/AddByte_test.go
+++ projects/crypto/sha256stream/AddByte_test.go
@@ -1,46 +1,69 @@
 package crypto
 
 import (
-	"reflect"
 	"testing"
 )
 
-func TestSha256StreamAddByte(t *testing.T) {
+func TestSha256Stream_AddByte_OneByte(t *testing.T) {
 	// Create a new Sha256Stream instance
-	stream := Sha256Stream{}
-
-	// Add some bytes to the buffer
-	bytesToAdd := []byte{0x61, 0x62, 0x63, 0x64} // 'a', 'b', 'c', 'd'
-	for _, b := range bytesToAdd {
-		stream.AddByte(b)
+	hash := NewSha256Stream()
+	if hash.bitNdx != 0 {
+		t.Fatalf("expected bitNdx 0. Got: %d", hash.bitNdx)
 	}
-
-	// Calculate the expected buffer state after adding the bytes
-	expectedBufferState := [64]byte{0x61, 0x62, 0x63, 0x64}
-	expectedByteNdx := int8(len(bytesToAdd))
-	expectedBitNdx := int8(0)
-
-	// Compare the actual buffer state with the expected buffer state
-	if !reflect.DeepEqual(stream.buffer, expectedBufferState) {
-		t.Errorf("AddByte() method didn't set the buffer as expected.\n"+
-			"Expected Buffer: %v\n"+
-			"Actual Buffer: %v",
-			expectedBufferState, stream.buffer)
+	if hash.byteNdx != 0 {
+		t.Fatalf("expected byteNdx 0. Got: %d", hash.bitNdx)
 	}
-
-	// Compare the actual byteNdx with the expected byteNdx
-	if stream.byteNdx != expectedByteNdx {
-		t.Errorf("AddByte() method didn't set the byteNdx as expected.\n"+
-			"Expected byteNdx: %d\n"+
-			"Actual byteNdx: %d",
-			expectedByteNdx, stream.byteNdx)
+	hash.AddByte(0xFF)
+	if hash.bitNdx != 0 {
+		t.Fatalf("expected bitNdx 0. Got: %d", hash.bitNdx)
 	}
+	if hash.byteNdx != 1 {
+		t.Fatalf("expected byteNdx 1. Got: %d", hash.bitNdx)
+	}
+}
 
-	// Compare the actual bitNdx with the expected bitNdx
-	if stream.bitNdx != expectedBitNdx {
-		t.Errorf("AddByte() method didn't set the bitNdx as expected.\n"+
-			"Expected bitNdx: %d\n"+
-			"Actual bitNdx: %d",
-			expectedBitNdx, stream.bitNdx)
+func TestSha256Stream_AddByte_TwoBytes(t *testing.T) {
+	// Create a new Sha256Stream instance
+	hash := NewSha256Stream()
+	if hash.bitNdx != 0 {
+		t.Fatalf("expected bitNdx 0. Got: %d", hash.bitNdx)
+	}
+	if hash.byteNdx != 0 {
+		t.Fatalf("expected byteNdx 0. Got: %d", hash.bitNdx)
+	}
+	hash.AddByte(0xFF)
+	if hash.bitNdx != 0 {
+		t.Fatalf("expected bitNdx 0. Got: %d", hash.bitNdx)
+	}
+	if hash.byteNdx != 1 {
+		t.Fatalf("expected byteNdx 1. Got: %d", hash.bitNdx)
+	}
+	hash.AddByte(0xFF)
+	if hash.bitNdx != 0 {
+		t.Fatalf("expected bitNdx 0. Got: %d", hash.bitNdx)
+	}
+	if hash.byteNdx != 2 {
+		t.Fatalf("expected byteNdx 2. Got: %d", hash.bitNdx)
+	}
+}
+
+func TestSha256Stream_AddByte_FullBuffer(t *testing.T) {
+	// Create a new Sha256Stream instance
+	hash := NewSha256Stream()
+	for i := int8(1); i < 64; i++ {
+		hash.AddByte(0xFF)
+		if hash.bitNdx != 0 {
+			t.Fatalf("expected bitNdx 0. Got: %d", hash.bitNdx)
+		}
+		if hash.byteNdx != i {
+			t.Fatalf("expected byteNdx %d. Got: %d", i, hash.bitNdx)
+		}
+	}
+	hash.AddByte(0xFF)
+	if hash.bitNdx != 0 {
+		t.Fatalf("expected bitNdx 0. Got: %d", hash.bitNdx)
+	}
+	if hash.byteNdx != 0 {
+		t.Fatalf("expect byteNdx 0. Got: %d", hash.byteNdx)
 	}
 }
