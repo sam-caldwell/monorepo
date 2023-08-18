bdf3cc918799f6a2c17bafd992b5eb8fc0aa0ae0
Sam Caldwell <github@samcaldwell.net>
2023-08-18 18:32:15 -0500

    CRSCE-4: code clean up

--- projects/fs/file/BinaryIo/BinaryIo.Flush.go
+++
@@ -1,8 +0,0 @@
-package file
-
-func (io *BinaryIo) Flush() {
-	if io.handle == nil {
-		return
-	}
-	_ = io.handle.Sync()
-}
--- projects/fs/file/BinaryIo/BinaryIo.go
+++
@@ -1,9 +0,0 @@
-package file
-
-import "os"
-
-// BinaryIo - A binary file reader.
-type BinaryIo struct {
-	// handle - File handle for the current file.
-	handle *os.File
-}
