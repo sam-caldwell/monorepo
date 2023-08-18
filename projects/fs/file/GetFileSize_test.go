90dd2ed911c538851a1b14078f65eac4622dda82
Sam Caldwell <github@samcaldwell.net>
2023-08-18 18:47:29 -0500

    CRSCE-4: add GetFileSize() to the fs/file project

---
+++ projects/fs/file/GetFileSize_test.go
@@ -0,0 +1,32 @@
+package file
+
+import (
+	"os"
+	"testing"
+)
+
+func TestGetFileSize(t *testing.T) {
+	// Create a temporary file for testing
+	tempFile, err := os.CreateTemp("", "testfile")
+	if err != nil {
+		t.Fatalf("Failed to create temporary file: %v", err)
+	}
+	defer func() { _ = tempFile.Close() }()
+	defer func() { _ = os.Remove(tempFile.Name()) }()
+
+	// Write some content to the temporary file
+	content := "Hello, world!"
+	_, err = tempFile.WriteString(content)
+	if err != nil {
+		t.Fatalf("Failed to write content to temporary file: %v", err)
+	}
+
+	// Get the file size using the function
+	fileSize := GetFileSize(tempFile)
+
+	// Verify the file size
+	expectedSize := int64(len(content))
+	if fileSize != expectedSize {
+		t.Errorf("Expected file size: %d, but got: %d", expectedSize, fileSize)
+	}
+}
