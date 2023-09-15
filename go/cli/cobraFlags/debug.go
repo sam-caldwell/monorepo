67720d3a06ebc4960e6d841e863d29825ae63657
Sam Caldwell <github@samcaldwell.net>
2023-09-15 18:12:34 -0500

    implement standardized --debug flag for all cli apps

--- go/cli/cobraFlags/debug.go
+++ go/cli/cobraFlags/debug.go
@@ -1,1 +1,18 @@
 package cobraFlags
+
+/*
+ * cli/cobraFlags/debug.go
+ * (c) 2023 Sam Caldwell.  See LICENSE.txt
+ *
+ * This file defines the reusable persistent --debug flag
+ * for cobra cli applications.
+ */
+import (
+	"github.com/sam-caldwell/monorepo/go/misc/words"
+	"github.com/spf13/cobra"
+)
+
+// Debug - create --debug flag for cobra applications
+func Debug(cmd *cobra.Command) {
+	cmd.PersistentFlags().BoolP(words.Debug, words.EmptyString, false, "Print verbose output")
+}
