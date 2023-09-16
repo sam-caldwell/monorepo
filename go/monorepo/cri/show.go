e72bf9497131a07cd655f8cb89b488d1608d9ea7
Sam Caldwell <github@samcaldwell.net>
2023-09-16 18:32:36 -0500

    update cri.Show()

--- go/monorepo/cri/show.go
+++ go/monorepo/cri/show.go
@@ -4,24 +4,18 @@ import (
 	"fmt"
 	"github.com/sam-caldwell/monorepo/go/ansi"
 	"github.com/sam-caldwell/monorepo/go/cli"
+	"github.com/sam-caldwell/monorepo/go/convert"
 	"github.com/sam-caldwell/monorepo/go/misc/words"
 	"github.com/spf13/viper"
 )
 
 func Show(name string) {
-	convertInterfacesToStrings := func(input []interface{}) []string {
-		result := make([]string, len(input))
-		for i, v := range input {
-			result[i] = v.(string)
-		}
-		return result
-	}
 	containerRuntimes := viper.Get(words.ContainerRuntimes).([]interface{})
 	criSet := cli.Set{}
 	for _, r := range containerRuntimes {
 		row := r.(map[string]interface{})
 		if row[words.Name].(string) == name {
-			platformList := convertInterfacesToStrings(row["platforms"].([]any))
+			platformList := convert.InterfacesToStrings(row["platforms"].([]any))
 			criSet.Add(
 				fmt.Sprintf("- name: %s\n  enabled: %t\n  platforms: %v\n",
 					name, row[words.Enabled].(bool), platformList))
