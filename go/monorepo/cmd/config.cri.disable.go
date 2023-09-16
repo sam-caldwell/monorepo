2b428da045dd3a83fac5e4e4b8dec4a64c76ffb1
Sam Caldwell <github@samcaldwell.net>
2023-09-16 18:32:05 -0500

    add cri.Enable() and cri.Disable()

---
+++ go/monorepo/cmd/config.cri.disable.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"github.com/sam-caldwell/monorepo/go/ansi"
+	"github.com/sam-caldwell/monorepo/go/cli"
+	monorepoCri "github.com/sam-caldwell/monorepo/go/monorepo/cri"
+	"github.com/spf13/cobra"
+)
+
+// configCriDisableCmd represents the 'config command
+var configCriDisableCmd = &cobra.Command{
+	Use:   "disable",
+	Short: "- Disable a specified container runtime configuration",
+	Long:  `Disable a specified container runtime configuration`,
+	Args:  cobra.ExactArgs(1), //name
+	Run: func(cmd *cobra.Command, args []string) {
+		name := args[0]
+		if cli.IsNoop() {
+			ansi.Yellow().Printf("CRI config (%s) disable (noop)\n", name).Reset()
+			return
+		}
+		if err := monorepoCri.Disable(name); err != nil {
+			ansi.Red().Printf("CRI config (%s) disable failed\n", name).Reset()
+		}
+		ansi.Green().Printf("CRI config (%s) disabled\n", name).Reset()
+
+		cli.WriteConfig(cmd)
+	},
+}
+
+func init() {
+	configCriCmd.AddCommand(configCriDisableCmd)
+}
---
+++ go/monorepo/cmd/config.cri.enable.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"github.com/sam-caldwell/monorepo/go/ansi"
+	"github.com/sam-caldwell/monorepo/go/cli"
+	monorepoCri "github.com/sam-caldwell/monorepo/go/monorepo/cri"
+	"github.com/spf13/cobra"
+)
+
+// configCriEnableCmd represents the 'config command
+var configCriEnableCmd = &cobra.Command{
+	Use:   "enable",
+	Short: "- Enable a specified container runtime configuration",
+	Long:  `Enable a specified container runtime configuration`,
+	Args:  cobra.ExactArgs(1), //name
+	Run: func(cmd *cobra.Command, args []string) {
+		name := args[0]
+		if cli.IsNoop() {
+			ansi.Yellow().Printf("CRI config (%s) enable (noop)\n", name).Reset()
+			return
+		}
+		if err := monorepoCri.Enable(name); err != nil {
+			ansi.Red().Printf("CRI config (%s) enable failed\n", name).Reset()
+		}
+		ansi.Green().Printf("CRI config (%s) enabled\n", name).Reset()
+		cli.WriteConfig(cmd)
+	},
+}
+
+func init() {
+	configCriCmd.AddCommand(configCriEnableCmd)
+}
