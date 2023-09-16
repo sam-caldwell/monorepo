c58fe0a0d70d817e73dde7e2564410642060acf5
Sam Caldwell <github@samcaldwell.net>
2023-09-16 18:33:23 -0500

    update cli.GetCriList() to use convert.InterfacesToStrings

--- go/cli/GetCriList.go
+++ go/cli/GetCriList.go
@@ -1,6 +1,7 @@
 package cli
 
 import (
+	"github.com/sam-caldwell/monorepo/go/convert"
 	"github.com/sam-caldwell/monorepo/go/misc/words"
 	"github.com/spf13/viper"
 )
@@ -8,18 +9,11 @@ import (
 // GetCriList - Compare the filter list (from cli) to the cri list from the config file and return the
 // names of container runtimes which match the filter.
 func GetCriList(filterSet []string) []string {
-	convertInterfacesToStrings := func(input []interface{}) []string {
-		result := make([]string, len(input))
-		for i, v := range input {
-			result[i] = v.(string)
-		}
-		return result
-	}
 	platformNames := Set{}
 	containerRuntimes := viper.Get(words.ContainerRuntimes).([]interface{})
 	for _, r := range containerRuntimes {
 		row := r.(map[string]interface{})
-		platformList := convertInterfacesToStrings(row["platforms"].([]any))
+		platformList := convert.InterfacesToStrings(row["platforms"].([]any))
 		if e := row["enabled"].(bool); e {
 			for _, platform := range platformList {
 				for _, filter := range filterSet {
