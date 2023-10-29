e623c9bfa89e44e18317bb1ba44f4d9b6b90255a
Sam Caldwell <github@samcaldwell.net>
2023-10-29 18:30:19 -0500

    increase btrfs commit time for crsce/collsion_test

--- go/crsce/tools/collision_test/run_test.sh
+++ go/crsce/tools/collision_test/run_test.sh
@@ -4,7 +4,7 @@ echo "reset the filesystem"
 sudo umount /opt/data || true
 #echo "y" | sudo mkfs.ext4 /dev/sdb
 sudo mkfs.btrfs /dev/sdb -f
-sudo mount -o noatime,rw,ssd,compress=zlib,autodefrag,nodatacow,commit=60 /dev/sdb /opt/data
+sudo mount -o noatime,rw,ssd,compress=zlib,autodefrag,nodatacow,commit=120 /dev/sdb /opt/data
 sudo chown $(whoami) /opt/data
 echo "start test"
 go run main.go
