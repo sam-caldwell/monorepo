a67451977934cec583c2cdf0da918ee6d721c92f
Sam Caldwell <github@samcaldwell.net>
2023-10-29 18:58:24 -0500

    btrfs performance optimization for testing in crsce collision_test

--- go/crsce/tools/collision_test/run_test.sh
+++ go/crsce/tools/collision_test/run_test.sh
@@ -4,7 +4,8 @@ echo "reset the filesystem"
 sudo umount /opt/data || true
 #echo "y" | sudo mkfs.ext4 /dev/sdb
 sudo mkfs.btrfs /dev/sdb -f
-sudo mount -o noatime,rw,ssd,compress=zlib,autodefrag,nodatacow,commit=120 /dev/sdb /opt/data
+#sudo mount -o noatime,rw,ssd,compress=zlib:9,autodefrag,nodatacow,commit=120,noacl,nobarrier,discard=async /dev/sdb /opt/data
+sudo mount -o noatime,rw,ssd,compress=zlib:9,autodefrag,commit=60,noacl,nobarrier,discard=async /dev/sdb /opt/data
 sudo chown $(whoami) /opt/data
 echo "start test"
 go run main.go
