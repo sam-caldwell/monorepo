58ca2b162a767935ee7b01fcb5ec499c25c055e8
Sam Caldwell <github@samcaldwell.net>
2023-10-29 17:43:32 -0500

    improve stats for crsce/collision_test

--- go/crsce/tools/collision_test/main.go
+++ go/crsce/tools/collision_test/main.go
@@ -10,10 +10,10 @@ package main
 import (
 	"github.com/sam-caldwell/monorepo/go/counters"
 	"github.com/sam-caldwell/monorepo/go/fs/directory"
-	"github.com/sam-caldwell/monorepo/go/fs/file"
 	"log"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 const (
@@ -38,7 +38,7 @@ func initialize() {
 
 func main() {
 	var directoryCount uint64
-	var fileCount uint64
+	//var fileCount uint64
 
 	log.Println("Starting")
 	initialize()
@@ -52,34 +52,47 @@ func main() {
 			break
 		}
 		func() {
+			startTime := time.Now().Unix()
+
 			hash := input.Sha1()
 
-			dirPath := filepath.Join(root, hash[0:2], hash[2:4], hash[4:6], hash[6:8], hash[8:10])
-			fileName := filepath.Join(dirPath, hash[10:]+fileExtension)
+			dirPath := filepath.Join(
+				root,
+				hash[0:2], hash[2:4], hash[4:6], hash[6:8], hash[8:10],
+				hash[10:12], hash[12:14], hash[14:16], hash[16:18], hash[18:20])
 
+			//fileName := filepath.Join(dirPath, "sha1")
+
+			if directory.Exists(dirPath) {
+				log.Fatalf("collision found at (%s): %v", dirPath, err)
+			}
 			if err := os.MkdirAll(dirPath, 0744); err != nil {
 				log.Fatalf("Failed to create path (%s): %v", dirPath, err)
 			}
 			directoryCount++
-			if file.Exists(fileName) {
-				log.Fatalf("Collision found at (%s): %s", fileName, hash)
-			}
-			fileHandle, err := os.Create(fileName)
-			if err != nil {
-				log.Fatalf("Error creating file(%s):%s", fileName, err)
-				return
-			}
-			fileCount++
+			stopTime := time.Now().Unix()
+
+			//if file.Exists(fileName) {
+			//	log.Fatalf("Collision found at (%s): %s", fileName, hash)
+			//}
+			//fileHandle, err := os.Create(fileName)
+			//if err != nil {
+			//	log.Fatalf("Error creating file(%s):%s", fileName, err)
+			//	return
+			//}
+			//fileCount++
 
 			defer func() {
-				log.Printf("directories: %d, files: %d", directoryCount, fileCount)
+				elapsedTime := stopTime - startTime
+				ops := float64(directoryCount) / float64(elapsedTime)
+				log.Printf("objects: %d, elapsedTime: %d object/sec: %f", directoryCount, elapsedTime, ops)
 			}()
 
-			defer func() {
-				if err := fileHandle.Close(); err != nil {
-					log.Fatalf("error closing file at %s: %v", hash, err)
-				}
-			}()
+			//defer func() {
+			//	if err := fileHandle.Close(); err != nil {
+			//		log.Fatalf("error closing file at %s: %v", hash, err)
+			//	}
+			//}()
 		}()
 	}
 }
