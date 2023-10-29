9b64af1673104c47794c7f025a574c11943f920b
Sam Caldwell <github@samcaldwell.net>
2023-10-29 17:06:41 -0500

    change to btrfs for running crsce collision_test

--- go/crsce/tools/collision_test/run_test.sh
+++ go/crsce/tools/collision_test/run_test.sh
@@ -2,7 +2,8 @@
 
 echo "reset the filesystem"
 sudo umount /opt/data || true
-echo "y" | sudo mkfs.ext4 /dev/sdb
+#echo "y" | sudo mkfs.ext4 /dev/sdb
+sudo mkfs.btrfs /dev/sdb -f
 sudo mount -o noatime,rw /dev/sdb /opt/data
 sudo chown $(whoami) /opt/data
 echo "start test"
