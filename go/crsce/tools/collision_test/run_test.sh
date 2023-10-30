3de4676ee517e5739b6e1dee04533423d70276cf
Sam Caldwell <github@samcaldwell.net>
2023-10-29 19:37:34 -0500

    turn off discard option for btrfs to try and improve performance for crsce/collision_test

--- go/crsce/tools/collision_test/run_test.sh
+++ go/crsce/tools/collision_test/run_test.sh
@@ -5,7 +5,7 @@ sudo umount /opt/data || true
 #echo "y" | sudo mkfs.ext4 /dev/sdb
 sudo mkfs.btrfs /dev/sdb -f
 #sudo mount -o noatime,rw,ssd,compress=zlib:9,autodefrag,nodatacow,commit=120,noacl,nobarrier,discard=async /dev/sdb /opt/data
-sudo mount -o noatime,rw,ssd,compress=zlib:9,autodefrag,commit=60,noacl,nobarrier,discard=async /dev/sdb /opt/data
+sudo mount -o noatime,rw,ssd,compress=zlib:9,autodefrag,commit=60,noacl,nobarrier,nodiscard /dev/sdb /opt/data
 sudo chown $(whoami) /opt/data
 echo "start test"
 go run main.go
