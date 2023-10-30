3c0fb9158b0f10cf052905756325b494e29ff085
Sam Caldwell <github@samcaldwell.net>
2023-10-29 21:33:27 -0500

    implement free-space-tree for btrfs

--- go/crsce/tools/collision_test/run_test.sh
+++ go/crsce/tools/collision_test/run_test.sh
@@ -3,7 +3,8 @@
 echo "reset the filesystem"
 sudo umount /opt/data || true
 #echo "y" | sudo mkfs.ext4 /dev/sdb
-sudo mkfs.btrfs /dev/sdb -f
+#sudo mkfs.btrfs /dev/sdb -f
+sudo mkfs.btrfs -R free-space-tree -f -L data /dev/sdb
 #sudo mount -o noatime,rw,ssd,compress=zlib:9,autodefrag,nodatacow,commit=120,noacl,nobarrier,discard=async /dev/sdb /opt/data
 sudo mount -o noatime,rw,ssd,compress=lzo,autodefrag,commit=120,noacl,nobarrier,nodiscard /dev/sdb /opt/data
 sudo chown $(whoami) /opt/data
