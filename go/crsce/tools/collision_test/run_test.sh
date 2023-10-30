68e69dd88207d1eef08a6b9124c5dbbf5a9ccfa4
Sam Caldwell <github@samcaldwell.net>
2023-10-29 20:21:29 -0500

    change to lzo compression for crsce collsision_test performance

--- go/crsce/tools/collision_test/run_test.sh
+++ go/crsce/tools/collision_test/run_test.sh
@@ -5,7 +5,7 @@ sudo umount /opt/data || true
 #echo "y" | sudo mkfs.ext4 /dev/sdb
 sudo mkfs.btrfs /dev/sdb -f
 #sudo mount -o noatime,rw,ssd,compress=zlib:9,autodefrag,nodatacow,commit=120,noacl,nobarrier,discard=async /dev/sdb /opt/data
-sudo mount -o noatime,rw,ssd,compress=zstd:15,autodefrag,commit=60,noacl,nobarrier,discard=async /dev/sdb /opt/data
+sudo mount -o noatime,rw,ssd,compress=lzo,autodefrag,commit=60,noacl,nobarrier,discard=async /dev/sdb /opt/data
 sudo chown $(whoami) /opt/data
 echo 2048 | sudo tee /sys/block/sdb/queue/nr_requests
 
