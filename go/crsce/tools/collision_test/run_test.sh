f5d0844b69d98bdee6a803f72f2d8363472ef37d
Sam Caldwell <github@samcaldwell.net>
2023-10-29 20:02:50 -0500

    increase nr_requests for crsce collission_test

--- go/crsce/tools/collision_test/run_test.sh
+++ go/crsce/tools/collision_test/run_test.sh
@@ -7,5 +7,7 @@ sudo mkfs.btrfs /dev/sdb -f
 #sudo mount -o noatime,rw,ssd,compress=zlib:9,autodefrag,nodatacow,commit=120,noacl,nobarrier,discard=async /dev/sdb /opt/data
 sudo mount -o noatime,rw,ssd,compress=zstd:15,autodefrag,commit=60,noacl,nobarrier,discard=async /dev/sdb /opt/data
 sudo chown $(whoami) /opt/data
+echo 2048 | sudo tee /sys/block/sdb/queue/nr_requests
+
 echo "start test"
 go run main.go
