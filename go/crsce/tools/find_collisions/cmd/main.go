52e8d75c90b2f5a7bf75878353e6da439c6ed088
Sam Caldwell <github@samcaldwell.net>
2023-11-02 05:47:03 -0500

    add a backoff for worker spawn on consumer side.

--- go/crsce/tools/find_collisions/cmd/main.go
+++ go/crsce/tools/find_collisions/cmd/main.go
@@ -12,8 +12,11 @@ import (
 	"bytes"
 	"encoding/hex"
 	"flag"
+	"github.com/sam-caldwell/monorepo/go/ansi"
 	"github.com/sam-caldwell/monorepo/go/counters"
 	"log"
+	"math"
+	"os"
 	"runtime"
 	"time"
 )
@@ -21,7 +24,7 @@ import (
 const (
 	logInterval         = 5 * time.Second
 	defaultKeySpaceSize = 1024
-	candidateQueueSize  = 65536
+	candidateQueueSize  = 32768
 )
 
 type Candidate struct {
@@ -52,10 +55,42 @@ func main() {
 	count := uint64(0)
 	queue := make(chan Candidate, candidateQueueSize)
 	completedPasses := 0
+	ops := float64(0)
+	minOps := math.MaxFloat64
+	maxOps := float64(0)
+	queueSz := candidateQueueSize
+	minQueueSz := math.MaxInt
+	maxQueueSz := 0
+
+	// Metrics collection
+	go func() {
+		time.Sleep(5 * time.Second)
+		metricsTicket := time.NewTicker(1 * time.Second)
+		defer metricsTicket.Stop()
+		for {
+			select {
+			case <-metricsTicket.C:
+				if (ops < minOps) || ((minOps == 0) && (ops < maxOps)) {
+					minOps = ops
+				}
+				if ops > maxOps {
+					maxOps = ops
+				}
+				queueSz = len(queue)
+				if (queueSz < minQueueSz) || ((minQueueSz == 0) && (queueSz < maxQueueSz)) {
+					minQueueSz = queueSz
+				}
+				if queueSz > maxQueueSz {
+					maxQueueSz = queueSz
+				}
+			}
+		}
+	}()
 
 	// Timed logging
 	go func() {
 		log.Printf("Log interval: %d", logInterval)
+		time.Sleep(logInterval)
 		ticker := time.NewTicker(logInterval) // Adjust the interval as needed
 		defer ticker.Stop()
 		for {
@@ -63,10 +98,13 @@ func main() {
 			case <-ticker.C:
 				elapsedTime := float64(time.Now().Unix() - startTime)
 				chgCompletedPasses := float64(completedPasses) / elapsedTime
-
-				log.Printf("elapsed: %f objectCnt: %d, object/sec: %6.2f "+
-					"queueSz:%d passes/sec: %6.4f",
-					elapsedTime, count, float64(count)/elapsedTime, len(queue), chgCompletedPasses)
+				ops = float64(count) / elapsedTime
+				log.Printf("elapsed: %f objectCnt: %d,"+
+					" object/sec: %6.2f (min: %6.2f, max: %6.2f) "+
+					"queueSz:%d (min: %d, max: %d)"+
+					"passes/sec: %6.4f",
+					elapsedTime, count, ops, minOps, maxOps, queueSz,
+					minQueueSz, maxQueueSz, chgCompletedPasses)
 			}
 		}
 	}()
@@ -89,38 +127,62 @@ func main() {
 					}
 				}
 			}(worker)
+			time.Sleep(1 * time.Second)
 		}
 	}()
 
 	//Test RHS against LHS values
 	exit := make(chan bool, 1)
-	for worker := uint(0); worker < *NumberOfWorkers; worker++ {
-		log.Printf("Start worker %d", worker)
-		for candidate := range queue {
-			go func(id uint, lhs Candidate) {
-				rhs, _ := counters.NewByteCounter(int(*keySpaceSize))
-				for {
-					if bytes.Compare(rhs.Bytes(), lhs.raw) >= 0 {
-						completedPasses++
-						break
-					}
-					if lhs.hash == rhs.Sha1() {
-						log.Printf("collision\n"+
-							"lhs %v\n"+
-							"rhs %v\n---\n"+
-							"%v\n"+
-							"---\n"+
-							"%v\n"+
-							"---\n",
-							lhs.hash, rhs.Sha1(),
-							hex.EncodeToString(lhs.raw), rhs.String())
-						exit <- true
-					}
-					count++
-					_ = rhs.Increment()
+	maxWorkers := int(*NumberOfWorkers) * 200
+	workerBackoff := time.Duration(1)
+	log.Printf("Start consumers")
+	backOffId := int64(0)
+	backOffIncreases := int64(0)
+	backOffDecreases := int64(0)
+	for candidate := range queue {
+		//log.Printf("Start cycle. workers (count: %d)", runtime.NumGoroutine())
+		go func(lhs Candidate) {
+			rhs, _ := counters.NewByteCounter(int(*keySpaceSize))
+			for {
+				if bytes.Compare(rhs.Bytes(), lhs.raw) >= 0 {
+					completedPasses++
+					break
 				}
-			}(worker, candidate)
+				if lhs.hash == rhs.Sha1() {
+					log.Printf("collision\n"+
+						"lhs %v\n"+
+						"rhs %v\n---\n"+
+						"%v\n"+
+						"---\n"+
+						"%v\n"+
+						"---\n",
+						lhs.hash, rhs.Sha1(),
+						hex.EncodeToString(lhs.raw), rhs.String())
+					os.Exit(1)
+				}
+				count++
+				_ = rhs.Increment()
+			}
+		}(candidate)
+		for runtime.NumGoroutine() >= maxWorkers {
+			log.Printf("%d worker count: %d (max %d, backoff: %v)",
+				backOffId, runtime.NumGoroutine(), maxWorkers, workerBackoff*time.Millisecond)
+			time.Sleep(time.Millisecond * workerBackoff)
+			if runtime.NumGoroutine() > maxWorkers/2 {
+				ansi.Red()
+				workerBackoff *= 2
+				backOffIncreases++
+			} else {
+				ansi.Green()
+				workerBackoff /= 2
+				backOffDecreases++
+			}
+			log.Printf("%d backoff complete (count: %d inc: %d, dec: %d)",
+				backOffId, runtime.NumGoroutine(), backOffIncreases, backOffDecreases)
+			ansi.Reset()
+			backOffId++
 		}
 	}
+	//}
 	<-exit
 }
