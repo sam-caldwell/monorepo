94f638f9f9c7fd0d02c1bbf14f53c03969975335
Sam Caldwell <github@samcaldwell.net>
2023-11-06 01:35:01 -0600

    Implement tests for fs/file.IsEndOfFile()

--- go/fs/file/IsEndOfFile.go
+++ go/fs/file/IsEndOfFile.go
@@ -1,12 +1,19 @@
 package file
 
+/*
+ * IsEndOfFile()
+ * (c) 2023 Sam Caldwell. See License.txt
+ *
+ * Safely return boolean true if EOF is reached.
+ */
+
 import (
 	"errors"
 	"io"
 )
 
-// isEndOfFile - evaluate an error and return whether it signals end of file
-func isEndOfFile(err error) (bool, error) {
+// IsEndOfFile - evaluate an error and return whether it signals end of file
+func IsEndOfFile(err error) (bool, error) {
 	if errors.Is(err, io.EOF) {
 		return true, nil
 	}
--- go/fs/file/IsEndOfFile_test.go
+++ go/fs/file/IsEndOfFile_test.go
@@ -1,1 +1,44 @@
 package file
+
+/*
+ * file.IsEndOfFile() test
+ * (c) 2023 Sam Caldwell.  See License.txt
+ *
+ * Test the IsEndOfFile() function
+ */
+
+import (
+	"errors"
+	"io"
+	"testing"
+)
+
+func TestIsEndOfFile(t *testing.T) {
+	func() {
+		// Test when error is io.EOF
+		err := io.EOF
+		isEOF, err := IsEndOfFile(err)
+
+		if !isEOF {
+			t.Errorf("Expected IsEndOfFile to return true for io.EOF, but got false")
+		}
+
+		if err != nil {
+			t.Errorf("Expected error to be nil for io.EOF, but got %v", err)
+		}
+
+	}()
+	func() {
+		// Test when error is not io.EOF
+		err := errors.New("some other error")
+		isEOF, err := IsEndOfFile(err)
+
+		if isEOF {
+			t.Errorf("Expected IsEndOfFile to return false for non-io.EOF error, but got true")
+		}
+
+		if err == nil {
+			t.Errorf("Expected error to be non-nil for non-io.EOF error, but got %v", err)
+		}
+	}()
+}
