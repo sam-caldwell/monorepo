a9772bfdfd606359b447f6d0fb2b0bf56f921c34
Sam Caldwell <github@samcaldwell.net>
2023-11-06 01:10:38 -0600

    add ReadBytes() and working tests

---
+++ go/bitfile/ReadBytes.go
@@ -0,0 +1,17 @@
+package bitfile
+
+/*
+ * CRSCE ReadBytes
+ * (c) 2023 Sam Caldwell.  All Rights Reserved.
+ *
+ * ReadBytes() will read a block of bytes from a given file
+ * and return a Block struct from which individual bits can be read
+ * or other block operations can be performed.
+ */
+
+// ReadBytes - Read a sequence of n bytes from the current file and return them as a Block.
+func (o *BitFile) ReadBytes(n int) (blk Block, err error) {
+	blk.buffer = make([]byte, n)
+	_, err = o.file.Read(blk.buffer)
+	return blk, err
+}
