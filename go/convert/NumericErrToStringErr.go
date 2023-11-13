b3f039dfe1d34c606ae4219c7d4184cfcbd27f9c
Sam Caldwell <github@samcaldwell.net>
2023-11-13 15:20:46 -0600

    Split out NumericType for modularity

--- go/convert/NumericErrToStringErr.go
+++ go/convert/NumericErrToStringErr.go
@@ -2,10 +2,6 @@ package convert
 
 import "fmt"
 
-type NumericType interface {
-	byte | int | int8 | int16 | int32 | int64 | uint | uint16 | uint32 | uint64 | float32 | float64
-}
-
 // NumericErrToStringErr - given a numeric type and error, return its string, error.  (useful as a wrapper)
 func NumericErrToStringErr[V NumericType](n V, err error) (string, error) {
 	return fmt.Sprintf("%v", n), err
---
+++ go/convert/NumericType.go
@@ -0,0 +1,5 @@
+package convert
+
+type NumericType interface {
+	byte | int | int8 | int16 | int32 | int64 | uint | uint16 | uint32 | uint64 | float32 | float64
+}
