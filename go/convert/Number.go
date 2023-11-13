44ed53e3b080adf04242554deb8956334c2af5f8
Sam Caldwell <github@samcaldwell.net>
2023-11-13 16:14:39 -0600

    change NumericType to Number because the first sounds more nerd than any nerd should nerd.

--- go/convert/Number.go
+++ go/convert/Number.go
@@ -1,5 +1,5 @@
 package convert
 
-type NumericType interface {
+type Number interface {
 	byte | int | int8 | int16 | int32 | int64 | uint | uint16 | uint32 | uint64 | float32 | float64
 }
--- go/convert/NumericErrToStringErr.go
+++ go/convert/NumericErrToStringErr.go
@@ -3,6 +3,6 @@ package convert
 import "fmt"
 
 // NumericErrToStringErr - given a numeric type and error, return its string, error.  (useful as a wrapper)
-func NumericErrToStringErr[V NumericType](n V, err error) (string, error) {
+func NumericErrToStringErr[V Number](n V, err error) (string, error) {
 	return fmt.Sprintf("%v", n), err
 }
