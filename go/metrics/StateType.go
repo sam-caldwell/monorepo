94cd400c9f893f51939c9a86912651c095315021
Sam Caldwell <github@samcaldwell.net>
2023-11-18 07:45:56 -0600

    Fix up StateType and add tests (working)

--- go/metrics/StateType.go
+++ go/metrics/StateType.go
@@ -8,7 +8,7 @@ import (
 // StateType - Define a generic StateType interface for use with StateMetric
 // A StateType is essentially some type we can use to store a fixed state (e.g. a string).
 type StateType interface {
-	[]byte | //arbitrary byte array
+	[]byte |
 		string |
 		hashes2.Sha1 |
 		hashes2.Sha256 |
@@ -16,10 +16,16 @@ type StateType interface {
 		generic.Block1KB |
 		generic.Block2KB |
 		generic.Block4KB |
-		generic.Block8KB
+		generic.Block8KB |
+		generic.Block16KB
 }
 type StateTypeMethods[T StateType] interface {
+	// FromSlice - Given a byte array, store the state internally
 	FromSlice([]byte)
+	// ToSlice - Return a byte array from internal state
 	ToSlice() []byte
+	// SizeOf - Return the size of the internal structure
 	SizeOf() uint
+	// String - Return a string representation of the internal state
+	String() string
 }
--- go/metrics/StateType_test.go
+++ go/metrics/StateType_test.go
@@ -9,30 +9,37 @@ import (
 type MockStateType int
 
 func (m MockStateType) FromSlice([]byte) {}
-func (m MockStateType) ToSlice() []byte  { return nil }
-func (m MockStateType) SizeOf() uint     { return 0 }
+func (m MockStateType) ToSlice() []byte  { return []byte("test slice") }
+func (m MockStateType) SizeOf() uint     { return 10 }
+func (m MockStateType) String() string   { return "test string" }
 
 func TestStateMetric_StateType_struct(t *testing.T) {
 
 	t.Run("test interface structure to make sure the abstract stuff works", func(t *testing.T) {
 		mockState := MockStateType(42)
 
-		// Test FromSlice method
-		mockState.FromSlice([]byte{1, 2, 3})
-		// You can add assertions here to check the expected behavior of the FromSlice method
-
 		// Test ToSlice method
 		result := mockState.ToSlice()
-		var expectedResult []byte // replace with the expected result
+		var expectedResult = []byte("test slice") // replace with the expected result
 		if !reflect.DeepEqual(result, expectedResult) {
 			t.Errorf("ToSlice() = %v, want %v", result, expectedResult)
 		}
 
+		// Test FromSlice method
+		mockState.FromSlice([]byte{1, 2, 3})
+		// You can add assertions here to check the expected behavior of the FromSlice method
+
 		// Test SizeOf method
 		size := mockState.SizeOf()
-		expectedSize := uint(0) // replace with the expected size
+		expectedSize := uint(10) // replace with the expected size
 		if size != expectedSize {
 			t.Errorf("SizeOf() = %d, want %d", size, expectedSize)
 		}
+
+		expectedString := "test string"
+		if actual := mockState.String(); actual != expectedString {
+			t.Errorf("String() failed")
+		}
+
 	})
 }
