ee430cb2110b5ebf39f67ecbc227ceaa3e443352
Sam Caldwell <github@samcaldwell.net>
2023-11-17 18:33:33 -0600

    latest on MetricStore (WIP)

--- go/metrics/MetricStore.go
+++ go/metrics/MetricStore.go
@@ -7,6 +7,11 @@ import (
 // MetricStore - A structure for organizing a collection of metrics in a thread safe way that minimizes GC
 type MetricStore struct {
 	lock sync.Mutex
-	err  []error
+
+	// A set of accumulated errors between checks
+	// This allows error checking to be deferred when methods are chained.
+	err []error
+
+	// This map stores our metric collector objects by name
 	data map[string]any
 }
