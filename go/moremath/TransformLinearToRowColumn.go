077bb62962029d67311c6d0d7f47d99cda08bad6
Sam Caldwell <github@samcaldwell.net>
2023-11-20 13:12:33 -0600

    TransformLinearToRowColumn() fixed.

--- go/moremath/TransformLinearToRowColumn.go
+++ go/moremath/TransformLinearToRowColumn.go
@@ -7,17 +7,20 @@ package moremath
  * Transform a linear address (i) to th 2D coordinate pair (r,c)
  * assuming a square matrix of (s x s) elements.
  */
-import "fmt"
+import (
+	"fmt"
+	"github.com/sam-caldwell/monorepo/go/exit/errors"
+)
 
 // TransformLinearToRowColumn - Transform linear address (i) to row (r), column (c) given matrix size (s)
 func TransformLinearToRowColumn(i int, s int) (row, column int, err error) {
 	if i < 0 {
-		return 0, 0, fmt.Errorf("bounds check error")
+		return 0, 0, fmt.Errorf(errors.BoundsCheckError)
 	}
 	if s <= 0 {
-		return 0, 0, fmt.Errorf("array size error")
+		return 0, 0, fmt.Errorf(errors.ArraySizeError)
 	}
-	row = i % s
-	column = i / s
+	row = i / s
+	column = i % s
 	return row, column, nil
 }
--- go/moremath/TransformLinearToRowColumn_test.go
+++ go/moremath/TransformLinearToRowColumn_test.go
@@ -1,8 +1,8 @@
 package moremath
 
 import (
-	"errors"
 	"fmt"
+	"github.com/sam-caldwell/monorepo/go/exit/errors"
 	"testing"
 )
 
@@ -16,19 +16,36 @@ import (
 
 func TestTransformLinearToRowColumn(t *testing.T) {
 	test := func(i, size, expectedRow, expectedColumn int, expectedError error) {
-		row, column, err := TransformLinearToRowColumn(0, 1024)
-		if !errors.Is(err, expectedError) {
-			t.Fatalf("unexpexted error (a: %v,e: %v", err, expectedError)
-		}
-		if row != expectedRow {
-			t.Fatalf("row is wrong (a: %d, e: %d)", row, expectedRow)
-		}
-		if column != expectedColumn {
-			t.Fatalf("column is wrong (a: %d, e: %d)", column, expectedColumn)
-		}
+		t.Run(fmt.Sprintf("Test Run %d %d %d %d", i, size, expectedRow, expectedColumn), func(t *testing.T) {
+			row, column, err := TransformLinearToRowColumn(i, size)
+			if err == nil {
+				if expectedError != nil {
+					t.Fatalf("expected nil but got %v", err)
+				}
+			} else {
+				if err.Error() != expectedError.Error() {
+					t.Fatalf("expected error not found\n"+
+						"actual: %v\n"+
+						"expected: %v",
+						err.Error(), expectedError.Error())
+				}
+			}
+			if row != expectedRow {
+				t.Fatalf("row is wrong\n"+
+					"i:%d, size: %d\n"+
+					"actual:   (%d,%d)\n"+
+					"expected: (%d,%d)", i, size, row, column, expectedRow, expectedColumn)
+			}
+			if column != expectedColumn {
+				t.Fatalf("column is wrong\n"+
+					"actual:   (%d,%d)\n"+
+					"expected: (%d,%d)", row, column, expectedRow, expectedColumn)
+			}
+		})
 	}
-	test(-1, 1024, 0, 0, fmt.Errorf("bounds check error"))
-	test(0, -1, 0, 0, fmt.Errorf("array size error"))
+
+	test(-1, 1024, 0, 0, fmt.Errorf(errors.BoundsCheckError))
+	test(0, -1, 0, 0, fmt.Errorf(errors.ArraySizeError))
 	test(0, 1024, 0, 0, nil)
 	test(1, 1024, 0, 1, nil)
 	test(2, 1024, 0, 2, nil)
