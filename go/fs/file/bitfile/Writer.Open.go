6efc35cf166ae0e67e1ef2eb69ed9b379486f344
Sam Caldwell <github@samcaldwell.net>
2023-11-20 17:03:30 -0600

    Fixes for bitfile Writer.Open() and tests

--- go/fs/file/bitfile/Writer.Open.go
+++ go/fs/file/bitfile/Writer.Open.go
@@ -13,10 +13,26 @@ import (
 
 // Open - Open/create a file.  If the file exists, truncate it and open for write.
 // The file mode (perms) will be set to 0666.
-func (o *Writer) Open(fileName *string) (err error) {
+func (o *Writer) Open(fileName *string, bufferSize int) (err error) {
 
+	// Make sure we don't do anything insane.
+	if fileName == nil {
+		panic("nil filename encountered by bitfile Writer")
+	}
+
+	// Revert to the defaultBufferSize
+	if bufferSize == 0 {
+		bufferSize = defaultBufferSize
+	}
+
+	// Reset all internal state
+	o.bufferPos = 0
+	o.bitPos = 0
+
+	// Create the buffer
+	o.buffer = make([]byte, bufferSize)
+
+	// Create/truncate the buffer file.
 	o.file, err = os.Create(*fileName)
-
 	return err
-
 }
--- go/fs/file/bitfile/Writer.Open_test.go
+++ go/fs/file/bitfile/Writer.Open_test.go
@@ -8,28 +8,70 @@ package bitfile
  */
 
 import (
-	"github.com/sam-caldwell/monorepo/go/fs/file"
 	"os"
 	"testing"
 )
 
 func TestWriter_Open(t *testing.T) {
+
 	var b Writer
-	tempFileName := "/tmp/TestBitFile_Create.txt"
-	if err := b.Open(&tempFileName); err != nil {
-		t.Fatal(err)
-	}
-	if file.Exists(tempFileName) {
-		if err := os.Remove(tempFileName); err != nil {
+	testFileName := "/tmp/TestWriter_Open.txt"
+
+	t.Cleanup(func() {
+		if b.file == nil {
+			return
+		}
+		if err := b.file.Close(); err != nil {
+			t.Fatalf("close failed: %v", err)
+		}
+		if err := os.Remove(testFileName); err != nil {
+			t.Fatalf("file remove failed: %v", err)
+		}
+	})
+
+	t.Run("Open the test file (after making sure it doesn't exist)", func(t *testing.T) {
+		_ = os.Remove(testFileName)
+		if err := b.Open(&testFileName, 0); err != nil {
 			t.Fatal(err)
 		}
-	}
-	defer func() {
-		if err := b.Close(); err != nil {
+	})
+	t.Run("Create test file and close it.  Then Open with the Writer.", func(t *testing.T) {
+		var err error
+		var f *os.File
+		if f, err = os.Create(testFileName); err != nil {
 			t.Fatal(err)
 		}
-		if err := os.Remove(tempFileName); err != nil {
+		if err = f.Close(); err != nil {
 			t.Fatal(err)
 		}
-	}()
+		if err := b.Open(&testFileName, 0); err != nil {
+			t.Fatal(err)
+		}
+	})
+	t.Run("Create test file, write data, close it. Then open and verify truncation", func(t *testing.T) {
+		var err error
+		var f *os.File
+		_ = os.Remove(testFileName)
+		if f, err = os.Create(testFileName); err != nil {
+			t.Fatal(err)
+		}
+		if err = f.Close(); err != nil {
+			t.Fatal(err)
+		}
+		if err := b.Open(&testFileName, 0); err != nil {
+			t.Fatal(err)
+		}
+		var n int
+		var data []byte
+		if _, err = b.file.Seek(0, 0); err != nil {
+			t.Fatalf("failed to seec position 0,0: %v", err)
+		}
+		if n, err = b.file.Read(data); err != nil {
+			t.Fatalf("failed to read. %v", err)
+		}
+		if len(data) != 0 || n != 0 {
+			t.Fatal("expected 0 bytes read")
+		}
+
+	})
 }
