432f95492ad4fc79f6e41134415e34b378463e77
Sam Caldwell <github@samcaldwell.net>
2023-11-21 09:23:43 -0600

    code cleanup

--- go/fs/file/bitfile/Writer.WriteBit.go
+++ go/fs/file/bitfile/Writer.WriteBit.go
@@ -4,7 +4,6 @@ package bitfile
  * bitfile Writer.WriteBit() method
  * (c) 2023 Sam Caldwell.  See LICENSE.txt
  *
- * Write a single bit to the buffer and ultimately to the target file.
  */
 
 // WriteBit - Write a given bit to the buffer and when all bits are buffered, push to file.
@@ -15,15 +14,11 @@ func (o *Writer) WriteBit(pos, inputValue byte) (err error) {
 	var readMask = byte(1 << pos)
 	// determine the bit value and store it.
 	if bit := readMask&inputValue != 0; bit {
-		//ansi.Blue().Printf("Set bit %d (mask %08b): %08b", pos, readMask, o.buffer[o.bufferPos]).LF().Reset()
 		o.bitPos = pos
 		o.setBit()
-		//ansi.Green().Printf("Set bit %d (mask %08b): %08b", pos, readMask, o.buffer[o.bufferPos]).LF().Reset()
 	} else {
-		//ansi.Blue().Printf("Clear bit %d (mask %08b): %08b", pos, readMask, o.buffer[o.bufferPos]).LF().Reset()
 		o.bitPos = pos
 		o.clearBit()
-		//ansi.Red().Printf("Clear bit %d (mask %08b): %08b", pos, readMask, o.buffer[o.bufferPos]).LF().Reset()
 	}
 	o.bitPos++
 	return nil
