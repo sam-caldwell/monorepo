ca6556059ffca8554bb5ad64a3a6032770d3bfa0
Sam Caldwell <github@samcaldwell.net>
2023-11-25 16:53:16 -0600

    improve db write performance

--- go/types/hashes/tools/findCollisions/generatePrecalculated.go
+++ go/types/hashes/tools/findCollisions/generatePrecalculated.go
@@ -89,9 +89,9 @@ func main() {
 			storeStart := time.Now()
 			if _, err := db.Exec(`
                 INSERT INTO hashes (h) VALUES ($1),($2),($3),($4),($1),($2),($3),($4),
-                                              ($1),($2),($3),($4),($1),($2),($3),($4),
-                                              ($1),($2),($3),($4),($1),($2),($3),($4),
-                                              ($1),($2),($3),($4),($1),($2),($3),($4);
+                                              ($5),($6),($7),($8),($9),($10),($11),($12),
+                                              ($13),($14),($15),($16),($17),($18),($19),($20),
+                                              ($21),($22),($23),($24),($25),($26),($27),($28);
                 `, <-queue, <-queue, <-queue, <-queue, <-queue, <-queue, <-queue, <-queue,
 				<-queue, <-queue, <-queue, <-queue, <-queue, <-queue, <-queue, <-queue,
 				<-queue, <-queue, <-queue, <-queue, <-queue, <-queue, <-queue, <-queue,
