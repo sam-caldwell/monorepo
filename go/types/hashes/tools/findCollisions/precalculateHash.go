11faa52ea576672df63f4684c7b38f0ed561393c
Sam Caldwell <github@samcaldwell.net>
2023-11-25 18:35:24 -0600

    update precalculateHash

--- go/types/hashes/tools/findCollisions/precalculateHash.go
+++ go/types/hashes/tools/findCollisions/precalculateHash.go
@@ -1,67 +1,68 @@
 package main
 
 import (
-	"encoding/hex"
-	"flag"
-	"github.com/sam-caldwell/monorepo/go/counters"
-	"github.com/sam-caldwell/monorepo/go/types/hashes"
-	"log"
-	"os"
-	"time"
+    "encoding/hex"
+    "flag"
+    "github.com/sam-caldwell/monorepo/go/counters"
+    "github.com/sam-caldwell/monorepo/go/types/hashes"
+    "log"
+    "os"
+    "time"
 )
 
 const (
-	keySpaceSz = 1024
-	TableSize  = 256 * 256 * 256 * 256 //Table Size: 256 * 256 * 256 * 256 * 20 / 1048576 MB
+    keySpaceSz = 1024
+    TableSize  = 256 * 256 * 256 * 256 //Table Size: 256 * 256 * 256 * 256 * 20 / 1048576 MB
 )
 
 func main() {
-	var genCount int
+    var genCount int
 
-	hashFile := flag.String("HashFile", "", "Pre-computed hashes")
-	flag.Parse()
-	if *hashFile == "" {
-		log.Fatalf("hashfile required")
-	}
+    hashFile := flag.String("file", "hashes.txt", "Pre-computed hashes")
+    flag.Parse()
+    log.Printf("create hash file: %v", *hashFile)
 
-	fh, err := os.Create(*hashFile)
-	if err != nil {
-		log.Fatalf("failed to create file: %v", err)
-	}
+    if *hashFile == "" {
+        log.Fatalf("hashfile required")
+    }
 
-	defer func() { _ = fh.Close() }()
+    fh, err := os.Create(*hashFile)
+    if err != nil {
+        log.Fatalf("failed to create file: %v", err)
+    }
 
-	var table [TableSize]hashes.Sha1
-	//table := make([]hashes.Sha1, TableSize)
-	c, _ := counters.NewByteCounter(keySpaceSz)
+    defer func() { _ = fh.Close() }()
 
-	go func() {
-		startTime := time.Now()
-		t := time.NewTicker(1 * time.Second)
-		defer t.Stop()
-		for {
-			select {
-			case <-t.C:
-				elapsed := time.Since(startTime)
-				gProgress := 100 * float64(genCount) / float64(TableSize)
-				gOps := float64(genCount) / float64(elapsed.Nanoseconds())
-				log.Printf("generating %d / %d (%8.2f %%) %8.2f (elapsed: %vs)",
-					genCount, len(table), gProgress, gOps, time.Since(startTime).Seconds())
-			}
-		}
-	}()
+    table := make([]hashes.Sha1, TableSize)
+    c, _ := counters.NewByteCounter(keySpaceSz)
 
-	for i := 0; i < TableSize; i++ {
-		table[i] = c.Sha1Bytes()
-		_ = c.FastIncrement()
-		genCount++
-	}
+    go func() {
+        startTime := time.Now()
+        t := time.NewTicker(1 * time.Second)
+        defer t.Stop()
+        for {
+            select {
+            case <-t.C:
+                elapsed := time.Since(startTime)
+                gProgress := 100 * float64(genCount) / float64(TableSize)
+                gOps := float64(genCount) / float64(elapsed.Nanoseconds())
+                log.Printf("generating %d / %d (%8.2f %%) %8.2f (elapsed: %vs)",
+                    genCount, len(table), gProgress, gOps, time.Since(startTime).Seconds())
+            }
+        }
+    }()
 
-	for _, hash := range table {
-		_, err := fh.WriteString(hex.EncodeToString(hash[:]))
-		if err != nil {
-			panic(err)
-		}
-	}
+    for i := 0; i < TableSize; i++ {
+        table[i] = c.Sha1Bytes()
+        _ = c.FastIncrement()
+        genCount++
+    }
+
+    for _, hash := range table {
+        _, err := fh.WriteString(hex.EncodeToString(hash[:]))
+        if err != nil {
+            panic(err)
+        }
+    }
 
 }
