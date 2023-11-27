34b427112aefa0a464b33fac42f2fb16fd8bdcc2
Sam Caldwell <github@samcaldwell.net>
2023-11-26 18:13:52 -0600

    add logging for writes to disk so we aren't blind during this phase.

--- go/types/hashes/tools/findCollisions/precalculateHash.go
+++ go/types/hashes/tools/findCollisions/precalculateHash.go
@@ -1,68 +1,76 @@
 package main
 
 import (
-    "encoding/hex"
-    "flag"
-    "github.com/sam-caldwell/monorepo/go/counters"
-    "github.com/sam-caldwell/monorepo/go/types/hashes"
-    "log"
-    "os"
-    "time"
+	"bytes"
+	"flag"
+	"github.com/sam-caldwell/monorepo/go/counters"
+	"github.com/sam-caldwell/monorepo/go/types/hashes"
+	"log"
+	"os"
+	"slices"
+	"time"
 )
 
 const (
-    keySpaceSz = 1024
-    TableSize  = 256 * 256 * 256 * 256 //Table Size: 256 * 256 * 256 * 256 * 20 / 1048576 MB
+	keySpaceSz = 1024
+	TableSize  = 256 * 256 * 256 * 256 //Table Size: 256 * 256 * 256 * 256 * 20 / 1048576 MB
 )
 
 func main() {
-    var genCount int
+	var genCount int
+	var writeCount int
 
-    hashFile := flag.String("file", "hashes.txt", "Pre-computed hashes")
-    flag.Parse()
-    log.Printf("create hash file: %v", *hashFile)
+	hashFile := flag.String("file", "hashes.txt", "Pre-computed hashes")
+	flag.Parse()
+	log.Printf("create hash file: %v", *hashFile)
 
-    if *hashFile == "" {
-        log.Fatalf("hashfile required")
-    }
+	if *hashFile == "" {
+		log.Fatalf("hashfile required")
+	}
 
-    fh, err := os.Create(*hashFile)
-    if err != nil {
-        log.Fatalf("failed to create file: %v", err)
-    }
+	fh, err := os.Create(*hashFile)
+	if err != nil {
+		log.Fatalf("failed to create file: %v", err)
+	}
 
-    defer func() { _ = fh.Close() }()
+	defer func() { _ = fh.Close() }()
 
-    table := make([]hashes.Sha1, TableSize)
-    c, _ := counters.NewByteCounter(keySpaceSz)
+	table := make([]hashes.Sha1, TableSize)
+	c, _ := counters.NewByteCounter(keySpaceSz)
 
-    go func() {
-        startTime := time.Now()
-        t := time.NewTicker(1 * time.Second)
-        defer t.Stop()
-        for {
-            select {
-            case <-t.C:
-                elapsed := time.Since(startTime)
-                gProgress := 100 * float64(genCount) / float64(TableSize)
-                gOps := float64(genCount) / float64(elapsed.Nanoseconds())
-                log.Printf("generating %d / %d (%8.2f %%) %8.2f (elapsed: %vs)",
-                    genCount, len(table), gProgress, gOps, time.Since(startTime).Seconds())
-            }
-        }
-    }()
+	go func() {
+		startTime := time.Now()
+		t := time.NewTicker(1 * time.Second)
+		defer t.Stop()
+		for {
+			select {
+			case <-t.C:
+				elapsed := time.Since(startTime)
+				gProgress := 100 * float64(genCount) / float64(TableSize)
+				wProgress := 100 * float64(writeCount) / float64(TableSize)
+				gOps := float64(genCount) / float64(elapsed.Nanoseconds())
+				log.Printf("generating %d / %d (%8.2f %%) %8.2f (elapsed: %vs) writeCount:%d (%8.2f %%)",
+					genCount, len(table), gProgress, gOps, time.Since(startTime).Seconds(), writeCount, wProgress)
+			}
+		}
+	}()
 
-    for i := 0; i < TableSize; i++ {
-        table[i] = c.Sha1Bytes()
-        _ = c.FastIncrement()
-        genCount++
-    }
+	for i := 0; i < TableSize; i++ {
+		table[i] = c.Sha1Bytes()
+		_ = c.FastIncrement()
+		genCount++
+	}
 
-    for _, hash := range table {
-        _, err := fh.WriteString(hex.EncodeToString(hash[:]))
-        if err != nil {
-            panic(err)
-        }
-    }
+	slices.SortFunc(table[:], func(a, b hashes.Sha1) int {
+		return bytes.Compare(a[:], b[:])
+	})
+
+	for _, hash := range table {
+		//_, err := fh.WriteString(hex.EncodeToString(hash[:]))
+		_, err := fh.Write(hash[:])
+		if err != nil {
+			panic(err)
+		}
+	}
 
 }
