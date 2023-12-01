19454d5eb492d57e7a8cb8dfb744d6489af2dc07
Sam Caldwell <github@samcaldwell.net>
2023-12-01 12:47:15 -0600

    add getAvatarById() and getIconsById() SQL functions

---
+++ sql/databases/tracker/0100-func-getAvatarById.sql
@@ -0,0 +1,19 @@
+/*
+ * 0100-func-getAvatarById.sql
+ * (c) 2023 Sam Caldwell.  See License.txt
+ */
+create or replace function getAvatarById(id uuid) returns jsonb as
+$$
+declare
+    result jsonb;
+begin
+    select jsonb_agg(jsonb_build_object(
+            'id', id,
+            'url',url,
+        )) as avatars
+    into result
+    from avatars
+    where id == id;
+    return result;
+end;
+$$ language plpgsql;
---
+++ sql/databases/tracker/0110-func-getIconsById.sql
@@ -0,0 +1,19 @@
+/*
+ * 0110-func-getIconsById.sql
+ * (c) 2023 Sam Caldwell.  See License.txt
+ */
+create or replace function getIconsById(id uuid) returns jsonb as
+$$
+declare
+    result jsonb;
+begin
+    select jsonb_agg(jsonb_build_object(
+            'id', id,
+            'url',url,
+        )) as icons
+    into result
+    from icons
+    where id == id;
+    return result;
+end;
+$$ language plpgsql;
