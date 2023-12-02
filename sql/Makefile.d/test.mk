a73171938b6779c6be71bbdb9d857ccda2ee05ed
Sam Caldwell <github@samcaldwell.net>
2023-12-02 17:14:06 -0600

    add makefile for sql testing

---
+++ sql/Makefile.d/test.mk
@@ -0,0 +1,13 @@
+# sql/Makefile.d/test.mk
+# (c) 2023 Sam Caldwell.  See License.txt
+#
+test/sql:
+	@echo ${GREEN}"start $@"${RESET}
+	@db_host=192.168.3.190 \
+	db_port=5432 \
+	db_name=tracker \
+	db_user=admin \
+	db_pass=admin  \
+	use_tls=false go test -v -failfast ./sql/databases/...
+	@echo ${GREEN}"completed $@"${RESET}
+
