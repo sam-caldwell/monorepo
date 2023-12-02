5257ce9c1ebd4e1d54d3bec70263947c6a81a92a
Sam Caldwell <github@samcaldwell.net>
2023-12-01 23:26:44 -0600

    add unit tests for tracker db schema

---
+++ sql/databases/tracker/0000-table-avatars_test.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"github.com/sam-caldwell/monorepo/sql/tools"
+	"testing"
+)
+
+func TestSqlDbTable_Avatars(t *testing.T) {
+	const tableName = "avatars"
+	db := database.InitializeTestDbConn(t)
+
+	t.Cleanup(func() {
+		err := db.Close()
+		database.CheckError(t, err)
+	})
+
+	t.Run("query the avatars table", func(t *testing.T) {
+		_, err := db.Query("select id, url from avatars limit 1;")
+		database.CheckError(t, err)
+	})
+
+	t.Run("check avatars table schema", func(t *testing.T) {
+		expectedColumns := []database.Record{
+			{"id", "uuid", "NO", "gen_random_uuid()"},
+			{"url", "text", "YES", "<<null>>"},
+		}
+		actualColumns := database.GetTableColumns(t, db, tableName)
+		database.CompareTableColumns(t, actualColumns, expectedColumns)
+	})
+}
