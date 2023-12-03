d9f5db89fed23d076daa4044b55dc947e6e5869e
Sam Caldwell <github@samcaldwell.net>
2023-12-03 12:41:29 -0600

    starting sql unit tests for function validation

---
+++ sql/databases/tracker/0100-func-deleteAvatar_test.go
@@ -0,0 +1,41 @@
+package database
+
+import (
+	"fmt"
+	"github.com/sam-caldwell/monorepo/sql/tools"
+	"strings"
+	"testing"
+)
+
+func TestSqlDbFunc_deleteAvatar(t *testing.T) {
+
+	const (
+		tableName    = "avatar"
+		functionName = "deleteAvatar"
+		testUrl      = "http://testUrlButNeverReal.tld/thisShouldNeverExistInTheDb.png"
+	)
+	db := database.InitializeTestDbConn(t)
+
+	t.Cleanup(func() {
+		// Note: we only clean up the avatar we expect to have created.
+		//       this should safeguard against an accidental run on prod.
+		_, _ = db.Query(fmt.Sprintf("delete from %s where url='%s';", tableName, testUrl))
+
+		err := db.Close()
+		database.CheckError(t, err)
+	})
+
+	t.Run("verify the function structure (params, return)", func(t *testing.T) {
+		database.VerifyFunctionStructure(t, db,
+			strings.ToLower(functionName),
+			fmt.Sprintf("fn:%s,"+
+				"pn:{avatarid},"+
+				"pt:{uuid},"+
+				"rt:int4", strings.ToLower(functionName)))
+	})
+
+	t.Run("happy path:run the function", func(t *testing.T) {
+		t.Skip("not implemented")
+	})
+
+}
---
+++ sql/tools/VerifyFunctionStructure.go
@@ -0,0 +1,44 @@
+package database
+
+import (
+	"fmt"
+	Postgres "github.com/sam-caldwell/monorepo/go/db/postgres"
+	"strings"
+	"testing"
+)
+
+func VerifyFunctionStructure(t *testing.T, db *Postgres.Db, functionName string, expected string) {
+
+	query := fmt.Sprintf("SELECT "+
+		"proname AS fn, "+
+		"proargnames AS pn, "+
+		"("+
+		"SELECT array_agg(typname) "+
+		"FROM pg_type "+
+		"WHERE oid = ANY(proargtypes)"+
+		") AS pt, "+
+		"("+
+		"SELECT typname "+
+		"FROM pg_type "+
+		"WHERE oid = prorettype "+
+		") AS rt "+
+		"FROM pg_proc "+
+		"WHERE proname = lower('%s')", functionName)
+
+	rows, err := db.Query(query)
+	CheckError(t, err)
+	defer func() {
+		CheckError(t, rows.Close())
+	}()
+	var fn, pn, pt, rt string
+	_ = rows.Next()
+	CheckError(t, rows.Scan(&fn, &pn, &pt, &rt))
+	actual := fmt.Sprintf("fn:%s,pn:%s,pt:%s,rt:%s", fn, pn, pt, rt)
+	if actual != strings.ToLower(expected) {
+		t.Fatalf("function structure mismatch\n"+
+			"actual:   %s\n"+
+			"expected: %s",
+			actual,
+			strings.ToLower(expected))
+	}
+}
