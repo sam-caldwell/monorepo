1e91784d78d21fb1e061456f254e63c9e1bdfa49
Sam Caldwell <github@samcaldwell.net>
2023-12-02 18:56:47 -0600

    add more sql unit tests

---
+++ sql/databases/tracker/0002-table-numericProperties_test.go
@@ -0,0 +1,31 @@
+package database
+
+import (
+	"fmt"
+	"github.com/sam-caldwell/monorepo/sql/tools"
+	"testing"
+)
+
+func TestSqlDbTable_numericProperties(t *testing.T) {
+	const tableName = "numericproperties"
+	db := database.InitializeTestDbConn(t)
+
+	t.Cleanup(func() {
+		err := db.Close()
+		database.CheckError(t, err)
+	})
+
+	t.Run("query the table (verifies permissions of user and existence of table)", func(t *testing.T) {
+		_, err := db.Query(fmt.Sprintf("select 1 from %s limit 1;", tableName))
+		database.CheckError(t, err)
+	})
+
+	t.Run("check table schema", func(t *testing.T) {
+		expectedColumns := []string{
+			"ColumnName:id,DataType:uuid,IsNullable:NO,ColumnDefault:<<null>>",
+			"ColumnName:value,DataType:integer,IsNullable:NO,ColumnDefault:<<null>>",
+		}
+		actualColumns := database.GetTableColumns(t, db, tableName)
+		database.CompareTwoStringLists(t, actualColumns, expectedColumns)
+	})
+}
