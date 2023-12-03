306b2dfe0ad96c6c7cd8b70e1aa1cd4f2be76fe1
Sam Caldwell <github@samcaldwell.net>
2023-12-03 00:01:09 -0600

    fix fk test for ticketTypes and add project unit tests in sql tracker db

--- sql/databases/tracker/0004-table-projects_test.go
+++ sql/databases/tracker/0004-table-projects_test.go
@@ -6,8 +6,8 @@ import (
 	"testing"
 )
 
-func TestSqlDbTable_TicketTypes(t *testing.T) {
-	const tableName = "tickettypes"
+func TestSqlDbTable_Projects(t *testing.T) {
+	const tableName = "projects"
 
 	db := database.InitializeTestDbConn(t)
 
@@ -26,7 +26,12 @@ func TestSqlDbTable_TicketTypes(t *testing.T) {
 			"ColumnName:id,datatype:uuid,IsNullable:no,ColumnDefault:gen_random_uuid()",
 			"ColumnName:name,datatype:character varying,IsNullable:no,ColumnDefault:<<null>>",
 			"ColumnName:iconId,datatype:uuid,IsNullable:no,ColumnDefault:<<null>>",
-			"ColumnName:workflowId,datatype:uuid,IsNullable:no,ColumnDefault:<<null>>",
+			"ColumnName:ownerId,datatype:uuid,IsNullable:no,ColumnDefault:<<null>>",
+			"ColumnName:teamId,datatype:uuid,IsNullable:no,ColumnDefault:<<null>>",
+			"ColumnName:owner,datatype:user-defined,IsNullable:no,ColumnDefault:'delete'::permissions",
+			"ColumnName:team,datatype:user-defined,IsNullable:no,ColumnDefault:'none'::permissions",
+			"ColumnName:everyone,datatype:user-defined,IsNullable:no,ColumnDefault:'none'::permissions",
+			"ColumnName:defaultTicketType,datatype:uuid,IsNullable:no,ColumnDefault:<<null>>",
 			"ColumnName:description,datatype:text,IsNullable:yes,ColumnDefault:<<null>>",
 		}
 		actualColumns := database.GetTableColumns(t, db, tableName)
@@ -34,7 +39,9 @@ func TestSqlDbTable_TicketTypes(t *testing.T) {
 	})
 
 	t.Run("check foreign keys", func(t *testing.T) {
+		database.ValidateForeignKey(t, db, tableName, "users", "ownerId", "id")
+		database.ValidateForeignKey(t, db, tableName, "teams", "teamId", "id")
 		database.ValidateForeignKey(t, db, tableName, "icons", "iconId", "id")
-		database.ValidateForeignKey(t, db, tableName, "workflow", "workflowId", "id")
+		database.ValidateForeignKey(t, db, tableName, "ticketTypes", "defaultTicketType", "id")
 	})
 }
