f96e858746a57d3742610b6e5cc841ab2d9950fb
Sam Caldwell <github@samcaldwell.net>
2023-12-02 18:31:22 -0600

    add test for 0001-table-users.sql and make the test failure more useful

---
+++ sql/databases/tracker/0001-table-users_test.go
@@ -0,0 +1,36 @@
+package database
+
+import (
+	"fmt"
+	"github.com/sam-caldwell/monorepo/sql/tools"
+	"testing"
+)
+
+func TestSqlDbTable_users(t *testing.T) {
+	const tableName = "users"
+	db := database.InitializeTestDbConn(t)
+
+	t.Cleanup(func() {
+		err := db.Close()
+		database.CheckError(t, err)
+	})
+
+	t.Run("query the table (verifies permissions of user and existence of table)", func(t *testing.T) {
+		_, err := db.Query(fmt.Sprintf("select 1 from %s limit 1;", tableName))
+		database.CheckError(t, err)
+	})
+
+	t.Run("check table schema", func(t *testing.T) {
+		expectedColumns := []string{
+			"ColumnName:id,DataType:uuid,IsNullable:NO,ColumnDefault:gen_random_uuid()",
+			"ColumnName:description,DataType:text,IsNullable:yes,ColumnDefault:<<null>>",
+			"ColumnName:email,DataType:character varying,IsNullable:no,ColumnDefault:<<null>>",
+			"ColumnName:firstName,DataType:character varying,IsNullable:no,ColumnDefault:<<null>>",
+			"ColumnName:lastName,DataType:character varying,IsNullable:no,ColumnDefault:<<null>>",
+			"ColumnName:avatarId,DataType:uuid,IsNullable:no,ColumnDefault:<<null>>",
+			"ColumnName:phonenumber,DataType:character varying,IsNullable:YES,ColumnDefault:<<null>>",
+		}
+		actualColumns := database.GetTableColumns(t, db, tableName)
+		database.CompareTwoStringLists(t, actualColumns, expectedColumns)
+	})
+}
--- sql/tools/CompareTwoStringLists.go
+++ sql/tools/CompareTwoStringLists.go
@@ -1,8 +1,8 @@
 package database
 
 import (
-	"reflect"
 	"sort"
+	"strings"
 	"testing"
 )
 
@@ -10,10 +10,17 @@ import (
 func CompareTwoStringLists(t *testing.T, lhs []string, rhs []string) {
 	sort.Strings(lhs)
 	sort.Strings(rhs)
-	if !reflect.DeepEqual(lhs, rhs) {
-		t.Fatalf("These two lists are not equal\n"+
-			"lhs: %v\n"+
-			"rhs: %v",
-			lhs, rhs)
+	for i := 0; i < len(lhs); i++ {
+		if i >= len(rhs) {
+			t.Fatalf("missing column.\n"+
+				"%d %v", i, lhs[i])
+		}
+		if strings.ToLower(lhs[i]) != strings.ToLower(rhs[i]) {
+			t.Fatalf("These two lists are not equal\n"+
+				"lhs: %v\n"+
+				"rhs: %v",
+				strings.ToLower(lhs[i]),
+				strings.ToLower(rhs[i]))
+		}
 	}
 }
