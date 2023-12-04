ab733dc51ac4a168e58361a0dee4d6e133346da3
Sam Caldwell <github@samcaldwell.net>
2023-12-04 09:06:21 -0600

    fix isValidUrl

---
+++ databases/sql/tracker/0099-func-isValidUrl.sql
@@ -0,0 +1,15 @@
+/*
+ * 0099-func-isValidUrl.sql
+ * (c) 2023 Sam Caldwell.  See License.txt
+ */
+
+create or replace function isValidUrl(url text) returns bool as
+$$
+begin
+    if (select count(*) from regexp_match(url, '^(http|https)?://.*\?.*=.*$')) = 0 then
+        return false;
+    end if;
+    return true;
+end;
+$$
+    language plpgsql;
