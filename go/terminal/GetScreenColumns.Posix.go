93969aa70af68854784456a2215af576b7ddbf74
Sam Caldwell <github@samcaldwell.net>
2023-12-06 21:16:19 -0600

    Fixing go/terminal GetScreenColumns()

--- go/terminal/GetScreenColumns.Posix.go
+++ go/terminal/GetScreenColumns.Posix.go
@@ -3,13 +3,32 @@
 
 package terminal
 
-import "github.com/sam-caldwell/monorepo/go/environment"
+import (
+	"os"
+	"syscall"
+	"unsafe"
+)
 
 const (
 	defaultColumnWidth = 80
 )
 
+type winsize struct {
+	Rows   uint16
+	Cols   uint16
+	Xpixel uint16
+	Ypixel uint16
+}
+
 // GetScreenColumns - Return Screen width of a terminal/console (tty/pty) in columns (Posix version)
 func GetScreenColumns() int {
-	return environment.GetIntOrDefault("COLUMNS", defaultColumnWidth)
+	var ws winsize
+
+	fd := int(os.Stdin.Fd())
+
+	_, _, errno := syscall.Syscall(syscall.SYS_IOCTL, uintptr(fd), syscall.TIOCGWINSZ, uintptr(unsafe.Pointer(&ws)))
+	if errno != 0 {
+		return defaultColumnWidth
+	}
+	return int(ws.Cols)
 }
