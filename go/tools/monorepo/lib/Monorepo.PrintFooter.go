8b307888e9a93552663b49df85dac6bec29cf2b1
Sam Caldwell <github@samcaldwell.net>
2023-12-06 23:02:51 -0600

    subdivide terminal project into terminal/geometry

--- go/tools/monorepo/lib/Monorepo.PrintFooter.go
+++ go/tools/monorepo/lib/Monorepo.PrintFooter.go
@@ -3,16 +3,16 @@ package monorepo
 import (
 	"fmt"
 	"github.com/sam-caldwell/monorepo/go/ansi"
-	"github.com/sam-caldwell/monorepo/go/terminal"
+	"github.com/sam-caldwell/monorepo/go/terminal/geometry"
 	"time"
 )
 
 // PrintFooter - print the footer text in an ascii box
 func (m *Monorepo) PrintFooter(command *string, err error) {
-	var window terminal.TextWindow
+	var window geometry.TextWindow
 	width := window.ColumnSize() - 2
 	if err == nil {
-		terminal.DrawAsciiBox(
+		geometry.DrawAsciiBox(
 			ansi.Green(),
 			[]string{
 				fmt.Sprintf("Success  %s at %v (elapsed: %vs)",
@@ -22,7 +22,7 @@ func (m *Monorepo) PrintFooter(command *string, err error) {
 			},
 			width)
 	} else {
-		terminal.DrawAsciiBox(
+		geometry.DrawAsciiBox(
 			ansi.Red(),
 			[]string{
 				fmt.Sprintf("Failed  %s at %v (elapsed: %vs)",
--- go/tools/monorepo/lib/Monorepo.PrintHeader.go
+++ go/tools/monorepo/lib/Monorepo.PrintHeader.go
@@ -4,15 +4,15 @@ import (
 	"fmt"
 	"github.com/sam-caldwell/monorepo/go/ansi"
 	"github.com/sam-caldwell/monorepo/go/convert"
-	"github.com/sam-caldwell/monorepo/go/terminal"
+	"github.com/sam-caldwell/monorepo/go/terminal/geometry"
 	"time"
 )
 
 // PrintHeader - print the header text in an ascii box
 func (m *Monorepo) PrintHeader(title string) {
-	var window terminal.TextWindow
+	var window geometry.TextWindow
 	width := window.ColumnSize() - 4
-	terminal.DrawAsciiBox(
+	geometry.DrawAsciiBox(
 		ansi.Cyan(),
 		[]string{
 			fmt.Sprintf("%s...  %v",
