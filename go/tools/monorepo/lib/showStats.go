533d13e2a0b8f4eec9acf99f06e43a55d8e56e1d
Sam Caldwell <github@samcaldwell.net>
2023-12-09 23:43:48 -0600

    only show fail count if > 0 so we don't have any red in output

--- go/tools/monorepo/lib/showStats.go
+++ go/tools/monorepo/lib/showStats.go
@@ -6,7 +6,9 @@ func showStats(pass, fail int) {
 	ansi.
 		LF().
 		Cyan().Printf("    Statistics\n").
-		Green().Printf("      pass: %d\n", pass).
-		Red().Printf("      fail: %d\n", fail).
-		LF().Reset()
+		Green().Printf("      pass: %d\n", pass)
+	if fail > 0 {
+		ansi.Red().Printf("      fail: %d\n", fail)
+	}
+	ansi.LF().Reset()
 }
