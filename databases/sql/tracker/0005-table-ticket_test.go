9272863e08876f721bcc5f266e6cf4cd2dfccabb
Sam Caldwell <github@samcaldwell.net>
2023-12-10 13:35:26 -0600

    add unit test for tracker db ticket table

---
+++ databases/sql/tracker/0005-table-ticket_test.go
@@ -0,0 +1,46 @@
+package psqlTrackerDb
+
+import (
+	"github.com/sam-caldwell/monorepo/go/db/sqldbtest"
+	"testing"
+)
+
+func TestSqlDbTable_Ticket(t *testing.T) {
+	const tableName = "ticket"
+
+	db := sqldbtest.InitializeTestDbConn(t)
+
+	t.Cleanup(func() {
+		err := db.Close()
+		sqldbtest.CheckError(t, err)
+	})
+
+	t.Run("query the table", func(t *testing.T) {
+		_, err := db.Query("select 1 from %s limit 1;", tableName)
+		sqldbtest.CheckError(t, err)
+	})
+
+	t.Run("check table schema", func(t *testing.T) {
+		sqldbtest.ValidateTable(t, db, tableName, []string{
+			"ColumnName:id,datatype:uuid,size:-1,IsNullable:no,ColumnDefault:gen_random_uuid()",
+			"ColumnName:projectId,datatype:uuid,size:-1,IsNullable:no,ColumnDefault:<<null>>",
+			"ColumnName:authorUserId,datatype:uuid,size:-1,IsNullable:no,ColumnDefault:<<null>>",
+			"ColumnName:assignedUserId,datatype:uuid,size:-1,IsNullable:no,ColumnDefault:<<null>>",
+			"ColumnName:ticketTypeId,datatype:uuid,size:-1,IsNullable:no,ColumnDefault:<<null>>",
+			"ColumnName:owner,datatype:user-defined,size:-1,IsNullable:no,ColumnDefault:'delete'::permissions",
+			"ColumnName:team,datatype:user-defined,size:-1,IsNullable:no,ColumnDefault:'read'::permissions",
+			"ColumnName:everyone,datatype:user-defined,size:-1,IsNullable:no,ColumnDefault:'read'::permissions",
+			"ColumnName:summary,datatype:character varying,size:2048,IsNullable:no,ColumnDefault:<<null>>",
+			"ColumnName:workflowStepId,datatype:uuid,size:-1,IsNullable:no,ColumnDefault:<<null>>",
+			"ColumnName:description,datatype:text,size:-1,IsNullable:yes,ColumnDefault:<<null>>",
+		})
+	})
+
+	t.Run("check foreign keys", func(t *testing.T) {
+		sqldbtest.ValidateForeignKey(t, db, tableName, "projects", "projectId", "id")
+		sqldbtest.ValidateForeignKey(t, db, tableName, "users", "authorUserId", "id")
+		sqldbtest.ValidateForeignKey(t, db, tableName, "users", "assignedUserId", "id")
+		sqldbtest.ValidateForeignKey(t, db, tableName, "ticketTypes", "ticketTypeId", "id")
+		sqldbtest.ValidateForeignKey(t, db, tableName, "workflowSteps", "workflowStepId", "id")
+	})
+}
