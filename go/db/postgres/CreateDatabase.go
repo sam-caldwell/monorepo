42d2833df56f18f0f08570bef7c07ef53c8f6090
Sam Caldwell <github@samcaldwell.net>
2023-12-10 11:38:07 -0600

    Make sure Postgres.CreateDatabase() only attempts to create a database if the database does not exist

--- go/db/postgres/CreateDatabase.go
+++ go/db/postgres/CreateDatabase.go
@@ -8,14 +8,16 @@ func (db *Db) CreateDatabase(dbName string, owner string) error {
 	if !SanitizeDBName(&dbName) {
 		return fmt.Errorf("invalid database name (%s)", dbName)
 	}
-	if _, err := db.conn.Exec(fmt.Sprintf("create database %s", dbName)); err != nil {
-		return fmt.Errorf("error creating the database %s: %v\n", dbName, err)
-	}
-	_, err := db.conn.Exec(fmt.Sprintf("grant all privileges on database %s to %s", dbName, owner))
-	if err != nil {
-		return fmt.Errorf("failed to grant privileges on the database %s to %s.  error: %v\n",
-			dbName, owner, err)
-	}
+	if !db.DbExists(dbName) {
+		if _, err := db.conn.Exec(fmt.Sprintf("create database %s", dbName)); err != nil {
+			return fmt.Errorf("error creating the database %s: %v\n", dbName, err)
+		}
 
+		_, err := db.conn.Exec(fmt.Sprintf("grant all privileges on database %s to %s", dbName, owner))
+		if err != nil {
+			return fmt.Errorf("failed to grant privileges on the database %s to %s.  error: %v\n",
+				dbName, owner, err)
+		}
+	}
 	return nil
 }
