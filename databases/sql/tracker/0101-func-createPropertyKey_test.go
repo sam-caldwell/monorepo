17101f4f1d8db18e594ee67da5bcd7dc51d19d33
Sam Caldwell <github@samcaldwell.net>
2023-12-11 00:53:32 -0600

    bug fix: change inputs to avoid conflict with createIntegerProperty()

--- databases/sql/tracker/0101-func-createPropertyKey_test.go
+++ databases/sql/tracker/0101-func-createPropertyKey_test.go
@@ -11,7 +11,7 @@ func TestSqlDbFunc_createPropertyKey(t *testing.T) {
 	const (
 		functionName     = "createPropertyKey"
 		tableName        = "propertyKeys"
-		testPropertyName = "testIntegerProperty"
+		testPropertyName = "testPropertyName"
 	)
 
 	db := sqldbtest.InitializeTestDbConn(t)
@@ -32,5 +32,48 @@ func TestSqlDbFunc_createPropertyKey(t *testing.T) {
 				"pn:{propertyname},"+
 				"pt:{varchar},"+
 				"rt:uuid", strings.ToLower(functionName)))
+	})
+
+	var propertyId string
+	t.Run("create a property using the function", func(t *testing.T) {
+		rows, err := db.Query("select createPropertyKey('%s');", testPropertyName)
+		if err != nil {
+			t.Fatalf("Failed when calling createPropertyKey(): %v", err)
+		}
+		t.Run("createPropertyKey() should return a row", func(t *testing.T) {
+			if !rows.Next() {
+				t.Fatal("no row returned")
+			}
+		})
+
+		t.Run("expect result is a uuid", func(t *testing.T) {
+			err = rows.Scan(&propertyId)
+			sqldbtest.CheckError(t, err)
+		})
+		if propertyId == "" {
+			t.Fatal("empty propertyId returned")
+		}
+	})
+
+	t.Run("select and verify the propertyId and propertyNAme", func(t *testing.T) {
+		rows, err := db.Query("select name from propertyKeys where id='%s'", propertyId)
+		if err != nil {
+			t.Fatalf("select query failed: %v", err)
+		}
+		t.Run("createPropertyKey() should return a row", func(t *testing.T) {
+			if !rows.Next() {
+				t.Fatal("no row returned")
+			}
+		})
+		var propertyName string
+		t.Run("expect the propertyName can be scanned", func(t *testing.T) {
+			err = rows.Scan(&propertyName)
+			if err != nil {
+				t.Fatalf("Error scanning row value: %v", err)
+			}
+			if propertyName != testPropertyName {
+				t.Fatalf("propertyName mismatch")
+			}
+		})
 	})
 }
