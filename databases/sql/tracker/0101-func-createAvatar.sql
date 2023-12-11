23e2bad539a9e99ef86627332401d83bfd2cd0d4
Sam Caldwell <github@samcaldwell.net>
2023-12-10 20:24:59 -0600

    bug fix for createAvatar() in sql tracker db

--- databases/sql/tracker/0101-func-createAvatar.sql
+++ databases/sql/tracker/0101-func-createAvatar.sql
@@ -3,12 +3,17 @@
  * (c) 2023 Sam Caldwell.  See License.txt
  */
 
-create or replace function createAvatar(avatarUrl text) returns uuid as $$
+create or replace function createAvatar(avatarUrl text) returns uuid as
+$$
 declare
     avatarId uuid;
 begin
-    avatarId := gen_random_uuid();
-    insert into avatars (id,url) values(avatarId,avatarUrl);
-    return avatarId;
+    if (select isvalidurl(avatarUrl) as result) then
+        avatarId := gen_random_uuid();
+        insert into avatars (id, url) values (avatarId, avatarUrl);
+        return avatarId;
+    else
+        return '00000000-0000-0000-0000-000000000000';
+    end if;
 end;
 $$ language plpgsql;
--- databases/sql/tracker/0101-func-createAvatar_test.go
+++ databases/sql/tracker/0101-func-createAvatar_test.go
@@ -43,13 +43,16 @@ func TestSqlDbFunc_createAvatar(t *testing.T) {
 		var avatarUrl string
 
 		t.Run("execute createAvatar('"+testUrl+"');", func(t *testing.T) {
+
 			rows, err := db.Query("select createAvatar('%s');", testUrl)
 			sqldbtest.CheckError(t, err)
+
 			t.Run("createAvatar() should return a row", func(t *testing.T) {
 				if !rows.Next() {
 					t.Fatal("no row returned")
 				}
 			})
+
 			t.Run("expect result is a uuid", func(t *testing.T) {
 				err = rows.Scan(&avatarId)
 				sqldbtest.CheckError(t, err)
@@ -62,7 +65,7 @@ func TestSqlDbFunc_createAvatar(t *testing.T) {
 				sqldbtest.CheckError(t, err)
 				rows.Next()
 				sqldbtest.CheckError(t, rows.Scan(&avatarId, &avatarUrl))
-				if avatarUrl != testUrl {
+				if avatarUrl == testUrl {
 					t.Fatalf("url mismatch\n"+
 						"actual:   %s\n"+
 						"expected: %s",
@@ -74,8 +77,8 @@ func TestSqlDbFunc_createAvatar(t *testing.T) {
 
 	t.Run("sad path:run the function with badUrl", func(t *testing.T) {
 		var avatarId uuid.UUID
+
 		t.Run("execute createAvatar('"+badTestUrl+"');", func(t *testing.T) {
-			t.Logf("createAvatar('%s');", badTestUrl)
 			rows, err := db.Query("select createAvatar('%s');", badTestUrl)
 			sqldbtest.CheckError(t, err)
 			t.Run("createAvatar() should return a row", func(t *testing.T) {
@@ -86,19 +89,21 @@ func TestSqlDbFunc_createAvatar(t *testing.T) {
 			t.Run("expect zeroUuid Result", func(t *testing.T) {
 				err = rows.Scan(&avatarId)
 				sqldbtest.CheckError(t, err)
-				if avatarId.String() == zeroUuid {
+				if avatarId.String() != zeroUuid {
 					t.Fatalf("invalid AvatarId: %s", avatarId)
 				}
 			})
+
 			t.Run("Query for expected avatarId (expect none)", func(t *testing.T) {
-				t.Logf("query table for data: %s", badTestUrl)
+
 				rows, err := db.Query(""+
 					"select id, url "+
 					"from avatars "+
 					"where url='%s';", badTestUrl)
-				t.Logf("query returned %s", badTestUrl)
+
 				sqldbtest.CheckError(t, err)
 				t.Logf("query had no error")
+
 				if rows.Next() {
 					t.Fatal("We should not have created an avatar entry on invalid url")
 				}
