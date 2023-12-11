d2295827b7826a7d1ae6fd7a47c706c5984827c2
Sam Caldwell <github@samcaldwell.net>
2023-12-10 21:29:29 -0600

    bug fixes for deleteAvatar() tests in tracker db

--- databases/sql/tracker/0101-func-deleteAvatar_test.go
+++ databases/sql/tracker/0101-func-deleteAvatar_test.go
@@ -2,6 +2,7 @@ package psqlTrackerDb
 
 import (
 	"fmt"
+	"github.com/google/uuid"
 	"github.com/sam-caldwell/monorepo/go/db/sqldbtest"
 	"strings"
 	"testing"
@@ -11,7 +12,7 @@ func TestSqlDbFunc_deleteAvatar(t *testing.T) {
 	const (
 		tableName    = "avatar"
 		functionName = "deleteAvatar"
-		testUrl      = "http://testUrlButNeverReal.tld/thisShouldNeverExistInTheDb.png"
+		testUrl      = "http://example.com/itMayExists.png"
 	)
 	db := sqldbtest.InitializeTestDbConn(t)
 
@@ -33,8 +34,53 @@ func TestSqlDbFunc_deleteAvatar(t *testing.T) {
 				"rt:int4", strings.ToLower(functionName)))
 	})
 
-	t.Run("happy path:run the function", func(t *testing.T) {
-		t.Skip("not implemented")
+	var avatarId uuid.UUID
+	t.Run("create an avatar record", func(t *testing.T) {
+		rows, err := db.Query("select createAvatar('%s');", testUrl)
+		if err != nil {
+			t.Fatalf("Failed to create record: %v", err)
+		}
+
+		t.Run("createAvatar() should return a row", func(t *testing.T) {
+			if !rows.Next() {
+				t.Fatal("no row returned")
+			}
+			if err := rows.Scan(&avatarId); err != nil {
+				t.Fatalf("err: %v", err)
+			}
+		})
+	})
+	t.Run("delete the avatar", func(t *testing.T) {
+		rows, err := db.Query("select deleteAvatar('%s');", avatarId)
+		if err != nil {
+			t.Fatalf("failed on call to deleteAvatar(): %v", err)
+		}
+		if !rows.Next() {
+			t.Fatal("no row returned")
+		}
+		var count int
+		if err := rows.Scan(&count); err != nil {
+			t.Fatalf("err: %v", err)
+		}
+		if count == 0 {
+			t.Fatal("Failed to delete the record.")
+		}
 	})
 
+	t.Run("delete the avatar...again and expect 0 results", func(t *testing.T) {
+		rows, err := db.Query("select deleteAvatar('%s');", avatarId)
+		if err != nil {
+			t.Fatalf("failed on call to deleteAvatar(): %v", err)
+		}
+		if !rows.Next() {
+			t.Fatal("no row returned")
+		}
+		var count int
+		if err := rows.Scan(&count); err != nil {
+			t.Fatalf("err: %v", err)
+		}
+		if count != 0 {
+			t.Fatal("We shouldn't be able to delete a record twice..")
+		}
+	})
 }
