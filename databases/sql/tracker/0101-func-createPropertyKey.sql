dea6b2f2c4b8b1785399ecf2b60156b8c017deb5
Sam Caldwell <github@samcaldwell.net>
2023-12-11 00:43:31 -0600

    WIP: first features of the createPropertyKey() tests

--- databases/sql/tracker/0101-func-createPropertyKey.sql
+++ databases/sql/tracker/0101-func-createPropertyKey.sql
@@ -3,10 +3,13 @@
  * (c) 2023 Sam Caldwell.  See License.txt
  */
 
-create or replace function createPropertyKey(pid uuid, propertyName varchar(64)) returns uuid as
+create or replace function createPropertyKey(propertyName varchar(64)) returns uuid as
 $$
+declare
+    propertyId uuid;
 begin
-    insert into propertyKeys (id, name) values (pid, propertyName);
-    return pid;
+    propertyId:=gen_random_uuid();
+    insert into propertyKeys (id, name) values (propertyId, propertyName);
+    return propertyId;
 end
 $$ language plpgsql;
--- databases/sql/tracker/0101-func-createPropertyKey_test.go
+++ databases/sql/tracker/0101-func-createPropertyKey_test.go
@@ -1,7 +1,36 @@
 package psqlTrackerDb
 
-import "testing"
+import (
+	"fmt"
+	"github.com/sam-caldwell/monorepo/go/db/sqldbtest"
+	"strings"
+	"testing"
+)
 
 func TestSqlDbFunc_createPropertyKey(t *testing.T) {
-	t.Skip("not implemented")
+	const (
+		functionName     = "createPropertyKey"
+		tableName        = "propertyKeys"
+		testPropertyName = "testIntegerProperty"
+	)
+
+	db := sqldbtest.InitializeTestDbConn(t)
+
+	t.Cleanup(func() {
+		// Note: we only clean up the avatar we expect to have created.
+		//       this should safeguard against an accidental run on prod.
+		_, _ = db.Query("delete from %s where name='%s' cascade;", tableName, testPropertyName)
+
+		err := db.Close()
+		sqldbtest.CheckError(t, err)
+	})
+
+	t.Run("verify the function structure (params, return)", func(t *testing.T) {
+		sqldbtest.VerifyFunctionStructure(t, db,
+			strings.ToLower(functionName),
+			fmt.Sprintf("fn:%s,"+
+				"pn:{propertyname},"+
+				"pt:{varchar},"+
+				"rt:uuid", strings.ToLower(functionName)))
+	})
 }
