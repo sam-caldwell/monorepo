9df20f7e071287f51dddd37eaebd4b179afd2792
Sam Caldwell <github@samcaldwell.net>
2023-12-11 20:13:20 -0600

    add updateUserDescription() with passing tests

--- databases/sql/tracker/0106-func-updateUserDescription.sql
+++ databases/sql/tracker/0106-func-updateUserDescription.sql
@@ -2,12 +2,12 @@
  * 0106-func-updateUserDescription.sql
  * (c) 2023 Sam Caldwell.  See License.txt
  */
-create or replace function updateUserDescription(userId uuid, description text) returns integer as
+create or replace function updateUserDescription(userId uuid, newDescription text) returns integer as
 $$
 declare
     count integer;
 begin
-    update users set description=description where id = userId;
+    update users set description=newDescription where id = userId;
     get diagnostics count = ROW_COUNT;
     return count;
 end;
