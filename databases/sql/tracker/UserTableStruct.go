e300390f402b6270708c19f3c43a7dc2654948b1
Sam Caldwell <github@samcaldwell.net>
2023-12-12 00:09:36 -0600

    add the user table struct

--- databases/sql/tracker/UserTableStruct.go
+++ databases/sql/tracker/UserTableStruct.go
@@ -5,9 +5,9 @@ import "github.com/google/uuid"
 // TrackerUser - This is a struct representing the Tracker DB User
 type TrackerUser struct {
 	Id          uuid.UUID `yaml:"id"`
+	AvatarId    uuid.UUID `yaml:"avatarId"`
 	FirstName   string    `yaml:"firstName"`
 	LastName    string    `yaml:"lastName"`
-	AvatarId    uuid.UUID `yaml:"avatarId"`
 	Email       string    `yaml:"email"`
 	PhoneNumber string    `yaml:"phoneNumber"`
 	Description string    `yaml:"description"`
