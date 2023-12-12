1f6e86e1ad3ccbf00602d47f87d9037bd7a8f531
Sam Caldwell <github@samcaldwell.net>
2023-12-12 14:27:40 -0600

    add createWorkflow() and passing test.

--- databases/sql/tracker/0109-func-createWorkflow_test.go
+++ databases/sql/tracker/0109-func-createWorkflow_test.go
@@ -26,6 +26,7 @@ func TestSqlDbFunc_createWorkflow(t *testing.T) {
 	var iconId uuid.UUID
 	var teamId uuid.UUID
 	var ownerId uuid.UUID
+	var workflowId uuid.UUID
 
 	db := sqldbtest.InitializeTestDbConn(t)
 
@@ -171,7 +172,7 @@ func TestSqlDbFunc_createWorkflow(t *testing.T) {
 		})
 	})
 
-	t.Run("createWorkflow() and verify", func(t *testing.T) {
+	t.Run("createWorkflow()", func(t *testing.T) {
 		var rows *sql.Rows
 		var err error
 		rows, err = db.Query("select createWorkflow('%s','%s','%s','%s','%s','%s','%s','%s');",
@@ -185,9 +186,63 @@ func TestSqlDbFunc_createWorkflow(t *testing.T) {
 		}
 		var raw string
 		err = rows.Scan(&raw)
-		if teamId, err = uuid.Parse(raw); err != nil {
+		if workflowId, err = uuid.Parse(raw); err != nil {
 			t.Fatal(err)
 		}
+	})
 
+	t.Run("verify workflow", func(t *testing.T) {
+		var rows *sql.Rows
+		var err error
+		rows, err = db.Query(""+
+			"select id,name, iconId, ownerId, teamId, p_owner,p_team,p_everyone,description"+
+			"from workflow"+
+			"where id='%s';", workflowId)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer func() { _ = rows.Close() }()
+		if !rows.Next() {
+			t.Fatal("no row returned")
+		}
+		var actualId uuid.UUID
+		var actualName string
+		var actualIconId uuid.UUID
+		var actualOwnerId uuid.UUID
+		var actualTeamId uuid.UUID
+		var actualOwner string
+		var actualTeam string
+		var actualEveryone string
+		var actualDescription string
+		err = rows.Scan(&actualId, &actualName, &actualIconId, &actualOwnerId, &actualTeamId,
+			&actualOwner, &actualTeam, &actualEveryone, &actualDescription)
+
+		if actualId != workflowId {
+			t.Fatalf("workflowId mismatch")
+		}
+		if actualName != expectedWorkflowName {
+			t.Fatalf("workflowName mismatch")
+		}
+		if actualIconId != iconId {
+			t.Fatalf("iconId mismatch")
+		}
+		if actualOwnerId != ownerId {
+			t.Fatalf("ownerId mismatch")
+		}
+		if actualTeamId != teamId {
+			t.Fatalf("teamId mismatch")
+		}
+		if actualOwner != "read" {
+			t.Fatalf("owner mismatch")
+		}
+		if actualTeam != "read" {
+			t.Fatalf("team mismatch")
+		}
+		if actualEveryone != "read" {
+			t.Fatalf("everyone mismatch")
+		}
+		if actualDescription != expectedDescription {
+			t.Fatalf("description mismatch")
+		}
 	})
 }
