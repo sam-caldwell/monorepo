370736405c921b4a113d19182c9df83030b8127d
Sam Caldwell <github@samcaldwell.net>
2023-12-11 20:03:18 -0600

    updateUserAvatar() and its test for tracker db

--- databases/sql/tracker/0106-func-updateUserAvatar.sql
+++ databases/sql/tracker/0106-func-updateUserAvatar.sql
@@ -2,12 +2,12 @@
  * 0106-func-updateUserAvatar.sql
  * (c) 2023 Sam Caldwell.  See License.txt
  */
-create or replace function updateUserAvatar(userId uuid, avatarId varchar(256)) returns integer as
+create or replace function updateUserAvatar(userId uuid, newAvatarId uuid) returns integer as
 $$
 declare
     count integer;
 begin
-    update users set avatarId=avatarId where id=userId;
+    update users set avatarId=newAvatarId where id=userId;
     get diagnostics count = ROW_COUNT;
     return count;
 end;
