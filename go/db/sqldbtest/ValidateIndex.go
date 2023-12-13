279c2fe446b6d7e7dc24d6c21a9ceb9bd304f8ab
Sam Caldwell <github@samcaldwell.net>
2023-12-13 16:19:06 -0600

    add validateIndex()

--- go/db/sqldbtest/ValidateIndex.go
+++ go/db/sqldbtest/ValidateIndex.go
@@ -2,7 +2,6 @@ package sqldbtest
 
 import (
 	"fmt"
-	"github.com/sam-caldwell/monorepo/go/convert"
 	Postgres "github.com/sam-caldwell/monorepo/go/db/postgres"
 	"strings"
 	"testing"
@@ -12,34 +11,50 @@ import (
 //
 //	Follow the indexName convention: ndx${tableName}${columnNames}
 func ValidateIndex(t *testing.T, db *Postgres.Db, tableName string, expectedColumns []string) {
+	tableName = strings.ToLower(tableName)
+	columnsForName := strings.ToLower(strings.Join(expectedColumns, ""))
+	indexName := fmt.Sprintf("ndx%s%s", tableName, columnsForName)
 
-	convert.CapitalizeStringList(&expectedColumns)
+	t.Run("verify index ("+indexName+")", func(t *testing.T) {
 
-	c := strings.Join(expectedColumns, "")
+		rows, err := db.Query(""+
+			"select indexname,indexdef "+
+			"from pg_indexes "+
+			"where tablename=lower('%s') "+
+			"and indexname=lower('%s')", tableName, indexName)
+		if err != nil {
+			t.Fatalf("Failed to query index\n"+
+				"table: %s\n"+
+				"index: %s\n"+
+				"err:   %v", tableName, indexName, err)
+		}
+		defer func() { _ = rows.Close() }()
+		if !rows.Next() {
+			t.Fatalf("no expected rows returned\n"+
+				"table: %s\n"+
+				"index: %s\n", tableName, indexName)
+		}
 
-	indexName := fmt.Sprintf("ndx%s%s", convert.Capitalize(tableName), c)
-	rows, err := db.Query(""+
-		"select indexname,indexdef "+
-		"from pg_indexes "+
-		"where tablename=lower('%s') "+
-		"and indexname=lower('%s')", tableName, indexName)
-	if err != nil {
-		t.Fatalf("Failed to query index\n"+
-			"table: %s\n"+
-			"index: %s\n"+
-			"err:   %v", tableName, indexName, err)
-	}
-	if !rows.Next() {
-		t.Fatalf("no expected rows returned\n"+
-			"table: %s\n"+
-			"index: %s\n"+
-			"err:   %v", tableName, indexName, err)
-	}
-	var actualIndexName, actualIndexDef string
-	if err := rows.Scan(&actualIndexName, &actualIndexDef); err != nil {
-		t.Fatalf("Failed to scan results\n"+
-			"table: %s\n"+
-			"index: %s\n"+
-			"err:   %v", tableName, indexName, err)
-	}
+		var actualIndexName, actualIndexDef string
+		actualIndexName = strings.ToLower(actualIndexName)
+		actualIndexDef = strings.ToLower(actualIndexDef)
+		if err := rows.Scan(&actualIndexName, &actualIndexDef); err != nil {
+			t.Fatalf("Failed to scan results\n"+
+				"table: '%s'\n"+
+				"index: '%s'\n"+
+				"err:   %v", tableName, indexName, err)
+		}
+		if actualIndexName != indexName {
+			t.Fatalf("indexName mismatch\n"+
+				"table: '%s'\n"+
+				"index: '%s'\n"+
+				"actual:'%s'", tableName, indexName, actualIndexDef)
+		}
+		if actualIndexDef == "" {
+			t.Fatalf("indexdef mismatch\n"+
+				"table: '%s'\n"+
+				"index: '%s'\n"+
+				"actual:'%s'", tableName, indexName, actualIndexDef)
+		}
+	})
 }
