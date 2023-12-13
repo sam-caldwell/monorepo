f3b48523b2fb192bd34af18cebd7751d960a68a3
Sam Caldwell <github@samcaldwell.net>
2023-12-13 00:35:03 -0600

    renumbering sql files (databases/sql/tracker/1800-0020-func-deleteTicketAttachment.sql)

--- databases/sql/tracker/1800-0020-func-deleteTicketAttachment.sql
+++ databases/sql/tracker/1800-0020-func-deleteTicketAttachment.sql
@@ -1,5 +1,5 @@
 /*
- * 0200-0020-func-deleteTicketAttachment.sql
+ * 1800-0020-func-deleteTicketAttachment.sql
  * (c) 2023 Sam Caldwell.  See License.txt
  */
 create or replace function deleteTicketAttachment(commentId uuid) returns integer as
