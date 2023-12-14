5bdd75a088eee29fd7ff9e933ae1749f3cdb63f4
Sam Caldwell <github@samcaldwell.net>
2023-12-14 01:57:54 -0600

    readding sql unit tests after refactor

---
+++ databases/sql/tracker/1000-users_test.go
@@ -0,0 +1,71 @@
+package psqlTrackerDb
+
+import (
+	"github.com/sam-caldwell/monorepo/go/db/sqldbtest"
+	"testing"
+)
+
+func TestSqlDbTable_users(t *testing.T) {
+	const tableName = "users"
+	db := sqldbtest.InitializeTestDbConn(t)
+
+	t.Cleanup(func() {
+		err := db.Close()
+		sqldbtest.CheckError(t, err)
+	})
+
+	t.Run("query the table (verifies permissions of user and existence of table)", func(t *testing.T) {
+		rows, err := db.Query("select 1 from %s limit 1;", tableName)
+		sqldbtest.CheckError(t, err)
+		defer func() { _ = rows.Close() }()
+	})
+
+	t.Run("check table schema", func(t *testing.T) {
+		sqldbtest.ValidateTable(t, db, tableName, []string{
+			"ColumnName:id,DataType:uuid,size:-1,IsNullable:NO,ColumnDefault:<<null>>",
+			"ColumnName:description,DataType:text,size:-1,IsNullable:yes,ColumnDefault:<<null>>",
+			"ColumnName:email,DataType:character varying,size:256,IsNullable:no,ColumnDefault:<<null>>",
+			"ColumnName:firstName,DataType:character varying,size:64,IsNullable:no,ColumnDefault:<<null>>",
+			"ColumnName:lastName,DataType:character varying,size:64,IsNullable:no,ColumnDefault:<<null>>",
+			"ColumnName:avatarId,DataType:uuid,size:-1,IsNullable:no,ColumnDefault:<<null>>",
+			"ColumnName:phonenumber,DataType:character varying,size:20,IsNullable:YES,ColumnDefault:<<null>>",
+			"ColumnName:created,DataType:timestamp without time zone,size:-1,IsNullable:NO,ColumnDefault:now()",
+		})
+	})
+
+	t.Run("check foreign keys", func(t *testing.T) {
+		sqldbtest.ValidateForeignKey(t, db, tableName, "avatars", "avatarId", "id")
+		sqldbtest.ValidateForeignKey(t, db, tableName, "entity", "id", "id")
+	})
+	t.Run("verify indexes: created", func(t *testing.T) {
+		columnNames := []string{
+			"created",
+		}
+		sqldbtest.ValidateIndex(t, db, tableName, columnNames, false)
+	})
+	t.Run("verify indexes: email", func(t *testing.T) {
+		columnNames := []string{
+			"email",
+		}
+		sqldbtest.ValidateIndex(t, db, tableName, columnNames, true)
+	})
+	t.Run("verify indexes: firstName", func(t *testing.T) {
+		columnNames := []string{
+			"firstName",
+		}
+		sqldbtest.ValidateIndex(t, db, tableName, columnNames, false)
+	})
+
+	t.Run("verify indexes: lastName", func(t *testing.T) {
+		columnNames := []string{
+			"lastName",
+		}
+		sqldbtest.ValidateIndex(t, db, tableName, columnNames, false)
+	})
+	t.Run("verify indexes: phoneNumber", func(t *testing.T) {
+		columnNames := []string{
+			"phoneNumber",
+		}
+		sqldbtest.ValidateIndex(t, db, tableName, columnNames, true)
+	})
+}
