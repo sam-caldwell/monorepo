8395f38dc66cbd014098622922fb336026455d8a
Sam Caldwell <github@samcaldwell.net>
2023-12-14 01:57:50 -0600

    readding sql unit tests after refactor

---
+++ databases/sql/tracker/010-preventUpdate.sql
@@ -0,0 +1,13 @@
+/*
+ * 010-preventUpdate.sql
+ * (c) 2023 Sam Caldwell. See License.txt
+ *
+ * A function which throws an exception to prevent updates
+ */
+
+create or replace function preventUpdate() returns trigger as
+$$
+begin
+    raise exception 'cannot update  records';
+end;
+$$ language plpgsql;
