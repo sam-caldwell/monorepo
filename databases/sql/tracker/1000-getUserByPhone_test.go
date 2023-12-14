2c57678761ee65f997a4093c7eb5f03b98271ca3
Sam Caldwell <github@samcaldwell.net>
2023-12-14 01:57:52 -0600

    readding sql unit tests after refactor

---
+++ databases/sql/tracker/1000-getUserByPhone_test.go
@@ -0,0 +1,177 @@
+package psqlTrackerDb
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/sam-caldwell/monorepo/go/db/sqldbtest"
+	"strings"
+	"testing"
+)
+
+func TestSqlDbFunc_getUserByPhone(t *testing.T) {
+	const (
+		avatarUrl           = "http://localhost/myfakeavatar.jpeg"
+		functionName        = "getUserByPhone"
+		tableName           = "user"
+		expectedFirstName   = "Ada"
+		expectedLastName    = "Lovelace"
+		expectedEmail       = "ada.lovelace@example.com"
+		expectedPhone       = "915.123.4567"
+		expectedDescription = "Test description"
+	)
+	var avatarId uuid.UUID
+	var userId uuid.UUID
+
+	db := sqldbtest.InitializeTestDbConn(t)
+
+	t.Cleanup(func() {
+		// Note: we only clean up the avatar we expect to have created.
+		//       this should safeguard against an accidental run on prod.
+		_, _ = db.Query("delete from %s where email='%s'", tableName, expectedEmail)
+		err := db.Close()
+		sqldbtest.CheckError(t, err)
+	})
+
+	t.Run("verify the function structure (params, return)", func(t *testing.T) {
+		sqldbtest.VerifyFunctionStructure(t, db,
+			strings.ToLower(functionName),
+			fmt.Sprintf("fn:%s,"+
+				"pn:{phone},"+
+				"pt:{varchar},"+
+				"rt:jsonb", strings.ToLower(functionName)))
+	})
+
+	t.Run("call createAvatar()", func(t *testing.T) {
+		/*
+		 * We need to create an avatar to create a user
+		 */
+		var rows *sql.Rows
+		var err error
+		rows, err = db.Query("select createAvatar('%s');", avatarUrl)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer func() { _ = rows.Close() }()
+		if !rows.Next() {
+			t.Fatal("no row returned")
+		}
+		var raw string
+		err = rows.Scan(&raw)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if avatarId, err = uuid.Parse(raw); err != nil {
+			t.Fatal(err)
+		}
+		if avatarId.String() == "00000000-0000-0000-0000-000000000000" {
+			t.Fatal("illegal zero uuid")
+		}
+	})
+
+	t.Run("call createUser()", func(t *testing.T) {
+		/*
+		 * create the user...
+		 */
+		var rows *sql.Rows
+		var err error
+		rows, err = db.Query("select createUser('%s','%s','%s','%s','%s','%s');",
+			expectedFirstName, expectedLastName, avatarId, expectedEmail, expectedPhone, expectedDescription)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer func() { _ = rows.Close() }()
+		if !rows.Next() {
+			t.Fatal("no row returned")
+		}
+		var raw string
+		err = rows.Scan(&raw)
+		if userId, err = uuid.Parse(raw); err != nil {
+			t.Fatal(err)
+		}
+		if userId.String() == "00000000-0000-0000-0000-000000000000" {
+			t.Fatal("illegal zero uuid")
+		}
+	})
+
+	t.Run("inspect and verify user", func(t *testing.T) {
+		/*
+		 * verify the user.
+		 */
+		var rows *sql.Rows
+		var err error
+		rows, err = db.Query("select id,firstName,lastName,avatarId,email,phoneNumber,description "+
+			"from users where id='%s'", userId)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer func() { _ = rows.Close() }()
+		if !rows.Next() {
+			t.Fatal("no row returned")
+		}
+
+		var actualId, actualAvatarId uuid.UUID
+		var actualFirstName, actualLastName, actualEmail, actualPhone, actualDescription string
+
+		err = rows.Scan(&actualId, &actualFirstName, &actualLastName, &actualAvatarId, &actualEmail, &actualPhone,
+			&actualDescription)
+		if err != nil {
+			t.Fatalf("Failed to read result: %v", err)
+		}
+		if actualId != userId {
+			t.Fatal("UserId mismatch")
+		}
+		if actualFirstName != expectedFirstName {
+			t.Fatalf("FirstName mismatch\n"+
+				"actual:   '%s'\n"+
+				"expected: '%s'", actualFirstName, expectedFirstName)
+		}
+		if actualLastName != expectedLastName {
+			t.Fatalf("LastName mismatch\n"+
+				"actual:   '%s'\n"+
+				"expected: '%s'", actualLastName, expectedLastName)
+		}
+		if actualAvatarId != avatarId {
+			t.Fatalf("AvatarId mismatch\n"+
+				"actual:   '%s'\n"+
+				"expected: '%s'", actualAvatarId, avatarId)
+		}
+		if actualEmail != expectedEmail {
+			t.Fatalf("Email mismatch\n"+
+				"actual:   '%s'\n"+
+				"expected: '%s'", actualEmail, expectedEmail)
+		}
+		if actualPhone != expectedPhone {
+			t.Fatalf("PhoneNumber mismatch\n"+
+				"actual:   '%s'\n"+
+				"expected: '%s'", actualPhone, expectedPhone)
+		}
+		if actualDescription != expectedDescription {
+			t.Fatalf("Description mismatch\n"+
+				"actual:   '%s'\n"+
+				"expected: '%s'", actualDescription, expectedDescription)
+		}
+	})
+
+	t.Run("call getUserByPhone(expectedPhone)", func(t *testing.T) {
+		var rows *sql.Rows
+		var err error
+		rows, err = db.Query("select getUserByPhone('%s');", expectedPhone)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer func() { _ = rows.Close() }()
+		if !rows.Next() {
+			t.Fatal("no row returned")
+		}
+		var raw string
+		if err = rows.Scan(&raw); err != nil {
+			t.Fatal(err)
+		}
+		var actualUser TrackerUser
+		if err = json.Unmarshal([]byte(raw), &actualUser); err != nil {
+			t.Fatalf("unmarshal failed: %v", err)
+		}
+	})
+}
