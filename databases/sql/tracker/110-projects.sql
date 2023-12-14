9a20715c19f2b9afda6ee57856a996bc579e753f
Sam Caldwell <github@samcaldwell.net>
2023-12-14 01:56:09 -0600

    refactored sql (databases/sql/tracker/110-projects.sql)

--- databases/sql/tracker/110-projects.sql
+++ databases/sql/tracker/110-projects.sql
@@ -27,7 +27,290 @@ create table if not exists projects
     foreign key (defaultTicketType) references ticketTypes(id),
     foreign key (id) references entity (id) on delete restrict
 );
-
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * entity indexes
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
 create unique index if not exists ndxProjectsName on projects (name);
 create index if not exists ndxProjectsCreated on projects (created);
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * createProjects() function
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+create or replace function createProjects(name varchar(64), iconId uuid, workflowId uuid,
+                                          description text) returns uuid as
+$$
+declare
+    newId uuid;
+begin
+    newId := gen_random_uuid();
+    insert into projects (id, name, iconId, workflowId, description)
+    values (newId, name, iconId, workflowId, description);
+    return newId;
+end;
+$$ language plpgsql;
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * deleteProjects() function
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+create or replace function deleteProjects(projectId uuid) returns integer as
+$$
+declare
+    count integer;
+begin
+    delete from projects where id = projectId;
+    get diagnostics count = ROW_COUNT;
+    return count;
+end;
+$$ language plpgsql;
 
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * getProjectsById() function
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+create or replace function getProjectsById(projectId uuid) returns jsonb as
+$$
+declare
+    result jsonb;
+begin
+    select jsonb_agg(jsonb_build_object(
+            'id', id,
+            'name', name,
+            'iconId', iconId,
+            'ownerId', ownerId,
+            'teamId', teamId,
+            'permissionOwner', owner,
+            'permissionTeam', team,
+            'permissionEveryone', everyone,
+            'defaultTicketType', defaultTicketType,
+            'description', description
+        ))  as data into result
+    from projects
+    where id == projectId;
+    return result;
+
+end ;
+$$ language plpgsql;
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * getProjectsByName() function
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+create or replace function getProjectsByName(projectName varchar(64)) returns jsonb as
+$$
+declare
+    result jsonb;
+begin
+    select jsonb_agg(jsonb_build_object(
+            'id', id,
+            'name', name,
+            'iconId', iconId,
+            'ownerId', ownerId,
+            'teamId', teamId,
+            'permissionOwner', owner,
+            'permissionTeam', team,
+            'permissionEveryone', everyone,
+            'defaultTicketType', defaultTicketType,
+            'description', description
+        ))  as data into result
+    from projects
+    where name == projectName;
+    return result;
+
+end ;
+$$ language plpgsql;
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * getProjectsByOwnerId() function
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+create or replace function getProjectsByOwnerId(OwnerId uuid) returns jsonb as
+$$
+declare
+    result jsonb;
+begin
+    select jsonb_agg(jsonb_build_object(
+            'id', id,
+            'name', name,
+            'iconId', iconId,
+            'ownerId', ownerId,
+            'teamId', teamId,
+            'permissionOwner', owner,
+            'permissionTeam', team,
+            'permissionEveryone', everyone,
+            'defaultTicketType', defaultTicketType,
+            'description', description
+        ))  as data into result
+    from projects
+    where ownerId == OwnerId;
+    return result;
+
+end ;
+$$ language plpgsql;
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * getProjectsByTeamId() function
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+create or replace function getProjectsByTeamId(TeamId uuid) returns jsonb as
+$$
+declare
+    result jsonb;
+begin
+    select jsonb_agg(jsonb_build_object(
+            'id', id,
+            'name', name,
+            'iconId', iconId,
+            'ownerId', ownerId,
+            'teamId', teamId,
+            'permissionOwner', owner,
+            'permissionTeam', team,
+            'permissionEveryone', everyone,
+            'defaultTicketType', defaultTicketType,
+            'description', description
+        ))  as data into result
+    from projects
+    where teamId == TeamId;
+    return result;
+
+end ;
+$$ language plpgsql;
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * updateProjectDescription() function
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+create or replace function updateProjectDescription(projectId uuid, descriptiveText text) returns integer as
+$$
+declare
+    count integer;
+begin
+    update projects set description=descriptiveText where id = projectId;
+    get diagnostics count = ROW_COUNT;
+    return count;
+end;
+$$ language plpgsql;
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * updateProjectDefaultTicketType() function
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+create or replace function updateProjectDefaultTicketType(projectId uuid, defaultTicketType uuid) returns integer as
+$$
+declare
+    count integer;
+begin
+    update projects set defaultTicketType=defaultTicketType where id = projectId;
+    get diagnostics count = ROW_COUNT;
+    return count;
+end;
+$$ language plpgsql;
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * updateProjectIconId() function
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+create or replace function updateProjectIconId(projectId uuid, iconId uuid) returns integer as
+$$
+declare
+    count integer;
+begin
+    update projects set iconId=iconId where id = projectId;
+    get diagnostics count = ROW_COUNT;
+    return count;
+end;
+$$ language plpgsql;
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * updateProjectName() function
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+create or replace function updateProjectName(projectId uuid, projectName varchar(64)) returns integer as
+$$
+declare
+    count integer;
+begin
+    update projects set name=projectName where id = projectId;
+    get diagnostics count = ROW_COUNT;
+    return count;
+end;
+$$ language plpgsql;
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * updateProjectOwnerId() function
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+create or replace function updateProjectOwnerId(projectId uuid, ownerId varchar(64)) returns integer as
+$$
+declare
+    count integer;
+begin
+    update projects set ownerId=ownerId where id = projectId;
+    get diagnostics count = ROW_COUNT;
+    return count;
+end;
+$$ language plpgsql;
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * updateProjectPermEveryone() function
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+create or replace function updateProjectPermEveryone(projectId uuid, everyone permissions) returns integer as
+$$
+declare
+    count integer;
+begin
+    update projects set everyone=everyone where id = projectId;
+    get diagnostics count = ROW_COUNT;
+    return count;
+end;
+$$ language plpgsql;
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * updateProjectPermOwner() function
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+create or replace function updateProjectPermOwner(projectId uuid, owner permissions) returns integer as
+$$
+declare
+    count integer;
+begin
+    update projects set owner=owner where id = projectId;
+    get diagnostics count = ROW_COUNT;
+    return count;
+end;
+$$ language plpgsql;
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * updateProjectDescription() function
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+create or replace function updateProjectPermTeam(projectId uuid, team permissions) returns integer as
+$$
+declare
+    count integer;
+begin
+    update projects set team=team where id = projectId;
+    get diagnostics count = ROW_COUNT;
+    return count;
+end;
+$$ language plpgsql;
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * updateProjectDescription() function
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+create or replace function updateProjectTeamId(projectId uuid, teamId uuid) returns integer as
+$$
+declare
+    count integer;
+begin
+    update projects set team=teamId where id = projectId;
+    get diagnostics count = ROW_COUNT;
+    return count;
+end;
+$$ language plpgsql;
