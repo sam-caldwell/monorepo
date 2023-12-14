02a35fcf95b2725825c3a9db8aaa14ed79e79c09
Sam Caldwell <github@samcaldwell.net>
2023-12-14 01:57:53 -0600

    readding sql unit tests after refactor

---
+++ databases/sql/tracker/1000-stringProperties_test.go
@@ -0,0 +1,35 @@
+package psqlTrackerDb
+
+import (
+	"github.com/sam-caldwell/monorepo/go/db/sqldbtest"
+	"testing"
+)
+
+func TestSqlDbTable_stringProperties(t *testing.T) {
+	const tableName = "stringproperties"
+	db := sqldbtest.InitializeTestDbConn(t)
+
+	t.Cleanup(func() {
+		err := db.Close()
+		if err != nil {
+			t.Fatalf("Error when cleaning up\nerr:%v", err)
+		}
+	})
+
+	t.Run("query the table (verifies permissions of user and existence of table)", func(t *testing.T) {
+		rows, err := db.Query("select 1 from %s limit 1;", tableName)
+		sqldbtest.CheckError(t, err)
+		defer func() { _ = rows.Close() }()
+	})
+
+	t.Run("check table schema", func(t *testing.T) {
+		sqldbtest.ValidateTable(t, db, tableName, []string{
+			"ColumnName:id,DataType:uuid,size:-1,IsNullable:NO,ColumnDefault:<<null>>",
+			"ColumnName:value,DataType:text,size:-1,IsNullable:NO,ColumnDefault:<<null>>",
+		})
+	})
+
+	t.Run("check foreign keys", func(t *testing.T) {
+		sqldbtest.ValidateForeignKey(t, db, tableName, "propertyKeys", "id", "id")
+	})
+}
