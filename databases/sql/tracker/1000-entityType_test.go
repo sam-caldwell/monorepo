a601a6baf37d938e0d50140415b605c4a7b5b13b
Sam Caldwell <github@samcaldwell.net>
2023-12-14 01:57:51 -0600

    readding sql unit tests after refactor

---
+++ databases/sql/tracker/1000-entityType_test.go
@@ -0,0 +1,22 @@
+package psqlTrackerDb
+
+import (
+	"github.com/sam-caldwell/monorepo/go/db/sqldbtest"
+	"testing"
+)
+
+func TestSqlDbType_entityType(t *testing.T) {
+	db := sqldbtest.InitializeTestDbConn(t)
+
+	t.Cleanup(func() {
+		err := db.Close()
+		sqldbtest.CheckError(t, err)
+	})
+	t.Run("verify the enumerated type values", func(t *testing.T) {
+		actual := sqldbtest.GetEnumValues(t, db, "entityType")
+		expected := []string{"avatar", "icon", "user", "team", "workflow", "workflow_step",
+			"workflow_action", "ticket_type", "project", "ticket", "attachment", "comment", "other"}
+		sqldbtest.CompareTwoStringLists(t, actual, expected)
+	})
+
+}
