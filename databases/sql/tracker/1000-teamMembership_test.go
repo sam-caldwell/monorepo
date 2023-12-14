df7537b0cf7afc6267953ce402d6da47fedaca08
Sam Caldwell <github@samcaldwell.net>
2023-12-14 01:57:53 -0600

    readding sql unit tests after refactor

---
+++ databases/sql/tracker/1000-teamMembership_test.go
@@ -0,0 +1,36 @@
+package psqlTrackerDb
+
+import (
+	"github.com/sam-caldwell/monorepo/go/db/sqldbtest"
+	"testing"
+)
+
+func TestSqlDbTable_TeamMembership(t *testing.T) {
+	const tableName = "teamMembership"
+
+	db := sqldbtest.InitializeTestDbConn(t)
+
+	t.Cleanup(func() {
+		err := db.Close()
+		sqldbtest.CheckError(t, err)
+	})
+
+	t.Run("query the table (verifies permissions of user and existence of table)", func(t *testing.T) {
+		rows, err := db.Query("select userId,teamId,created from %s limit 1;", tableName)
+		sqldbtest.CheckError(t, err)
+		defer func() { _ = rows.Close() }()
+	})
+
+	t.Run("check table schema", func(t *testing.T) {
+		sqldbtest.ValidateTable(t, db, tableName, []string{
+			"ColumnName:userId,datatype:uuid,size:-1,IsNullable:no,ColumnDefault:<<null>>",
+			"ColumnName:teamId,DataType:uuid,size:-1,IsNullable:no,ColumnDefault:<<null>>",
+			"ColumnName:created,DataType:timestamp without time zone,size:-1,IsNullable:NO,ColumnDefault:now()",
+		})
+	})
+
+	t.Run("check foreign keys", func(t *testing.T) {
+		sqldbtest.ValidateForeignKey(t, db, tableName, "users", "userId", "id")
+		sqldbtest.ValidateForeignKey(t, db, tableName, "teams", "teamId", "id")
+	})
+}
