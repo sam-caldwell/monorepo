90e318229fd071cda2291a1d31752982a9db27f7
Sam Caldwell <github@samcaldwell.net>
2023-12-15 01:14:23 -0600

    add countById()

---
+++ databases/sql/tracker/010-countById.go
@@ -0,0 +1,28 @@
+package psqlTrackerDb
+
+import (
+	"database/sql"
+	"github.com/google/uuid"
+	Postgres "github.com/sam-caldwell/monorepo/go/db/postgres"
+	"testing"
+)
+
+// countById - delete a new avatar object
+func countById(t *testing.T, db *Postgres.Db, tableName string, entityId uuid.UUID) int {
+	var err error
+	var count int
+	var rows *sql.Rows
+	t.Run("count entities by id in table '"+tableName+"';", func(t *testing.T) {
+		rows, err = db.Query("select count(id) from %s where id=('%s');", tableName, entityId)
+		if err != nil {
+			t.Fatalf("count query failed %v teamId:  %v", err, entityId)
+		}
+		defer func() { _ = rows.Close() }()
+		if !rows.Next() {
+			t.Fatal("no row returned")
+		}
+		var count int
+		err = rows.Scan(&count)
+	})
+	return count
+}
