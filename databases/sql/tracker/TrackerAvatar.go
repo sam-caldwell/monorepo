50074d3697e5014e84941098482522c0b714bb69
Sam Caldwell <github@samcaldwell.net>
2023-12-15 05:13:45 -0600

    refactor TrackerAvatar to be consistent with SQL changes

--- databases/sql/tracker/TrackerAvatar.go
+++ databases/sql/tracker/TrackerAvatar.go
@@ -4,6 +4,7 @@ import "github.com/google/uuid"
 
 // TrackerAvatar - This is a struct representing the Tracker DB Avatar
 type TrackerAvatar struct {
-	Id  uuid.UUID `json:"id"`
-	Url string    `json:"url"`
+	Id       uuid.UUID `json:"id"`
+	Hash     string    `json:"hash"`
+	MimeType string    `json:"mimeType"`
 }
