ee74dce4be2e81fa76347b52de422875f8c30a74
Sam Caldwell <github@samcaldwell.net>
2023-12-15 02:25:06 -0600

    Minor improvements to sqldbtest.VerifyFunctionsStructure

--- go/db/sqldbtest/VerifyFunctionStructure.go
+++ go/db/sqldbtest/VerifyFunctionStructure.go
@@ -1,6 +1,7 @@
 package sqldbtest
 
 import (
+	"database/sql"
 	"fmt"
 	Postgres "github.com/sam-caldwell/monorepo/go/db/postgres"
 	"strings"
@@ -8,39 +9,47 @@ import (
 )
 
 func VerifyFunctionStructure(t *testing.T, db *Postgres.Db, functionName string, expected string) {
+	var err error
+	var rows *sql.Rows
+	t.Run("verify the function structure (params, return)", func(t *testing.T) {
+		query := fmt.Sprintf("SELECT "+
+			"proname AS fn, "+
+			"proargnames AS pn, "+
+			"("+
+			"SELECT array_agg(typname) "+
+			"FROM pg_type "+
+			"WHERE oid = ANY(proargtypes)"+
+			") AS pt, "+
+			"("+
+			"SELECT typname "+
+			"FROM pg_type "+
+			"WHERE oid = prorettype "+
+			") AS rt "+
+			"FROM pg_proc "+
+			"WHERE proname = lower('%s')", functionName)
 
-	query := fmt.Sprintf("SELECT "+
-		"proname AS fn, "+
-		"proargnames AS pn, "+
-		"("+
-		"SELECT array_agg(typname) "+
-		"FROM pg_type "+
-		"WHERE oid = ANY(proargtypes)"+
-		") AS pt, "+
-		"("+
-		"SELECT typname "+
-		"FROM pg_type "+
-		"WHERE oid = prorettype "+
-		") AS rt "+
-		"FROM pg_proc "+
-		"WHERE proname = lower('%s')", functionName)
+		rows, err = db.Query(query)
+		CheckError(t, err)
 
-	rows, err := db.Query(query)
-	CheckError(t, err)
-	defer func() {
-		CheckError(t, rows.Close())
-	}()
-	var fn, pn, pt, rt string
-	_ = rows.Next()
-	if err := rows.Scan(&fn, &pn, &pt, &rt); err != nil {
-		t.Fatalf("Error scanning row: %v", err)
-	}
-	actual := fmt.Sprintf("fn:%s,pn:%s,pt:%s,rt:%s", fn, pn, pt, rt)
-	if actual != strings.ToLower(expected) {
-		t.Fatalf("function structure mismatch\n"+
-			"actual:   %s\n"+
-			"expected: %s",
-			actual,
-			strings.ToLower(expected))
-	}
+		if !rows.Next() {
+			t.Fatalf("Fail: expected at least one row returned.")
+		}
+
+		var functionName, parameterNames, parameterTypes, returnTypes string
+
+		if err = rows.Scan(&functionName, &parameterNames, &parameterTypes, &returnTypes); err != nil {
+			t.Fatalf("failed scanning row: %v", err)
+		}
+
+		actual := fmt.Sprintf("fn:%s,pn:%s,pt:%s,rt:%s",
+			functionName, parameterNames, parameterTypes, returnTypes)
+
+		if actual != strings.ToLower(expected) {
+			t.Fatalf("fail: function structure mismatch\n"+
+				"actual:   %s\n"+
+				"expected: %s",
+				actual,
+				strings.ToLower(expected))
+		}
+	})
 }
