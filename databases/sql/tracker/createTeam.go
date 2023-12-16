ae9cc3f0c407f6b236661ed22b2748dbdf6e11a3
Sam Caldwell <github@samcaldwell.net>
2023-12-16 04:01:31 -0600

    code cleanup

--- databases/sql/tracker/createTeam.go
+++ databases/sql/tracker/createTeam.go
@@ -9,15 +9,15 @@ import (
 
 // createTeam - create a new team object
 func createTeam(t *testing.T, db *Postgres.Db, teamName string, iconId, ownerId uuid.UUID,
-	description string) (entityId uuid.UUID) {
+	owner, team, everyone, description string) (entityId uuid.UUID) {
 
 	var err error
 	var rows *sql.Rows
 
 	t.Run("call createUser();", func(t *testing.T) {
 
-		rows, err = db.Query("select createTeam('%s','%s','%s','read','read','read','%s');",
-			teamName, iconId, ownerId, description)
+		rows, err = db.Query("select createTeam('%s','%s','%s','%s','%s','%s','%s');",
+			teamName, iconId, ownerId, owner, team, everyone, description)
 
 		if err != nil {
 			t.Fatalf("Fail: function call failed: %v", err)
