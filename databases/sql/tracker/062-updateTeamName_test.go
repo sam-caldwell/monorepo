06c81553f90a5dabf6c2a3c7c89529ae0a8676b5
Sam Caldwell <github@samcaldwell.net>
2023-12-16 04:53:41 -0600

    fix typo

--- databases/sql/tracker/062-updateTeamName_test.go
+++ databases/sql/tracker/062-updateTeamName_test.go
@@ -10,17 +10,18 @@ import (
 
 func TestSqlDbFunc_updateTeamTeamName(t *testing.T) {
 	const (
-		avatarHash      = "ce9e67fb9ac29bcc831ee93a5bb9431063c4652704fb3a715826b2beef57c6e2"
+		avatarHash      = "ce9e67fb9ac29bcc831ee93a5bb9431063c4652704fb3a815826b2beef57c6e2"
 		avatarType      = "image/png"
 		oIconHash       = "880f67ded3d5c953348039c0bd575919455fc3e0a5cbcf1ac408c493cf53cdd4"
 		oIconType       = "image/png"
-		functionName    = "updateTeamTeamName"
+		functionName    = "updateTeamName"
 		userFirstName   = "Amy"
 		userLastName    = "Budwit"
 		userEmail       = "amy.budwitt1@example.com"
 		userPhone       = "123.991.3544"
 		userDescription = "A good mentor...missed"
-		teamName        = "Engineering Professionals"
+		oTeamName       = "Engineering Professionals1"
+		nTeamName       = "Engineering Professionals2"
 		teamDescription = "this is the team"
 		pRead           = "read"
 	)
@@ -50,10 +51,10 @@ func TestSqlDbFunc_updateTeamTeamName(t *testing.T) {
 	avatarId = createAvatar(t, db, avatarType, avatarHash)
 	IconId = createIcon(t, db, oIconType, oIconHash)
 	ownerId = createUser(t, db, userFirstName, userLastName, avatarId, userEmail, userPhone, userDescription)
-	teamId = createTeam(t, db, teamName, IconId, ownerId, pRead, pRead, pRead, teamDescription)
+	teamId = createTeam(t, db, oTeamName, IconId, ownerId, pRead, pRead, pRead, teamDescription)
 
-	if count := updateTeamOwner(t, db, teamId, ownerId); count != 1 {
+	if count := updateTeamName(t, db, teamId, nTeamName); count != 1 {
 		t.Fatalf("expect 1 but got %d", count)
 	}
-	verifyTeamRecord(t, db, teamId, teamName, IconId, ownerId, pRead, pRead, pRead, teamDescription)
+	verifyTeamRecord(t, db, teamId, nTeamName, IconId, ownerId, pRead, pRead, pRead, teamDescription)
 }
