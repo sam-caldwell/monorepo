5e064fcde9c2543a8e22bdfe1d42fa1c51eff1c7
Sam Caldwell <github@samcaldwell.net>
2023-12-16 03:41:38 -0600

    add generic updateTeamDescription() func for testing

---
+++ databases/sql/tracker/updateTeamDescription.go
@@ -0,0 +1,30 @@
+package psqlTrackerDb
+
+import (
+	"database/sql"
+	"github.com/google/uuid"
+	Postgres "github.com/sam-caldwell/monorepo/go/db/postgres"
+	"testing"
+)
+
+// updateTeamDescription - update the team description field
+func updateTeamDescription(t *testing.T, db *Postgres.Db, teamId uuid.UUID, description string) int {
+
+	var err error
+	var rows *sql.Rows
+	var count int
+
+	t.Run("update description", func(t *testing.T) {
+		rows, err = db.Query("select updateTeamDescription('%s','%s')", teamId, description)
+		if err != nil {
+			t.Fatalf("Fail: Query error: %v", err)
+		}
+		if !rows.Next() {
+			t.Fatal("Fail: no row returned")
+		}
+		if err = rows.Scan(&count); err != nil {
+			t.Fatal(err)
+		}
+	})
+	return count
+}
