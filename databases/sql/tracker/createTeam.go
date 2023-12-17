f282551afe872c040606498a091a63efba0eefce
Sam Caldwell <github@samcaldwell.net>
2023-12-17 02:15:44 -0600

    code cleanup

--- databases/sql/tracker/createTeam.go
+++ databases/sql/tracker/createTeam.go
@@ -1,42 +1,42 @@
 package psqlTrackerDb
 
 import (
-	"database/sql"
-	"github.com/google/uuid"
-	Postgres "github.com/sam-caldwell/monorepo/go/db/postgres"
-	"testing"
+    "database/sql"
+    "github.com/google/uuid"
+    Postgres "github.com/sam-caldwell/monorepo/go/db/postgres"
+    "testing"
 )
 
 // createTeam - create a new team object
 func createTeam(t *testing.T, db *Postgres.Db, teamName string, iconId, ownerId uuid.UUID,
-	owner, team, everyone, description string) (entityId uuid.UUID) {
+    owner, team, everyone, description string) (entityId uuid.UUID) {
 
-	var err error
-	var rows *sql.Rows
+    var err error
+    var rows *sql.Rows
 
-	t.Run("call createTeam();", func(t *testing.T) {
+    t.Run("call createTeam();", func(t *testing.T) {
 
-		rows, err = db.Query("select createTeam('%s','%s','%s','%s','%s','%s','%s');",
-			teamName, iconId, ownerId, owner, team, everyone, description)
+        rows, err = db.Query("select createTeam('%s','%s','%s','%s','%s','%s','%s');",
+            teamName, iconId, ownerId, owner, team, everyone, description)
 
-		if err != nil {
-			t.Fatalf("Fail: function call failed: %v", err)
-		}
+        if err != nil {
+            t.Fatalf("Fail: function call failed: %v", err)
+        }
 
-		defer func() { _ = rows.Close() }()
+        defer func() { _ = rows.Close() }()
 
-		if !rows.Next() {
-			t.Fatal("Fail: no row returned")
-		}
+        if !rows.Next() {
+            t.Fatal("Fail: no row returned")
+        }
 
-		err = rows.Scan(&entityId)
+        err = rows.Scan(&entityId)
 
-		if err != nil {
-			t.Fatalf("Failed to scan rows. %v", err)
-		}
+        if err != nil {
+            t.Fatalf("Failed to scan rows. %v", err)
+        }
 
-	})
+    })
 
-	return entityId
+    return entityId
 
 }
