acd6162d3dc09ba25935bd2bcf734d705aa5016e
Sam Caldwell <github@samcaldwell.net>
2023-12-17 01:37:50 -0600

    createTicketType() tests

--- databases/sql/tracker/102-createTicketType_test.go
+++ databases/sql/tracker/102-createTicketType_test.go
@@ -11,18 +11,22 @@ import (
 
 func TestSqlDbFunc_createTicketType(t *testing.T) {
 	const (
-		avatarUrl            = "http://localhost/myfakeavatar.jpeg"
-		iconUrl              = "http://localhost/myfakeicon.ico"
+		avatarHash           = "4ab7b2cbfa7a2120025400e1d08ace0ec81b9a27a5411b00e1ec75e74edb8f51"
+		avatarType           = "image/png"
+		iconHash             = "182e31fa48267c22d598dfcddb66e2dffd0b4ec2b0192e28c3b73336b71ea8b4"
+		iconType             = "image/png"
 		functionName         = "createTicketType"
-		expectedFirstName    = "John"
-		expectedLastName     = "Rackham"
-		expectedEmail        = "john.rackham@example.com"
-		expectedPhone        = "100.100.1000"
+		expectedFirstName    = "Jack"
+		expectedLastName     = "Cook"
+		expectedEmail        = "jack.cook@example.com"
+		expectedPhone        = "321.321.6543"
 		expectedDescription  = "Test description"
-		expectedTeamName     = "CalicoPirates"
-		expectedWorkflowName = "StealPlunderAndSail"
+		testTeamName         = "OceanExplorers"
+		expectedWorkflowName = "NavigationProcess"
 		expectedTicketType   = "epic"
+		pRead                = "read"
 	)
+
 	var avatarId uuid.UUID
 	var iconId uuid.UUID
 	var teamId uuid.UUID
@@ -33,165 +37,29 @@ func TestSqlDbFunc_createTicketType(t *testing.T) {
 	db := sqldbtest.InitializeTestDbConn(t)
 
 	t.Cleanup(func() {
-		// Note: we only clean up the avatar we expect to have created.
-		//       this should safeguard against an accidental run on prod.
-		_, _ = db.Query("delete from users where email='%s'", expectedEmail)
-		_, _ = db.Query("delete from teams where name='%s'", expectedTeamName)
-		_, _ = db.Query("delete from avatars where url='%s'", avatarUrl)
-		_, _ = db.Query("delete from icons where url='%s'", iconUrl)
-		_, _ = db.Query("delete from teamMembership where teamId='%s'", teamId)
-		err := db.Close()
-		sqldbtest.CheckError(t, err)
+		_ = cleanUpObject(db, "ticketTypes", ticketTypeId)
+		_ = cleanUpObject(db, "workflows", workflowId)
+		_ = cleanUpObject(db, "teams", teamId)
+		_ = cleanUpObject(db, "users", ownerId)
+		_ = cleanUpObject(db, "icons", iconId)
+		_ = cleanUpObject(db, "avatars", avatarId)
+		sqldbtest.CheckError(t, db.Close())
 	})
 
-	t.Run("verify the function structure (params, return)", func(t *testing.T) {
-		sqldbtest.VerifyFunctionStructure(t, db,
-			strings.ToLower(functionName),
-			fmt.Sprintf("fn:%s,"+
-				"pn:{name,iconid,workflowid,description},"+
-				"pt:{text,varchar,uuid},"+
-				"rt:uuid", strings.ToLower(functionName)))
-	})
+	sqldbtest.VerifyFunctionStructure(t, db,
+		strings.ToLower(functionName),
+		fmt.Sprintf("fn:%s,"+
+			"pn:{typeName,typeIconId,wid,typeDescription},"+
+			"pt:{text,varchar,uuid},"+
+			"rt:uuid", strings.ToLower(functionName)))
 
-	t.Run("call createAvatar()", func(t *testing.T) {
-		var rows *sql.Rows
-		var err error
-		rows, err = db.Query("select createAvatar('%s'::mimeType,'%s');", avatarHash, avatarType)
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer func() { _ = rows.Close() }()
-		if !rows.Next() {
-			t.Fatal("no row returned")
-		}
-		var raw string
-		err = rows.Scan(&raw)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if avatarId, err = uuid.Parse(raw); err != nil {
-			t.Fatal(err)
-		}
-		if avatarId.String() == "00000000-0000-0000-0000-000000000000" {
-			t.Fatal("illegal zero uuid")
-		}
-	})
-
-	t.Run("call createIcons()", func(t *testing.T) {
-		var rows *sql.Rows
-		var err error
-		rows, err = db.Query("select createIcons('%s');", iconUrl)
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer func() { _ = rows.Close() }()
-		if !rows.Next() {
-			t.Fatal("no row returned")
-		}
-		var raw string
-		err = rows.Scan(&raw)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if iconId, err = uuid.Parse(raw); err != nil {
-			t.Fatal(err)
-		}
-	})
-
-	t.Run("call createUser()", func(t *testing.T) {
-		var rows *sql.Rows
-		var err error
-		rows, err = db.Query("select createUser('%s','%s','%s','%s','%s','%s');",
-			expectedFirstName, expectedLastName, avatarId, expectedEmail, expectedPhone, expectedDescription)
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer func() { _ = rows.Close() }()
-		if !rows.Next() {
-			t.Fatal("no row returned")
-		}
-		var raw string
-		err = rows.Scan(&raw)
-		if ownerId, err = uuid.Parse(raw); err != nil {
-			t.Fatal(err)
-		}
-	})
-
-	t.Run("call createTeam()", func(t *testing.T) {
-		var rows *sql.Rows
-		var err error
-		rows, err = db.Query("select createTeam('%s','%s','%s','%s','%s','%s','%s');",
-			expectedTeamName, iconId, ownerId, "read", "read", "read", expectedDescription)
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer func() { _ = rows.Close() }()
-		if !rows.Next() {
-			t.Fatal("no row returned")
-		}
-		var raw string
-		err = rows.Scan(&raw)
-		if teamId, err = uuid.Parse(raw); err != nil {
-			t.Fatal(err)
-		}
-		t.Run("call addUserToTeam()", func(t *testing.T) {
-			var rows *sql.Rows
-			var err error
-			rows, err = db.Query("select addUserToTeam('%s','%s');", ownerId, teamId)
-			if err != nil {
-				t.Fatal(err)
-			}
-			defer func() { _ = rows.Close() }()
-			if !rows.Next() {
-				t.Fatal("no row returned")
-			}
-			var count int
-			err = rows.Scan(&count)
-			if count != 1 {
-				t.Fatalf("expected count 1 but got %d", count)
-			}
-		})
-		t.Run("verify user membership", func(t *testing.T) {
-			var rows *sql.Rows
-			var err error
-			rows, err = db.Query(""+
-				"select count(userId) "+
-				"from teamMembership "+
-				"where userId='%s' "+
-				"and teamId='%s';", ownerId, teamId)
-			if err != nil {
-				t.Fatal(err)
-			}
-			defer func() { _ = rows.Close() }()
-			if !rows.Next() {
-				t.Fatal("no row returned")
-			}
-			var count int
-			err = rows.Scan(&count)
-			if count != 1 {
-				t.Fatalf("expected count 1 but got %d", count)
-			}
-		})
-	})
-
-	t.Run("createWorkflow()", func(t *testing.T) {
-		var rows *sql.Rows
-		var err error
-		rows, err = db.Query("select createWorkflow('%s','%s','%s','%s','%s','%s','%s','%s');",
-			expectedWorkflowName, iconId, ownerId, teamId, "read", "read", "read", expectedDescription)
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer func() { _ = rows.Close() }()
-		if !rows.Next() {
-			t.Fatal("no row returned")
-		}
-		var raw string
-		err = rows.Scan(&raw)
-		if workflowId, err = uuid.Parse(raw); err != nil {
-			t.Fatal(err)
-		}
-	})
+	avatarId = createAvatar(t, db, avatarType, avatarHash)
+	iconId = createIcon(t, db, iconType, iconHash)
+	ownerId = createUser(t, db, expectedFirstName, expectedLastName, avatarId, expectedEmail,
+		expectedPhone, expectedDescription)
+	teamId = createTeam(t, db, testTeamName, iconId, ownerId, pRead, pRead, pRead, expectedDescription)
+	workflowId = createWorkflow(t, db, expectedWorkflowName, iconId, ownerId, teamId,
+		pRead, pRead, pRead, expectedDescription)
 
 	t.Run("createTicketType()", func(t *testing.T) {
 		var rows *sql.Rows
@@ -250,6 +118,5 @@ func TestSqlDbFunc_createTicketType(t *testing.T) {
 		if actualDescription != expectedDescription {
 			t.Fatalf("Description mismatch")
 		}
-
 	})
 }
