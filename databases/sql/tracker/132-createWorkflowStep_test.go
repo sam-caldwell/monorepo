9791215f4247bda71318c2f96748faa68ae70aac
Sam Caldwell <github@samcaldwell.net>
2023-12-18 17:51:52 -0600

    WIP: createWorkflowStep

---
+++ databases/sql/tracker/132-createWorkflowStep_test.go
@@ -0,0 +1,125 @@
+package psqlTrackerDb
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/sam-caldwell/monorepo/go/db/sqldbtest"
+	"strings"
+	"testing"
+)
+
+func TestSqlDbFunc_createWorkflowStep(t *testing.T) {
+	const (
+		avatarHash           = "4ab7b2cbfa7a2120025400e1d08ace0ec81b9a27a5411b00e1ec75e74e6b8f51"
+		avatarType           = "image/png"
+		iconHash             = "182e31fa48267c22d598dfcddb66e2dafd0b4ec2b0192e28c3b73336b71ea7b4"
+		iconType             = "image/png"
+		functionName         = "createWorkflowStep"
+		expectedFirstName    = "Billy"
+		expectedLastName     = "Gibbons"
+		expectedEmail        = "billy.gibbons@example.com"
+		expectedPhone        = "235.235.0248"
+		expectedDescription  = "Test description"
+		testTeamName         = "ZZ Top"
+		expectedProject      = "Blues"
+		expectedWorkflowName = "Haw haw haw"
+		expectedStepName     = "sing"
+		pRead                = "read"
+	)
+
+	var avatarId uuid.UUID
+	var iconId uuid.UUID
+	var teamId uuid.UUID
+	var ownerId uuid.UUID
+	var projectId uuid.UUID
+	var workflowId uuid.UUID
+	var ticketTypeId uuid.UUID
+	var prevStepId uuid.UUID //fake object
+	var nextStepId uuid.UUID //fake object
+	var stepId uuid.UUID
+
+	db := sqldbtest.InitializeTestDbConn(t)
+
+	t.Cleanup(func() {
+		_ = cleanUpObject(db, "workflowSteps", stepId)
+		_ = cleanUpObject(db, "ticketTypes", ticketTypeId)
+		_ = cleanUpObject(db, "workflows", workflowId)
+		_ = cleanUpObject(db, "projects", projectId)
+		_ = cleanUpObject(db, "teams", teamId)
+		_ = cleanUpObject(db, "users", ownerId)
+		_ = cleanUpObject(db, "icons", iconId)
+		_ = cleanUpObject(db, "avatars", avatarId)
+		sqldbtest.CheckError(t, db.Close())
+	})
+
+	sqldbtest.VerifyFunctionStructure(t, db,
+		strings.ToLower(functionName),
+		fmt.Sprintf("fn:%s,"+
+			"pn:{stepName,workflow,pStepId,nStepId,stepDescription},"+
+			"pt:{varchar,uuid,text},rt:uuid", strings.ToLower(functionName)))
+
+	t.Run("setup", func(t *testing.T) {
+		avatarId = createAvatar(t, db, avatarType, avatarHash)
+		iconId = createIcon(t, db, iconType, iconHash)
+		ownerId = createUser(t, db, expectedFirstName, expectedLastName, avatarId,
+			expectedEmail, expectedPhone, expectedDescription)
+		teamId = createTeam(t, db, testTeamName, iconId, ownerId, pRead, pRead, pRead, expectedDescription)
+		projectId = createProject(t, db, expectedProject, iconId, ownerId, teamId, pRead, pRead, pRead, expectedDescription)
+		workflowId = createWorkflow(t, db, expectedWorkflowName, iconId, ownerId, teamId, pRead, pRead, pRead,
+			expectedDescription)
+		prevStepId = createEntity(t, db, "workflow_step")
+		nextStepId = createEntity(t, db, "workflow_step")
+
+		stepId = createWorkflowStep(t, db, workflowId, expectedStepName, prevStepId, nextStepId, expectedDescription)
+	})
+
+	t.Run("verify", func(t *testing.T) {
+		var err error
+		var rows *sql.Rows
+
+		rows, err = db.Query(""+
+			"select id, workflowId, name, prevStepId, nextStepId, description "+
+			"from workflowSteps "+
+			"where id=%s", stepId)
+
+		if err != nil {
+			t.Fatalf("Fail: function call failed: %v", err)
+		}
+
+		defer func() { _ = rows.Close() }()
+
+		if !rows.Next() {
+			t.Fatal("Fail: no row returned")
+		}
+		var actualStepId uuid.UUID
+		var actualWorkflowId uuid.UUID
+		var actualName string
+		var actualPrevStepId uuid.UUID
+		var actualNextStepId uuid.UUID
+		var actualDescription string
+
+		err = rows.Scan(&actualStepId, &actualWorkflowId, &actualName, &actualPrevStepId, &actualNextStepId, &actualDescription)
+		if err != nil {
+			t.Fatalf("Failed to scan rows. %v", err)
+		}
+		if actualStepId != stepId {
+			t.Fatalf("stepId mismatch")
+		}
+		if actualWorkflowId != workflowId {
+			t.Fatalf("workflowId mismatch")
+		}
+		if actualName != expectedStepName {
+			t.Fatalf("expectedStepName mismatch")
+		}
+		if actualPrevStepId != prevStepId {
+			t.Fatalf("prevStepId mismatch")
+		}
+		if actualNextStepId != nextStepId {
+			t.Fatalf("nextStepId mismatch")
+		}
+		if actualDescription != expectedDescription {
+			t.Fatalf("expectedDescription mismatch")
+		}
+	})
+}
---
+++ databases/sql/tracker/createWorkflowStep.go
@@ -0,0 +1,31 @@
+package psqlTrackerDb
+
+import (
+	"database/sql"
+	"github.com/google/uuid"
+	Postgres "github.com/sam-caldwell/monorepo/go/db/postgres"
+	"testing"
+)
+
+// createWorkflowStep - create a new workflow step object in the database for testing
+func createWorkflowStep(t *testing.T, db *Postgres.Db, workflowId uuid.UUID, stepName string,
+	prevStepId, nextStepId uuid.UUID, description string) (entityId uuid.UUID) {
+
+	t.Run("createWorkflow()", func(t *testing.T) {
+		var err error
+		var rows *sql.Rows
+		rows, err = db.Query("select createWorkflowStep('%s','%s','%s','%s','%s');",
+			stepName, prevStepId, nextStepId, description, description)
+		if err != nil {
+			t.Fatalf("Fail: function call failed: %v", err)
+		}
+		defer func() { _ = rows.Close() }()
+		if !rows.Next() {
+			t.Fatal("Fail: no row returned")
+		}
+		if err = rows.Scan(&entityId); err != nil {
+			t.Fatalf("Failed to scan rows. %v", err)
+		}
+	})
+	return entityId
+}
