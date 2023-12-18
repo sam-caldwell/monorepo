7973c8e848cf9a0c787e09321f68a3db1e7dacc9
Sam Caldwell <github@samcaldwell.net>
2023-12-17 22:59:48 -0600

    remove whitespace

--- databases/sql/tracker/createTicketType.go
+++ databases/sql/tracker/createTicketType.go
@@ -15,26 +15,18 @@ func createTicketType(t *testing.T, db *Postgres.Db, name string, iconId, workfl
 	var rows *sql.Rows
 
 	t.Run("call createTicketType();", func(t *testing.T) {
-
 		rows, err = db.Query("select createTicketType('%v','%v','%v','%v');",
 			name, iconId, workflowId, description)
-
 		if err != nil {
 			t.Fatalf("Fail: function call failed: %v", err)
 		}
-
 		defer func() { _ = rows.Close() }()
-
 		if !rows.Next() {
 			t.Fatal("Fail: no row returned")
 		}
-
 		if err = rows.Scan(&entityId); err != nil {
 			t.Fatalf("Failed to scan rows. %v", err)
 		}
-
 	})
-
 	return entityId
-
 }
