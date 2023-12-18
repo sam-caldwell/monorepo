2ce7b57e61e52aa6ea92daa6338ab307f922aa87
Sam Caldwell <github@samcaldwell.net>
2023-12-17 20:49:29 -0600

    add getProjectById() test

---
+++ databases/sql/tracker/TrackerProject.go
@@ -0,0 +1,16 @@
+package psqlTrackerDb
+
+import "github.com/google/uuid"
+
+type TrackerProject struct {
+	Id          uuid.UUID `yaml:"id"`
+	IconId      uuid.UUID `yaml:"iconId"`
+	OwnerId     uuid.UUID `yaml:"ownerId"`
+	TeamId      uuid.UUID `yaml:"teamId"`
+	Created     string    `yaml:"created"`
+	Name        string    `yaml:"name"`
+	Owner       string    `yaml:"owner"`
+	Team        string    `yaml:"team"`
+	Everyone    string    `yaml:"everyone"`
+	Description string    `yaml:"description"`
+}
