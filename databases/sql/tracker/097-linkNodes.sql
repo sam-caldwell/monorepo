ee5380167d2ae4ebf9cfe321b54d060fe8319af6
Sam Caldwell <github@samcaldwell.net>
2023-12-19 14:50:39 -0600

    documentation

--- databases/sql/tracker/097-linkNodes.sql
+++ databases/sql/tracker/097-linkNodes.sql
@@ -4,6 +4,8 @@
  *
  * Given two nodes (LHS and RHS) representing a prev node (left hand) and next node (right hand)
  * this function will point lhs.nextId->rhs and rhs.prevId->lhs.
+ *
+ * This is needed when deleting workflow steps.
  */
 create or replace function linkNodes(lhs uuid, rhs uuid) returns boolean as
 $$
