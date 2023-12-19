2997f70b3ab876dc0372e63ce81e243b5021abd9
Sam Caldwell <github@samcaldwell.net>
2023-12-19 16:38:52 -0600

    Fix deleteWorkflowById() and tests

--- databases/sql/tracker/096-deleteWorkflowById.sql
+++ databases/sql/tracker/096-deleteWorkflowById.sql
@@ -9,8 +9,8 @@ declare
     count integer;
 begin
     -- ToDo: we should not be able to delete any workflows if it is mapped to a ticketType
-    delete from workflows where id = thisWorkflowId;
     delete from workflowsteps where workflowid=thisWorkflowId;
+    delete from workflows where id = thisWorkflowId;
     get diagnostics count = ROW_COUNT;
     return count;
 end;
--- databases/sql/tracker/096-deleteWorkflowById_test.go
+++ databases/sql/tracker/096-deleteWorkflowById_test.go
@@ -9,7 +9,6 @@ import (
 )
 
 func TestSqlDbFunc_deleteWorkflowById(t *testing.T) {
-	t.Skip("disabled for debugging")
 	const (
 		avatarHash           = "4ab7b2cbfa7a2120025400e1d08ace0ec81b9a27a5411b00e1ec75e74edb8f51"
 		avatarType           = "image/png"
@@ -45,7 +44,7 @@ func TestSqlDbFunc_deleteWorkflowById(t *testing.T) {
 	sqldbtest.VerifyFunctionStructure(t, db,
 		strings.ToLower(functionName),
 		fmt.Sprintf("fn:%s,"+
-			"pn:{workflowId},"+
+			"pn:{thisworkflowid},"+
 			"pt:{uuid},"+
 			"rt:int4", strings.ToLower(functionName)))
 
