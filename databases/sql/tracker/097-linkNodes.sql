ad2809a3b6aa80925532d7b6d5c3b8b3e758dd96
Sam Caldwell <github@samcaldwell.net>
2023-12-18 23:46:38 -0600

    add linkNodes()

---
+++ databases/sql/tracker/097-linkNodes.sql
@@ -0,0 +1,15 @@
+/*
+ * 097-linkNodes.sql
+ * (c) 2023 Sam Caldwell.  See License.txt
+ *
+ * Given two nodes (LHS and RHS) representing a prev node (left hand) and next node (right hand)
+ * this function will point lhs.nextId->rhs and rhs.prevId->lhs.
+ */
+create or replace function linkNodes(lhs uuid, rhs uuid) returns boolean as
+$$
+begin
+    execute updateWorkflowNextStep(lhs, rhs);
+    execute updateWorkflowPrevStep(rhs, lhs);
+    return true;
+end;
+$$ language plpgsql;
