0173b007978b30e8f17c906a514a498852ff024a
Sam Caldwell <github@samcaldwell.net>
2023-12-19 20:01:45 -0600

    implement pre check function

--- databases/sql/tracker/890-preChecks.sql
+++ databases/sql/tracker/890-preChecks.sql
@@ -7,28 +7,47 @@
 /*
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * deleteAvatarPreCheck() function
+ *      avatars cannot be deleted if any users exist.
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  */
 create or replace function deleteAvatarPreCheck(entityId uuid) returns boolean as
 $$
 begin
-    return (select count(id) from users where avatarId = entityId) == 0;
+    return (select count(id) from users where avatarId = entityId) = 0;
 end;
 $$ language plpgsql;
 /*
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * deleteIconPreCheck() function
+ *      icons cannot be deleted if any of the following exist using the icon...
+ *          - team
+ *          - workflow
+ *          - project
+ *          - ticket
+ *          - ticketType
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  */
 create or replace function deleteIconPreCheck(entityId uuid) returns boolean as
 $$
 begin
-    /*
-     * This is currently a placeholder.  This function should be overloaded later
-     * as record consumers are defined so that we can prevent any record here from being
-     * deleted until all related records are removed.
-     */
-    return ((select count(id) from teams where iconId = entityId) == 0) and
-           ((select count(id) from workflows where iconId = entityId) == 0);
+    return ((select count(id) from teams where iconId = entityId) = 0) and
+           ((select count(id) from workflows where iconId = entityId) = 0);
+end;
+$$ language plpgsql;
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * deleteUsersByIdPreCheck() function
+ *      users cannot be deleted if any related records exist in...
+ *          - teams
+ *          - teamMemberships (associating users and teams)
+ *          - workflows
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+create or replace function deleteUsersByIdPreCheck(entityId uuid) returns boolean as
+$$
+begin
+    return ((select count(id) from teams where ownerId = entityId) = 0) and
+           ((select count(id) from teammemberships where userId = entityId)) and
+           ((select count(id) from workflows where iconId = entityId) = 0);
 end;
 $$ language plpgsql;
