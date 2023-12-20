628ea499fb8381a012f80491036978888d9a3f97
Sam Caldwell <github@samcaldwell.net>
2023-12-19 19:37:53 -0600

    implement delete precheck for avatars

--- databases/sql/tracker/030-avatars.sql
+++ databases/sql/tracker/030-avatars.sql
@@ -47,14 +47,29 @@ $$
 declare
     count integer;
 begin
-    count := (select count(id) from users where avatarId = entityId);
-    if count > 0 then
-        count := 0;
-        raise exception 'cannot delete avatar if it is in use';
+    if deleteAvatarPreCheck(entityId) then
+        delete from avatars where id = entityId;
+        get diagnostics count = ROW_COUNT;
+        return count;
+    else
+        return 0; --error state
     end if;
-    delete from avatars where id = entityId;
-    get diagnostics count = ROW_COUNT;
-    return count;
+end;
+$$ language plpgsql;
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * deleteAvatarPreCheck() function
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+create or replace function deleteAvatarPreCheck(entityId uuid) returns boolean as
+$$
+begin
+    /*
+     * This is currently a placeholder.  This function should be overloaded later
+     * as record consumers are defined so that we can prevent any record here from being
+     * deleted until all related records are removed.
+     */
+    return true;
 end;
 $$ language plpgsql;
 /*
