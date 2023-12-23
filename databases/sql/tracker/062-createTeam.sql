a25067269f9c260729fb053845fbe00de584dbbf
Sam Caldwell <github@samcaldwell.net>
2023-12-22 22:09:54 -0600

    add permission validation to createTeam()

--- databases/sql/tracker/062-createTeam.sql
+++ databases/sql/tracker/062-createTeam.sql
@@ -2,15 +2,21 @@
  * 062-createTeam.sql
  * (c) 2023 Sam Caldwell.  See License.txt
  */
-create or replace function createTeam(tName varchar(64), tIcon uuid, tOwner uuid, pOwner permissions,
+create or replace function createTeam(newTeamName varchar(64), tIcon uuid, tOwner uuid, pOwner permissions,
                                       pTeam permissions, pEveryone permissions, tDescription text) returns uuid as
 $$
 declare
     teamId uuid;
 begin
+    if not validName(newTeamName) then
+        raise exception 'invalid team name';
+    end if;
+    if validatePermissionSet(pOwner, pTeam, pEveryone) then
+        raise exception 'invalid permission set';
+    end if;
     teamId := (select createEntity('team'::entityType));
     insert into teams (id, name, iconId, ownerId, owner, team, everyone, description)
-    values (teamId, tName, tIcon, tOwner, pOwner, pTeam, pEveryone, tDescription);
+    values (teamId, newTeamName, tIcon, tOwner, pOwner, pTeam, pEveryone, tDescription);
     return teamId;
 end;
 $$ language plpgsql;
