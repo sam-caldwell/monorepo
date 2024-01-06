99c395c823e693f3c412167e0fa4531e8b8795cb
Sam Caldwell <github@samcaldwell.net>
2024-01-06 05:40:21 -0600

    change snake case is_valid_email() to isValidEmail() to conform to standards

--- databases/sql/tracker/010-isValidEmail.sql
+++ databases/sql/tracker/010-isValidEmail.sql
@@ -2,7 +2,7 @@
  * 010-isValidEmail.sql
  * (c) 2023 Sam Caldwell.  See License.txt
  */
-CREATE OR REPLACE FUNCTION is_valid_email(inpEmail VARCHAR) RETURNS BOOLEAN AS $$
+CREATE OR REPLACE FUNCTION isValidEmail(inpEmail VARCHAR) RETURNS BOOLEAN AS $$
 DECLARE
     email_pattern VARCHAR := '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$';
 BEGIN
