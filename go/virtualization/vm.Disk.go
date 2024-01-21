4786bf5660ff209d6aa134f3638eb5ae454ad6e7
Sam Caldwell <github@samcaldwell.net>
2024-01-21 13:32:32 -0600

    adding whitespace for readability

--- go/virtualization/vm.Disk.go
+++ go/virtualization/vm.Disk.go
@@ -9,19 +9,29 @@ import (
 
 func (vm *VM) Disk(name string, sz size.Memory, format diskFormat.DiskFormat) *VM {
 	if vm.readOnly {
+
 		vm.errors.Push(fmt.Errorf("readonly violation (disk)"))
+
 	} else {
+
 		var disk vmDisk.DiskDescriptor
+
 		if err := disk.Name(name); err != nil {
 			vm.errors.Push(err)
 		}
+
 		if err := disk.Size(sz * size.MB); err != nil {
 			vm.errors.Push(err)
 		}
+
 		if err := disk.Format(format); err != nil {
 			vm.errors.Push(err)
 		}
+
 		vm.disks = append(vm.disks, disk)
+
 	}
+
 	return vm
+
 }
