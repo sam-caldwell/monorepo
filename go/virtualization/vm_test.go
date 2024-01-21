737108f7f147b2800066ccfbbe4e159bdbdc0055
Sam Caldwell <github@samcaldwell.net>
2024-01-21 10:27:33 -0600

    add vm_test.go

---
+++ go/virtualization/vm_test.go
@@ -0,0 +1,33 @@
+package virtualization
+
+import (
+	"github.com/sam-caldwell/monorepo/go/types/size"
+	"github.com/sam-caldwell/monorepo/go/virtualization/vmDisk/diskFormat"
+	"testing"
+)
+
+func TestVm(t *testing.T) {
+
+	t.Run("Setup the vm object only", func(t *testing.T) {
+
+		vm := NewVm().
+			Name("myTestVm").
+			Cpu(2).
+			Ram(1024*size.MB).
+			Image("{{home}}/vmImages/{{name}}").
+			Iso("{{home}}/iso/ubuntu.iso").
+			Disk("opsysRoot", 10*size.GB, diskFormat.Ext4).
+			Disk("swap", 1*size.GB, diskFormat.Swap)
+
+		errors := vm.GetErrors()
+
+		for n, err := range errors {
+			t.Errorf("error[%d]: %v", n, err)
+		}
+
+		if len(errors) != 0 {
+			t.Fatal("has errors")
+		}
+
+	})
+}
