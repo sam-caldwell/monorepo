2d172d4eb793ca1aacdf81dfe6500398d6191300
Sam Caldwell <github@samcaldwell.net>
2024-01-21 12:45:23 -0600

    fix tests for file.HasExtension() and add case insensitivity to extension comparison

--- go/fs/file/HasExtension.go
+++ go/fs/file/HasExtension.go
@@ -1,6 +1,9 @@
 package file
 
+import "strings"
+
 // HasExtension - return boolean result if a YAML extension exists
 func HasExtension(filename, extension string) bool {
+	extension = strings.ToLower(extension)
 	return GetExtension(filename) == extension
 }
--- go/fs/file/HasExtension_test.go
+++ go/fs/file/HasExtension_test.go
@@ -5,33 +5,37 @@ import (
 )
 
 func TestHasExtension(t *testing.T) {
-	t.Run("File with matching extension", func(t *testing.T) {
-		filename := "file.yaml"
-		extension := ".yaml"
-		result := HasExtension(filename, extension)
-
-		if !result {
-			t.Error("Expected true, but got false")
-		}
-	})
-
-	t.Run("File with different extension", func(t *testing.T) {
-		filename := "file.txt"
-		extension := ".yaml"
-		result := HasExtension(filename, extension)
-
-		if result {
-			t.Error("Expected false, but got true")
-		}
-	})
-
-	t.Run("File with uppercase extension", func(t *testing.T) {
-		filename := "file.YAML"
-		extension := ".yaml"
-		result := HasExtension(filename, extension)
-
-		if !result {
-			t.Error("Expected true, but got false")
-		}
-	})
+	testFunc := func(index int, fileName, extension string, expectedResult bool) {
+		t.Run("File with matching extension", func(t *testing.T) {
+			if actualResult := HasExtension(fileName, extension); actualResult != expectedResult {
+				t.Errorf("%d Expected %v, but got %v on '%s' with '%v'",
+					index, expectedResult, actualResult, fileName, extension)
+			}
+		})
+	}
+	type TestData struct {
+		name      string
+		extension string
+		result    bool
+	}
+	testData := []TestData{
+		{"file.yaml", ".yaml", true},
+		{"file.yaml", ".YAML", true},
+		{"file.json", ".json", true},
+		{"file.yml", ".yml", true},
+		{"file.iso", ".iso", true},
+		{"file.iso", "iso", false},
+		{"file.yml", "yml", false},
+		{"file.yaml", "yaml", false},
+		{"file.yaml", "YAML", false},
+		{"file.json", "json", false},
+		{"file", "yaml", false},
+		{"file", "yml", false},
+		{"file", "json", false},
+		{"file", "iso", false},
+		{"file", "", true},
+	}
+	for n, row := range testData {
+		testFunc(n, row.name, row.extension, row.result)
+	}
 }
