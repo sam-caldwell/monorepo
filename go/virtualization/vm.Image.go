be080c700ec35c5c5fa4d0d2e7c5f232fc917311
Sam Caldwell <github@samcaldwell.net>
2024-01-21 09:42:29 -0600

    substitute {{home}} and {{name}} for .Iso() and .Image()

--- go/virtualization/vm.Image.go
+++ go/virtualization/vm.Image.go
@@ -1,19 +1,37 @@
 package virtualization
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/sam-caldwell/monorepo/go/environment"
+	"strings"
+)
 
 // Image - Set the virtual machine image filename/path and return *VM to allow method chaining.
 func (vm *VM) Image(name string) *VM {
 
 	if vm.readOnly {
+
 		vm.errors.Push(fmt.Errorf("readonly violation (image)"))
+
 	} else {
+
+		envHome, err := environment.RequireString("HOME")
+
+		if err != nil {
+			vm.errors.Push(err)
+		}
+
+		name = strings.Replace(name, "{{name}}", vm.GetName(), -1)
+
+		name = strings.Replace(name, "{{home}}", envHome, -1)
+
 		if err := vm.image.Set(name); err != nil {
 
 			vm.errors.Push(err)
 
 		}
 	}
+
 	return vm
 
 }
--- go/virtualization/vm.Iso.go
+++ go/virtualization/vm.Iso.go
@@ -1,9 +1,37 @@
 package virtualization
 
+import (
+	"fmt"
+	"github.com/sam-caldwell/monorepo/go/environment"
+	"github.com/sam-caldwell/monorepo/go/fs/file/extensions"
+	"strings"
+)
+
 // Iso - Specify the filename/path of the iso image.
 func (vm *VM) Iso(fileName string) *VM {
-	if err := vm.iso.SetWithExtension(fileName, []string{".iso"}); err != nil {
-		vm.errors.Push(err)
+
+	if vm.readOnly {
+
+		vm.errors.Push(fmt.Errorf("readonly violation (iso)"))
+
+	} else {
+
+		envHome, err := environment.RequireString("HOME")
+
+		if err != nil {
+			vm.errors.Push(err)
+		}
+
+		fileName = strings.Replace(fileName, "{{name}}", vm.GetName(), -1)
+
+		fileName = strings.Replace(fileName, "{{home}}", envHome, -1)
+
+		if err := vm.iso.SetWithExtension(fileName, []string{extensions.ISO}); err != nil {
+			vm.errors.Push(err)
+		}
+
 	}
+
 	return vm
+
 }
