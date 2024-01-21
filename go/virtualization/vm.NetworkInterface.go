96a97e9ff2c7be5944b4b0fd6526cfb305e313cd
Sam Caldwell <github@samcaldwell.net>
2024-01-21 14:08:14 -0600

    add vm.NetworkInterface() and test

--- go/virtualization/vm.NetworkInterface.go
+++ go/virtualization/vm.NetworkInterface.go
@@ -1,16 +1,32 @@
 package virtualization
 
-import "github.com/sam-caldwell/monorepo/go/virtualization/vmNet"
+import (
+	"fmt"
+	"github.com/sam-caldwell/monorepo/go/virtualization/vmNet"
+)
 
 // NetworkInterface - add a new network interface to the virtual machine definition
 func (vm *VM) NetworkInterface(name string, interfaceType vmNet.InterfaceType) *VM {
-	var net vmNet.NetworkInterfaceDescriptor
-	if err := net.Name(name); err != nil {
-		vm.errors.Push(err)
+	if vm.readOnly {
+
+		vm.errors.Push(fmt.Errorf("readonly violation (NetworkInterface)"))
+
+	} else {
+
+		var net vmNet.NetworkInterfaceDescriptor
+
+		if err := net.Name(name); err != nil {
+			vm.errors.Push(err)
+		}
+
+		if err := net.Type(interfaceType); err != nil {
+			vm.errors.Push(err)
+		}
+
+		vm.networkInterfaces = append(vm.networkInterfaces, net)
+
 	}
-	if err := net.Type(interfaceType); err != nil {
-		vm.errors.Push(err)
-	}
-	vm.networkInterfaces = append(vm.networkInterfaces, net)
+
 	return vm
+
 }
--- go/virtualization/vm.NetworkInterface_test.go
+++ go/virtualization/vm.NetworkInterface_test.go
@@ -1,1 +1,82 @@
 package virtualization
+
+import (
+	"github.com/sam-caldwell/monorepo/go/virtualization/vmNet"
+	"testing"
+)
+
+func TestVM_NetworkInterface(t *testing.T) {
+	t.Run("readonly protections", func(t *testing.T) {
+		var vm VM
+		if vm.readOnly {
+			t.Fatalf("expect readonly false")
+		}
+		vm.readOnly = true
+		if !vm.readOnly {
+			t.Fatalf("expect readonly true")
+		}
+		if p := vm.NetworkInterface("eth0", vmNet.Bridge); p != &vm {
+			t.Fatalf("expect address of vm object")
+		}
+		if vm.errors.Size() == 0 {
+			t.Fatalf("expect an error on readonly violation")
+		}
+		if err := vm.errors.Dump(); err[0].Error() != "readonly violation (NetworkInterface)" {
+			t.Fatalf("error message mismatch")
+		}
+	})
+	t.Run("initial state", func(t *testing.T) {
+		var vm VM
+		if len(vm.networkInterfaces) != 0 {
+			t.Fatalf("expected no network interfaces")
+		}
+		if vm.errors.Size() != 0 {
+			t.Fatalf("expect no errors")
+		}
+	})
+	t.Run("happy path: create one interface", func(t *testing.T) {
+		var vm VM
+		vm.NetworkInterface("eth0", vmNet.Bridge).
+			NetworkInterface("eth1", vmNet.NAT).
+			NetworkInterface("eth2", vmNet.Bridge)
+
+		if vm.errors.Size() != 0 {
+			t.Fatalf("expected zero errors")
+		}
+		if len(vm.networkInterfaces) != 3 {
+			t.Fatalf("expected 3 interfaces")
+		}
+		if i := vm.GetNetworkInterface(0); i.GetName() != "eth0" {
+			t.Fatalf("interface name mismatch 0")
+		}
+		if i := vm.GetNetworkInterface(1); i.GetName() != "eth1" {
+			t.Fatalf("interface name mismatch 1")
+		}
+		if i := vm.GetNetworkInterface(2); i.GetName() != "eth2" {
+			t.Fatalf("interface name mismatch 2")
+		}
+		if i := vm.GetNetworkInterface(0); i.GetType() != vmNet.Bridge {
+			t.Fatalf("interface type mismatch 0")
+		}
+		if i := vm.GetNetworkInterface(1); i.GetType() != vmNet.NAT {
+			t.Fatalf("interface type mismatch 1")
+		}
+		if i := vm.GetNetworkInterface(2); i.GetType() != vmNet.Bridge {
+			t.Fatalf("interface type mismatch 2")
+		}
+	})
+	t.Run("sad path: bad interface name", func(t *testing.T) {
+		var vm VM
+		_ = vm.NetworkInterface("bad.eth0", vmNet.Bridge)
+		if vm.errors.Size() < 1 {
+			t.Fatalf("expected one error")
+		}
+		_ = vm.NetworkInterface("bad.eth1", vmNet.Bridge)
+		if vm.errors.Size() < 2 {
+			t.Fatalf("expected two errors")
+		}
+		if i := vm.errors.Dump(); i[0].Error() != "invalid name" {
+			t.Fatalf("error mismatch: %s", i[0].Error())
+		}
+	})
+}
