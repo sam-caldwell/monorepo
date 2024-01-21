cc6a844dd19a0f0794361d18c14ed9edb95a0f70
Sam Caldwell <github@samcaldwell.net>
2024-01-21 12:47:42 -0600

    Add better comment to file.HasExtension()

--- go/fs/file/HasExtension.go
+++ go/fs/file/HasExtension.go
@@ -2,7 +2,10 @@ package file
 
 import "strings"
 
-// HasExtension - return boolean result if a YAML extension exists
+// HasExtension - return boolean result if a given extension exists on the provided filename.
+//
+// The extension must start with a period (.) character, and the extension is case insensitive
+// for the comparison.
 func HasExtension(filename, extension string) bool {
 	extension = strings.ToLower(extension)
 	return GetExtension(filename) == extension
