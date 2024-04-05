f9e6e54f39c7782dfe225139f2cf5bb52a80b39f
Sam Caldwell <github@samcaldwell.net>
2024-04-05 07:59:52 -0500

    fix Fqdn.FromString() to accept IPv4, IPv6 or FQDN

--- go/net/Fqdn.go
+++ go/net/Fqdn.go
@@ -10,7 +10,7 @@ type Fqdn string
 // FromString - Given a string pointer, convert it to an FQDN
 func (o *Fqdn) FromString(v *string) (err error) {
 
-	if IsValidFqdn(*v) {
+	if IsValidAddress(*v) {
 		*o = Fqdn(*v)
 	} else {
 		err = fmt.Errorf("FQDN cannot be empty")
