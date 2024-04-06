4b1897c97e51b4b284400971ea3e1b95e1517fc8
Sam Caldwell <github@samcaldwell.net>
2024-04-05 21:39:05 -0500

    add portNumber.FromString()

--- go/net/PortNumber_test.go
+++ go/net/PortNumber_test.go
@@ -6,12 +6,15 @@ import (
 
 func TestPortNumber_FromInt(t *testing.T) {
 	var n PortNumber
-	for i := uint(0); i < 65535; i++ {
+	for i := int(0); i < 65535; i++ {
 		if err := n.FromInt(i); err != nil {
 			t.Fatalf("i:%d ->%v", i, err)
 		}
 	}
-	if err := n.FromInt(uint(65536)); err == nil && err.Error() != "invalid number must be 0...65535" {
+	if err := n.FromInt(int(-1)); err == nil && err.Error() != "invalid number must be 0...65535" {
+		t.Fatal("expected overflow error")
+	}
+	if err := n.FromInt(int(65536)); err == nil && err.Error() != "invalid number must be 0...65535" {
 		t.Fatal("expected overflow error")
 	}
 }
