7c7fda6d69bc490f19af72c215c256126ebe9ba9
Sam Caldwell <github@samcaldwell.net>
2024-04-06 07:59:21 -0500

    add debuggable server.SendEvent() WIP

--- go/tools/osRecon/server/SendEvent.go
+++ go/tools/osRecon/server/SendEvent.go
@@ -1,10 +1,22 @@
 package server
 
 import (
+	"github.com/sam-caldwell/monorepo/go/ansi"
 	"github.com/sam-caldwell/monorepo/go/types"
 )
 
 // SendEvent - Send an event object to the server
-func (svr *Server) SendEvent(message types.Event) *Server {
+func (svr *Server) SendEvent(event types.Event) *Server {
+	//Only attempt to send if error state is clear
+	if svr.err == nil {
+		//ToDo: Send the event to the server
+		ansi.Cyan().
+			Printf("Debug (SendEvent):%v", event.ToString()).
+			LF().
+			Reset()
+		//ToDo: set err state if error occurred in send.
+		svr.err = nil
+	}
+
 	return svr
 }
