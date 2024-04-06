10d7983d2cbfdbffae663f016256f486fcf7db0d
Sam Caldwell <github@samcaldwell.net>
2024-04-06 07:44:48 -0500

    add event send times to types.Event

--- go/types/Events.go
+++ go/types/Events.go
@@ -1,14 +1,16 @@
 package types
 
 import (
-	"time"
+    "time"
 )
 
+// Event - Struct describes a single event captured by the system
 type Event struct {
-	// This object defines an event when it is pushed into the eventQueue
-	// serialized for transmission.
-	Time     time.Time
-	Resource ResourceId
-	Field    FieldId
-	Value    []byte
+    // This object defines an event when it is pushed into the eventQueue
+    // serialized for transmission.
+    EventTime time.Time  //Event occurred when?
+    SendTime  time.Time  //Event sent to server when?
+    Resource  ResourceId //numeric resource Id
+    Field     FieldId    //numeric field Id
+    Value     []byte     //serialized value
 }
