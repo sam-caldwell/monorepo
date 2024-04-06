05fe8eab8cff8fc3df1a4768d4ea73411557667b
Sam Caldwell <github@samcaldwell.net>
2024-04-05 21:40:17 -0500

    add main()

---
+++ go/tools/osRecon/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/sam-caldwell/monorepo/go/ansi"
+	"github.com/sam-caldwell/monorepo/go/exit"
+	"github.com/sam-caldwell/monorepo/go/tools/osRecon/application"
+	"github.com/sam-caldwell/monorepo/go/types"
+)
+
+func main() {
+
+	var app application.Application
+
+	ansi.Blue().Println("Starting...").Reset()
+
+	exit.TerminateOnError(app.Configure())
+
+	switch app.Mode {
+	case types.Client:
+		exit.TerminateOnError(app.Emitter())
+		exit.TerminateOnError(app.CheckIn())
+		exit.TerminateOnError(app.ProcMon())
+		exit.TerminateOnError(app.NetMon())
+		exit.TerminateOnError(app.HwMon())
+		exit.TerminateOnError(app.FsMon())
+		exit.TerminateOnError(app.SysMon())
+		exit.TerminateOnError(app.Query())
+	case types.Server:
+		exit.TerminateOnError(app.Collector())
+		exit.TerminateOnError(app.QuerySender())
+	default:
+		ansi.Red().Println("Error: Invalid Mode").Fatal(exit.GeneralError).Reset()
+	}
+
+	exit.TerminateOnError(app.SignalHandler())
+
+	ansi.Green().Println("Terminating").Reset()
+
+}
