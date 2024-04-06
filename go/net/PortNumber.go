077dee317ef4df7223ba3dd4c26ec3d50c5f874d
Sam Caldwell <github@samcaldwell.net>
2024-04-05 21:38:10 -0500

    add portNumber.FromString()

--- go/net/PortNumber.go
+++ go/net/PortNumber.go
@@ -1,14 +1,27 @@
 package net
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 // PortNumber - Network port number (0-65535)
 type PortNumber uint16
 
-func (o *PortNumber) FromInt(v uint) error {
-	if v > 65535 {
+// FromInt - Get port number from integer
+func (o *PortNumber) FromInt(v int) error {
+	if v < 0 || v > 65535 {
 		return fmt.Errorf("invalid number must be 0...65535")
 	}
 	*o = PortNumber(v)
 	return nil
+}
+
+// FromString - Get port number from string
+func (o *PortNumber) FromString(v string) error {
+	n, err := strconv.Atoi(v)
+	if err != nil {
+		return err
+	}
+	return o.FromInt(n)
 }
