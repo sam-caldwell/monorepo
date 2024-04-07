7cdf063c4296b0ebf203fd452a071dd19e871413
Sam Caldwell <github@samcaldwell.net>
2024-04-07 16:19:54 -0500

    starting Connect()

--- go/tools/osRecon/server/Connect.go
+++ go/tools/osRecon/server/Connect.go
@@ -2,5 +2,8 @@ package server
 
 // Connect - Connect to the server and authenticate the connection.
 func (svr *Server) Connect() *Server {
+	//ToDo: create a persistent connection between client and server
+	//      note that if the connection already exists and is okay,
+	//      we will just reuse the existing connection.
 	return svr
 }
