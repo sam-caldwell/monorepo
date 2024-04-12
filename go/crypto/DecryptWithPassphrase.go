aa3fa6247afd91e5a8a8f7cbd5e3d52840f43306
Sam Caldwell <github@samcaldwell.net>
2024-04-12 00:15:53 -0500

    add some GC calls to clear memory

--- go/crypto/DecryptWithPassphrase.go
+++ go/crypto/DecryptWithPassphrase.go
@@ -2,10 +2,12 @@ package crypto
 
 import (
 	helper "github.com/ProtonMail/gopenpgp/v2/helper"
+	"runtime"
 )
 
 // DecryptWithPassphrase - Decrypt a cipher text with a given passphrase.
 func DecryptWithPassphrase(cipher, passphrase string) (clearText string, err error) {
 	// just an abstraction point from upstream
+	defer runtime.GC()
 	return helper.DecryptMessageWithPassword([]byte(passphrase), cipher)
 }
--- go/crypto/EncryptWithPassphrase.go
+++ go/crypto/EncryptWithPassphrase.go
@@ -2,10 +2,12 @@ package crypto
 
 import (
 	helper "github.com/ProtonMail/gopenpgp/v2/helper"
+	"runtime"
 )
 
 // EncryptWithPassphrase - Encrypt a clearText string with a given passphrase.
 func EncryptWithPassphrase(clearText, passphrase string) (cipher string, err error) {
 	// just an abstraction point from upstream
+	defer runtime.GC()
 	return helper.EncryptMessageWithPassword([]byte(passphrase), clearText)
 }
