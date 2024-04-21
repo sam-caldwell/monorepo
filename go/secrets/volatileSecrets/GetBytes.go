7146e21671fededcad500b7d83a4c6bb8a9de5af
Sam Caldwell <github@samcaldwell.net>
2024-04-21 00:17:32 -0500

    remove debugging logs from secrets/*

--- go/secrets/volatileSecrets/GetBytes.go
+++ go/secrets/volatileSecrets/GetBytes.go
@@ -3,7 +3,6 @@ package volatileSecrets
 import (
 	"encoding/binary"
 	"github.com/sam-caldwell/monorepo/go/crypto"
-	"log"
 )
 
 // GetBytes - Retrieve the secret in memory (yeah...this will put it in cleartext in memory for a time).
@@ -15,35 +14,34 @@ func (p *Password) GetBytes() []byte {
 	szStart := uint(0)
 	szStop := szStart + secretSzLen
 	secretSz := uint(binary.BigEndian.Uint32(p.data[szStart:szStop]))
-	log.Printf(""+
-		"    szStart: %04d "+
-		"     szStop: %04d [ v:%d]", szStart, szStop, secretSz)
+	//log.Printf(""+
+	//	"    szStart: %04d "+
+	//	"     szStop: %04d [ v:%d]", szStart, szStop, secretSz)
 	//
 	// get the start and stop boundary for the key/passphrase
 	//
 	keyStart := paddingA + szStop
 	keyStop := keyStart + keySz
-	log.Printf(""+
-		"   keyStart: %04d "+
-		"    keyStop: %04d [sz:%d]", keyStart, keyStop, keyStop-keyStart)
-
+	//log.Printf(""+
+	//	"   keyStart: %04d "+
+	//	"    keyStop: %04d [sz:%d]", keyStart, keyStop, keyStop-keyStart)
 	//
 	// get the start and stop boundary for the secret
 	//
 	secretStart := keyStop + paddingB
 	secretStop := secretStart + secretSz
-	log.Printf(""+
-		"secretStart: %04d "+
-		" secretStop: %04d [sz:%d]", secretStart, secretStop, secretStop-secretStart)
+	//log.Printf(""+
+	//	"secretStart: %04d "+
+	//	" secretStop: %04d [sz:%d]", secretStart, secretStop, secretStop-secretStart)
 	//
 	// decrypt the secret using the passphrase
 	//
-	log.Printf("inputs:\n"+
-		"\t   key: (%04d) %02x\n"+
-		"\tcipher: %02x\n",
-		len(p.data[keyStart:keyStop]),
-		p.data[keyStart:keyStop],
-		p.data[secretStart:secretStop])
+	//log.Printf("inputs:\n"+
+	//	"\t    key: (%04d) %02x\n"+
+	//	"\t cipher: %02x\n",
+	//	len(p.data[keyStart:keyStop]),
+	//	p.data[keyStart:keyStop],
+	//	p.data[secretStart:secretStop])
 	s, err := crypto.DecryptWithPassphrase(p.data[secretStart:secretStop], p.data[keyStart:keyStop])
 	if err != nil {
 		panic(err)
