5f1b06f2af75a41234cd652164132d65059d8261
Sam Caldwell <github@samcaldwell.net>
2024-04-20 22:31:08 -0500

    WIP: working through volatileSecrets.Get{Bytes}()

--- go/secrets/volatileSecrets/constants.go
+++ go/secrets/volatileSecrets/constants.go
@@ -3,6 +3,6 @@ package volatileSecrets
 import "github.com/sam-caldwell/monorepo/go/types/hashes"
 
 const (
-	secretSzLen uint = 32 / 8                  //number bytes used to encode the secret size
-	keySz       uint = hashes.Sha512Length * 2 // 64 bytes * 2char/byte = 128
+	secretSzLen uint = 32 / 8              //number bytes used to encode the secret size
+	keySz       uint = hashes.Sha512Length // 64 bytes
 )
