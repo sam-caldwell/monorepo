5858cea0299bac9da52b2bc600ae6e148e6f07d1
Sam Caldwell <github@samcaldwell.net>
2024-04-23 00:04:00 -0500

    clean up

--- go/tools/osRecon/cli/GetMode.go
+++ go/tools/osRecon/cli/GetMode.go
@@ -5,7 +5,6 @@ import (
 	"github.com/sam-caldwell/monorepo/go/exit/errors"
 	"github.com/sam-caldwell/monorepo/go/types"
 	"os"
-	"strings"
 )
 
 // GetMode - Get the mode (os.Args[1]) and expect client or server
@@ -15,10 +14,6 @@ func GetMode() (m types.OperatingMode, err error) {
 
 	if len(os.Args) < 2 {
 		return m, fmt.Errorf(errors.MissingArguments)
-	}
-
-	if command = strings.TrimSpace(os.Args[1]); (command != "client") && (command != "server") {
-		return m, fmt.Errorf(errors.InvalidCommand)
 	}
 
 	err = m.FromString(command)
