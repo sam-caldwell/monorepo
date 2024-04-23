e3355bff2650d7994d612ce76148a938dd8b6b03
Sam Caldwell <github@samcaldwell.net>
2024-04-23 00:04:54 -0500

    cleanup

--- go/simpleArgs/GetCommand.go
+++ go/simpleArgs/GetCommand.go
@@ -2,7 +2,7 @@ package simpleArgs
 
 import (
 	"fmt"
-	exit2 "github.com/sam-caldwell/monorepo/go/exit"
+	"github.com/sam-caldwell/monorepo/go/exit"
 	"github.com/sam-caldwell/monorepo/go/exit/errors"
 	"github.com/sam-caldwell/monorepo/go/version"
 	"os"
@@ -10,9 +10,9 @@ import (
 
 // GetCommand - Given commandline args (os.Args) return the parsed command or handle --help and --version
 func GetCommand(helpText string) (command string) {
-	exit2.OnCondition(len(os.Args) < 2, exit2.InvalidInput, errors.MissingArguments, helpText)
+	exit.OnCondition(len(os.Args) < 2, exit.InvalidInput, errors.MissingArguments, helpText)
 	command = os.Args[1]
-	exit2.OnCondition((command == "--help") || (command == "-h"), 0, "Help", helpText)
+	exit.OnCondition((command == "--help") || (command == "-h"), 0, "Help", helpText)
 	if command == "--version" {
 		fmt.Println(version.Version)
 		os.Exit(0)
