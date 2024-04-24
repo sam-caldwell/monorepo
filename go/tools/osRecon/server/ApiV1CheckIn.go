a128390317972a8d318e68e7bf463d6ea9897d50
Sam Caldwell <github@samcaldwell.net>
2024-04-23 23:25:28 -0500

    WIP: /api/v1/CheckIn endpoint handler

--- go/tools/osRecon/server/ApiV1CheckIn.go
+++ go/tools/osRecon/server/ApiV1CheckIn.go
@@ -1,23 +1,31 @@
 package server
 
 import (
-	"net/http"
+    "github.com/google/uuid"
+    "log"
+    "net/http"
 )
 
 // ApiV1CheckIn - Provide payload for the Server Check-in API endpoint (ApiV1CheckIn).
 func (svr *Server) ApiV1CheckIn(w http.ResponseWriter, r *http.Request) {
+	const (
+		queryFetchCount uint16 = 5
+	)
 	if r.Method == http.MethodGet {
-		//ToDo: log the checkin from the client as its heartbeat.
+		var clientId uuid.UUID
 
+		if err := getClientId(r, &clientId); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			_, _ = w.Write([]byte("Bad Request"))
+			return
+		}
+
+		if err := svr.queryQueue.FetchQueryList(clientId, queryFetchCount); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte("QueryFetchError()"))
+			log.Printf("QueryFetchError: %v", err)
+		}
+		//ToDo: log the heartbeat with the clientId
 	}
 	//ToDo: create API endpoint to handle healthcheck / query queue polling
-	/*
-	 * The server needs a directory where queries can be added (one per file)
-	 * and a client can request the queries pending for it...
-	 *
-	 * <queue_directory>/<clientUUID>/<queue_record_filename>.tql
-	 *
-	 * queue_record_filenames should be numeric to allow them to be
-	 * executed in order.
-	 */
 }
