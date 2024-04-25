b1cd3a53da5fb17bfc567d55d856e4a4976d6def
Sam Caldwell <github@samcaldwell.net>
2024-04-25 18:37:46 -0500

    fixes for Auth (tests needed)

--- go/tools/osRecon/server/Auth.go
+++ go/tools/osRecon/server/Auth.go
@@ -2,22 +2,30 @@ package server
 
 import (
 	"net/http"
+	"strings"
 )
 
 type HttpHandlerFunc func(http.ResponseWriter, *http.Request)
 
 // Auth - Perform API Key authentication for the server listener
 func (svr *Server) Auth(apiFunc HttpHandlerFunc) HttpHandlerFunc {
+
+	const httpApiKeyHeaderName = "APIKEY"
+
+	// Return the API handler
 	return func(w http.ResponseWriter, r *http.Request) {
-		var authenticated bool
-		//ToDo: inspect the request (r) for apiKey
-		if authenticated {
+
+		if key := strings.TrimSpace(r.Header.Get(httpApiKeyHeaderName)); key == string(svr.apiKey) {
+
+			// Invoke the payload function
 			apiFunc(w, r)
-			return
+
 		} else {
+
+			// If APIKey is not correct, then reject as http/401 (unauthorized)
 			w.WriteHeader(http.StatusUnauthorized)
 			_, _ = w.Write([]byte("not authorized"))
-			return
+
 		}
 	}
 }
