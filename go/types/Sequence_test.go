33b8c7113f57bbcc9aa6f4c9a73d959b525572d1
Sam Caldwell <github@samcaldwell.net>
2024-04-25 19:33:18 -0500

    add Sequence.Current() test

--- go/types/Sequence_test.go
+++ go/types/Sequence_test.go
@@ -7,10 +7,19 @@ func TestSequence(t *testing.T) {
 	if int(seq) != 0 {
 		t.Fatal("expected 0")
 	}
+	if c := seq.Current(); c != 0 {
+		t.Fatal("expected 0")
+	}
 	if int(seq.Get()) != 1 {
 		t.Fatal("expected 1")
 	}
+	if c := seq.Current(); c != 1 {
+		t.Fatal("expected 1")
+	}
 	if int(seq.Get()) != 2 {
+		t.Fatal("expected 2")
+	}
+	if c := seq.Current(); c != 2 {
 		t.Fatal("expected 2")
 	}
 }
