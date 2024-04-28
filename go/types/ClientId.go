6224950242caae70581a0376d290d025dc16dc45
Sam Caldwell <github@samcaldwell.net>
2024-04-28 13:46:39 -0500

    Add ClientId.FromString()

--- go/types/ClientId.go
+++ go/types/ClientId.go
@@ -2,8 +2,19 @@ package types
 
 import "github.com/google/uuid"
 
+// ClientId - a uuid-based client identifier
 type ClientId uuid.UUID
 
+// String - Return the string form of the ClientId
 func (c *ClientId) String() string {
 	return uuid.UUID(*c).String()
+}
+
+// FromString - Given a string, parse it as a UUID and store as ClientId
+func (c *ClientId) FromString(s string) (err error) {
+	var u uuid.UUID
+	if u, err = uuid.Parse(s); err == nil {
+		*c = ClientId(u)
+	}
+	return err
 }
