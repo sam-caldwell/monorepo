84823b59deaacf44237cbd15bf192f5ed5614059
Sam Caldwell <github@samcaldwell.net>
2024-04-28 15:25:18 -0500

    Add NewQueryFile()

---
+++ go/tools/osRecon/threatQL/NewQueryFile.go
@@ -0,0 +1,31 @@
+package threatQL
+
+import (
+	"github.com/sam-caldwell/monorepo/go/fs/directory"
+	"github.com/sam-caldwell/monorepo/go/fs/file"
+	"github.com/sam-caldwell/monorepo/go/types"
+	"path/filepath"
+)
+
+// NewQueryFile - Create a new file and write the given query to the same.
+// Return the QueryFile (in-memory) object or error.  If error is returned
+// test first because QueryFile may be nil.
+//
+//	Query files are written to <path>/<client>/<timestamp>.tql
+func NewQueryFile(path directory.Path, clientId types.ClientId, query Query) (queryFile *QueryFile, err error) {
+	fullPath := filepath.Join(path.String(), clientId.String())
+	fileName, err := file.GenerateUniqueFileName(fullPath, ".tql", 3)
+	if err != nil {
+		return nil, err
+	}
+	qry, err := query.Bytes()
+	if err != nil {
+		return nil, err
+	}
+	queryFileName := file.File(fileName)
+	return &(QueryFile{
+			query:    query,
+			fileName: &queryFileName,
+		}),
+		file.Create(fileName, qry, 0600)
+}
