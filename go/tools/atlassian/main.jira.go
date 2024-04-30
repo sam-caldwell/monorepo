1729bc17f899d4ca6e78acad87d7bc5c2762c81b
Sam Caldwell <github@samcaldwell.net>
2024-04-30 01:29:27 -0500

    configure jira cli

--- go/tools/atlassian/main.jira.go
+++ go/tools/atlassian/main.jira.go
@@ -1,7 +1,26 @@
 package main
 
-import "github.com/sam-caldwell/monorepo/go/ansi"
+import (
+	"github.com/sam-caldwell/monorepo/go/ansi"
+	"github.com/sam-caldwell/monorepo/go/exit"
+	"github.com/sam-caldwell/monorepo/go/tools/atlassian/cli"
+)
 
+/*
+jira create {ticket,project} <name> --descriptor <filename.yaml> [--api-key <string>]
+
+jira read {ticket, project} <name> [--api-key <string>]
+
+jira update {ticket,project  --descriptor <filename.yaml>
+
+jira delete {ticket, project} <name> [--api-key <string>]
+
+jira list ticket <jql> [--api-key <string>]
+
+jira list projects --offset <int> --count <int> [--api-key <string>]
+*/
 func main() {
-	ansi.Red().Println("not implemented").Fatal(1).Reset()
+	ansi.Blue().Println("Starting...").Reset()
+	var app cli.JiraClient
+	exit.TerminateOnError(app.Configure())
 }
