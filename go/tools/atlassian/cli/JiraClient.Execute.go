5bf5d1ca2c54b90f670db2754aae6fd1f9f823c9
Sam Caldwell <github@samcaldwell.net>
2024-05-01 16:49:45 -0500

    Code complete for JiraClient.Execute()

--- go/tools/atlassian/cli/JiraClient.Execute.go
+++ go/tools/atlassian/cli/JiraClient.Execute.go
@@ -1,18 +1,42 @@
 package cli
 
-// Execute - Execute the Jira Client command-line operation
-func (jira *JiraClient) Execute() (err error) {
-	switch jira.command {
-	case Create:
-		err = jira.Create()
-	case Read:
-		err = jira.Read()
-	case Update:
-		err = jira.Update()
-	case Delete:
-		err = jira.Delete()
-	case List:
-		err = jira.List()
-	}
-	return err
+import (
+    "fmt"
+    "github.com/sam-caldwell/monorepo/go/ansi"
+    "github.com/sam-caldwell/monorepo/go/tools/atlassian/commands"
+)
+
+// Execute Given all context, execute the API action
+func (client *JiraClient[T]) Execute(action commands.Commands) (err error) {
+
+    var results any
+
+    switch action {
+
+    case commands.Create:
+        results, err = client.descriptor.Create()
+
+    case commands.Read:
+        results, err = client.descriptor.Read()
+
+    case commands.Update:
+        results, err = client.descriptor.Update()
+
+    case commands.Delete:
+        results, err = client.descriptor.Delete()
+
+    case commands.List:
+        results, err = client.descriptor.List()
+
+    default:
+        results, err = "", fmt.Errorf("unknown/unexpected command")
+    }
+
+    if err != nil {
+        return err
+    }
+
+    //ToDo: better output formatting...
+    ansi.Reset().Printf("%v", results).LF().Reset()
+    return err
 }
--- go/tools/atlassian/cli/Objects.go
+++
@@ -1,9 +0,0 @@
-package cli
-
-// Objects - The numeric representation of the commandline top-level object
-type Objects uint8
-
-const (
-	Ticket Objects = iota
-	Project
-)
