7a4c257896920aa3d878630048c61d16e045df03
Sam Caldwell <github@samcaldwell.net>
2024-05-01 17:12:31 -0500

    code complete: JiraClient

--- go/tools/atlassian/cli/JiraClient.go
+++ go/tools/atlassian/cli/JiraClient.go
@@ -1,14 +1,15 @@
 package cli
 
 import (
-	"github.com/sam-caldwell/monorepo/go/fs/file"
-	"github.com/sam-caldwell/monorepo/go/types"
+    AtlassianJira "github.com/sam-caldwell/monorepo/go/atlassian/Client"
+    AtlassianTypes "github.com/sam-caldwell/monorepo/go/atlassian/types"
+    "github.com/sam-caldwell/monorepo/go/types"
 )
 
 // JiraClient - A struct representing a single instance of the JIRA CLI client.
-type JiraClient struct {
-	apiKey     types.ApiKey
-	command    Commands
-	object     Objects
-	descriptor file.File
+type JiraClient[T AtlassianTypes.Descriptor] struct {
+    client     AtlassianJira.Client
+    apiKey     types.ApiKey // aka atlassian_token
+    descriptor T            // A JSON/YAML descriptor for an issue or project
+    domain     AtlassianTypes.AtlassianDomain
 }
