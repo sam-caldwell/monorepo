fa313c446317e0d63ed2cc263365c774dfdbac64
Sam Caldwell <github@samcaldwell.net>
2024-05-01 16:46:39 -0500

    finishing tools/atlassian/cli/GetDomain()

---
+++ go/tools/atlassian/cli/GetDomain.go
@@ -0,0 +1,19 @@
+package cli
+
+import (
+    "fmt"
+    "github.com/sam-caldwell/monorepo/go/exit/errors"
+    "github.com/sam-caldwell/monorepo/go/simpleArgs"
+)
+
+// GetDomain Get the --domain cli parameter
+func (client *JiraClient[T]) GetDomain() error {
+
+    thisDescriptor, err := simpleArgs.GetOptionValue("--domain")
+
+    if err != nil {
+        return fmt.Errorf(errors.MissingArguments)
+    }
+
+    return client.client.SetApiKey(thisDescriptor)
+}
