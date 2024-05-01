9e27c0dc7534e93cde61215803589ab804eea6a6
Sam Caldwell <github@samcaldwell.net>
2024-05-01 16:56:13 -0500

    Code complete: JiraClient.GetApiKey()

--- go/tools/atlassian/cli/JiraClient.GetApiKey.go
+++ go/tools/atlassian/cli/JiraClient.GetApiKey.go
@@ -1,43 +1,36 @@
 package cli
 
 import (
-	"github.com/sam-caldwell/monorepo/go/ansi"
-	env "github.com/sam-caldwell/monorepo/go/environment"
-	"github.com/sam-caldwell/monorepo/go/exit"
-	"github.com/sam-caldwell/monorepo/go/simpleArgs"
-	"github.com/sam-caldwell/monorepo/go/types"
+    "fmt"
+    env "github.com/sam-caldwell/monorepo/go/environment"
+    "github.com/sam-caldwell/monorepo/go/simpleArgs"
 )
 
 // GetApiKey - Get the API key from the commandline --apiKey value
-func (jira *JiraClient) GetApiKey() {
+func (client *JiraClient[T]) GetApiKey() (err error) {
 
-	argServerApiKey, err := simpleArgs.GetOptionValue("--apiKey")
+    if argServerApiKey, err := simpleArgs.GetOptionValue("--apiKey"); err != nil {
 
-	if err != nil {
-		if err.Error() != simpleArgs.OptionNotFound {
-			ansi.Red().
-				Printf("Parsing error (%s): %v", "--apiKey", err.Error()).
-				LF().
-				Fatal(exit.GeneralError).
-				Reset()
+        if err.Error() != simpleArgs.OptionNotFound {
 
-		} else {
+            err = fmt.Errorf("parsing error (--apiKey): %v", err)
 
-			if argServerApiKey, err = env.RequireString("ATLASSIAN_API_KEY"); err != nil {
-				ansi.Red().
-					Printf("Expected --apiKey or ATLASSIAN_API_KEY env var. %v", err).
-					LF().
-					Fatal(exit.GeneralError).
-					Reset()
-			}
+        } else {
 
-		}
+            if argServerApiKey, err = env.RequireString("ATLASSIAN_TOKEN"); err != nil {
 
-	}
+                err = fmt.Errorf("expected --apiKey or ATLASSIAN_TOKEN env var. %v", err)
 
-	var serverApiKey types.ApiKey
+            }
 
-	exit.TerminateOnError(serverApiKey.FromString(&argServerApiKey))
-	jira.apiKey = serverApiKey
+        }
+
+    } else {
+
+        err = client.client.SetApiKey(argServerApiKey)
+
+    }
+
+    return err
 
 }
