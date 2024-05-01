75cc04c6abe9baaa088297743a1e1267b57723c2
Sam Caldwell <github@samcaldwell.net>
2024-05-01 17:02:53 -0500

    code complete for JiraClient.GetDescriptor()

--- go/tools/atlassian/cli/JiraClient.GetDescriptor.go
+++ go/tools/atlassian/cli/JiraClient.GetDescriptor.go
@@ -1,21 +1,19 @@
 package cli
 
 import (
-	"github.com/sam-caldwell/monorepo/go/ansi"
-	"github.com/sam-caldwell/monorepo/go/exit"
-	"github.com/sam-caldwell/monorepo/go/exit/errors"
-	"github.com/sam-caldwell/monorepo/go/simpleArgs"
+    "github.com/sam-caldwell/monorepo/go/simpleArgs"
 )
 
-// GetDescriptor - Get the --descriptor value if appropriate
-func (jira *JiraClient) GetDescriptor() {
-	if jira.command == Create || jira.command == Update {
-		if thisDescriptor, err := simpleArgs.GetOptionValue("--descriptor"); err != nil {
-			ansi.Red().Println(errors.MissingArguments).Fatal(exit.MissingArg).Reset()
-		} else {
-			if err := jira.descriptor.Set(thisDescriptor); err != nil {
-				ansi.Red().Println(errors.MissingArguments).Fatal(exit.MissingArg).Reset()
-			}
-		}
-	}
+// GetDescriptor - Get the --descriptor value and load the file
+func (client *JiraClient[T]) GetDescriptor() error {
+
+    thisDescriptor, err := simpleArgs.GetOptionValue("--descriptor")
+
+    if err != nil {
+
+        return err
+
+    }
+
+    return client.descriptor.Load(thisDescriptor)
 }
