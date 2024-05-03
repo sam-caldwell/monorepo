81d02815ea0953768c1a7bf13cfe9b71bb3ddc3d
Sam Caldwell <github@samcaldwell.net>
2024-05-03 09:13:00 -0500

    ATLASSIAN-1: Add Client.SetAuthHeader() so auth header stuff is testable offline

--- go/atlassian/Client.SetAuthHeader.go
+++ go/atlassian/Client.SetAuthHeader.go
@@ -5,6 +5,7 @@ import (
     "net/http"
 )
 
+// SetAuthHeader - Set the Auth header for a given request
 func (client *Client) SetAuthHeader(request *http.Request) error {
     //ToDo: Double-check that the apiKey is base64-encoded properly.
     request.Header.Set(words.Authorization, words.Basic+client.GetApiKey())
