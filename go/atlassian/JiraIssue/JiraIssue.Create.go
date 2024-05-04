ba458312575bfe872f92fa17a3ffc355e7ce54a8
Sam Caldwell <github@samcaldwell.net>
2024-05-04 10:21:48 -0500

    Atlassian-1: cleanup formatting

--- go/atlassian/JiraIssue/JiraIssue.Create.go
+++ go/atlassian/JiraIssue/JiraIssue.Create.go
@@ -1,17 +1,18 @@
 package JiraIssue
 
 import (
-    "bytes"
-    Atlassian "github.com/sam-caldwell/monorepo/go/atlassian"
-    "net/http"
+	"bytes"
+	Atlassian "github.com/sam-caldwell/monorepo/go/atlassian"
+	"net/http"
 )
 
 // Create - create issue defined by the internal Issue struct state
 func (jira Issue) Create(domain *Atlassian.Domain) (*http.Request, error) {
+	const path = "/rest/api/2/issue/"
 
-    return http.NewRequest(
-        http.MethodPost,
-        Atlassian.JiraUrlFactory(Atlassian.JiraUrlPattern, domain.Get(), Atlassian.JiraApiIssue),
-        bytes.NewBuffer(jira.Marshall()))
+	return http.NewRequest(
+		http.MethodPost,
+		Atlassian.JiraUrlFactory(Atlassian.JiraUrlPattern, domain.Get(), path),
+		bytes.NewBuffer(jira.Marshall()))
 
 }
