af1003804194b752bcaa822ca360b9dd13f5e109
Sam Caldwell <github@samcaldwell.net>
2024-05-05 12:44:26 -0500

    refactor JiraIssue.Load() for the new pattern

--- go/atlassian/JiraIssue/JiraIssue.Load.go
+++ go/atlassian/JiraIssue/JiraIssue.Load.go
@@ -8,19 +8,23 @@ import (
 )
 
 // Load - Load the Jira Issue JSON file
-func (jira Issue) Load(fileName string) error {
-	if !file.Exists(fileName) {
+func (jira *Issue) Load(fileName *string) error {
+
+	if !file.Existsp(fileName) {
 		return fmt.Errorf("descriptor file does not exist")
 	}
-	content, err := os.ReadFile(fileName)
+
+	content, err := os.ReadFile(*fileName)
+
 	if err != nil {
 		return err
 	}
+
 	err = json.Unmarshal(content, &jira)
+
 	if err != nil {
 		return err
 	}
-	//
-	//ansi.Blue().Printf("load descriptor: %s", fileName).LF().Reset()
+
 	return nil
 }
