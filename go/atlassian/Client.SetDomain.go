9b02b3e19cbec21773371c90b35f7e1d8023b49e
Sam Caldwell <github@samcaldwell.net>
2024-05-05 12:28:28 -0500

    pass domain string by ref to Client.SetDomain

--- go/atlassian/Client.SetDomain.go
+++ go/atlassian/Client.SetDomain.go
@@ -1,8 +1,6 @@
 package Atlassian
 
 // SetDomain - Set the Atlassian tenant domain name (e.g. samcaldwell in samcaldwell.atlassian.net)
-func (client *Client) SetDomain(domain string) error {
-
-    client.err = client.domain.Set(&domain)
-    return client.err
+func (client *Client) SetDomain(domain *string) error {
+	return client.domain.Set(domain)
 }
