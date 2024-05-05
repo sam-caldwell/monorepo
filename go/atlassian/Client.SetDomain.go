3e8f14bd35902a3a0f687975b8a098310842e6e8
Sam Caldwell <github@samcaldwell.net>
2024-05-05 17:15:31 -0500

    Fix Client.SetDomain() to detect missing argument

--- go/atlassian/Client.SetDomain.go
+++ go/atlassian/Client.SetDomain.go
@@ -1,6 +1,11 @@
 package Atlassian
 
+import "fmt"
+
 // SetDomain - Set the Atlassian tenant domain name (e.g. samcaldwell in samcaldwell.atlassian.net)
 func (client *Client) SetDomain(domain *string) error {
+	if domain == nil || *domain == "" {
+		return fmt.Errorf("missing domain (use --domain <value>)")
+	}
 	return client.domain.Set(domain)
 }
