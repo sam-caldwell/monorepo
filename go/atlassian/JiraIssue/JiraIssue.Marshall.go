84c1d181af352c42c15524069539d0751bb84d3d
Sam Caldwell <github@samcaldwell.net>
2024-05-05 12:45:02 -0500

    refactor JiraIssue.{,Un}Marshall()

--- go/atlassian/JiraIssue/JiraIssue.Marshall.go
+++ go/atlassian/JiraIssue/JiraIssue.Marshall.go
@@ -3,7 +3,7 @@ package JiraIssue
 import "encoding/json"
 
 // Marshall - marshall the jira issue and return its json []byte form
-func (jira Issue) Marshall() []byte {
+func (jira *Issue) Marshall() []byte {
 
 	result, err := json.Marshal(jira)
 
