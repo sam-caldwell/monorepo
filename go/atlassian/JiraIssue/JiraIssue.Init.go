30953a1186f698ff7f9a776e0fb424f735225ba6
Sam Caldwell <github@samcaldwell.net>
2024-05-05 11:56:44 -0500

    add Jiraissue.Init() --needs tests

---
+++ go/atlassian/JiraIssue/JiraIssue.Init.go
@@ -0,0 +1,27 @@
+package JiraIssue
+
+// Init - Initialize the state of the Jira Issue object
+func (jira *Issue) Init(debug bool, noop bool, apiKey, domain, descriptor, issueOrKey, jqlString *string) error {
+
+	var err error
+	jira.client.SetDebug(debug)
+	jira.client.SetNoop(noop)
+	if err = jira.client.SetApiKey(apiKey); err != nil {
+		return err
+	}
+	if err = jira.client.SetDomain(domain); err != nil {
+		return err
+	}
+	if err = jira.Load(descriptor); err != nil {
+		return err
+	}
+	if (issueOrKey != nil) && (*issueOrKey != "") {
+		jira.IssueKey = *issueOrKey
+	}
+	if (jqlString != nil) && (*jqlString != "") {
+		if err = jira.jql.FromString(jqlString); err != nil {
+			return err
+		}
+	}
+	return nil
+}
