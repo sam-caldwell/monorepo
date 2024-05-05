649169cc346c63fa6d1c9e9dd5069e46eedf52e3
Sam Caldwell <github@samcaldwell.net>
2024-05-05 12:08:39 -0500

    minor fixes for Domain.Set()

--- go/atlassian/Domain.Set.go
+++ go/atlassian/Domain.Set.go
@@ -1,17 +1,16 @@
 package Atlassian
 
 import (
-    "fmt"
-    "regexp"
+	"fmt"
+	"regexp"
 )
 
 // Set - Sanitize and store the Jira Domain
 func (jira *Domain) Set(s *string) (err error) {
-    re := regexp.MustCompile(JiraDomainRegEx)
-    if re.MatchString(*s) {
-        *jira = Domain(*s)
-    } else {
-        err = fmt.Errorf(MalformedJiraDomain)
-    }
-    return err
+	re := regexp.MustCompile(JiraDomainRegEx)
+	if !re.MatchString(*s) {
+		return fmt.Errorf(MalformedJiraDomain)
+	}
+	*jira = Domain(*s)
+	return nil
 }
