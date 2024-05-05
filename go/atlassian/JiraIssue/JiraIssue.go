93532c26ff819e1b288b55ba5f483d5aaebfdd01
Sam Caldwell <github@samcaldwell.net>
2024-05-05 12:42:35 -0500

    extend JiraIssue for refactored pattern.  add IssueOrKey, Client and jql query without json field reference

--- go/atlassian/JiraIssue/JiraIssue.go
+++ go/atlassian/JiraIssue/JiraIssue.go
@@ -1,26 +1,33 @@
 package JiraIssue
 
+import (
+	Atlassian "github.com/sam-caldwell/monorepo/go/atlassian"
+	AtlassianTypes "github.com/sam-caldwell/monorepo/go/atlassian/types"
+)
+
 // Issue - a structure representing a JIRA issue
 type Issue struct {
-    IssueKey string `json:"issueKey,omitempty"`
-    Fields   struct {
-        Project struct {
-            Key string `json:"key"`
-        } `json:"project"`
-        Summary     string `json:"summary"`
-        Description struct {
-            Type    string `json:"type"`
-            Version int    `json:"version"`
-            Content []struct {
-                Type    string `json:"type"`
-                Content []struct {
-                    Type string `json:"type"`
-                    Text string `json:"text"`
-                } `json:"content"`
-            } `json:"content"`
-        } `json:"description"`
-        IssueType struct {
-            Name string `json:"name"`
-        } `json:"issuetype"`
-    } `json:"fields"`
+	IssueKey string `json:"issueKey,omitempty"`
+	Fields   struct {
+		Project struct {
+			Key string `json:"key"`
+		} `json:"project"`
+		Summary     string `json:"summary"`
+		Description struct {
+			Type    string `json:"type"`
+			Version int    `json:"version"`
+			Content []struct {
+				Type    string `json:"type"`
+				Content []struct {
+					Type string `json:"type"`
+					Text string `json:"text"`
+				} `json:"content"`
+			} `json:"content"`
+		} `json:"description"`
+		IssueType struct {
+			Name string `json:"name"`
+		} `json:"issuetype"`
+	} `json:"fields"`
+	client Atlassian.Client
+	jql    AtlassianTypes.JqlQuery
 }
