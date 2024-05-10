19d4ded32fb6d17a01a59b1076accdfb785cc8f4
Sam Caldwell <github@samcaldwell.net>
2024-05-09 20:02:31 -0500

    add JiraTransition.GetTransitionNames()

---
+++ go/atlassian/JiraTransition/JiraTransition.GetTransitionNames.go
@@ -0,0 +1,15 @@
+package JiraTransition
+
+// GetTransitionNames - Given an issue key, return the list of Jira Transition names and ids for the issue.
+func (jira *JiraTransition) GetTransitionNames() (result *map[string]string, err error) {
+	output := make(map[string]string, 1)
+	for _, item := range jira.transitions.Transitions {
+		output[item.ID] = item.Name
+	}
+	return &output, err
+}
+
+// GetTransitions - Return the transitions object by reference (raw)
+func (jira *JiraTransition) GetTransitions() *Transitions {
+	return &jira.transitions
+}
