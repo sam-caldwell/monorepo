2c6b23de14311fc0f801d6d0579d4f28da34bf8d
Sam Caldwell <github@samcaldwell.net>
2024-05-10 08:40:55 -0500

    disable logic we need to rewrite for JiraIssue

--- go/atlassian/JiraIssue/JiraIssue.Transition.go
+++ go/atlassian/JiraIssue/JiraIssue.Transition.go
@@ -1,29 +1,23 @@
 package JiraIssue
 
-import (
-	"fmt"
-	AtlassianTypes "github.com/sam-caldwell/monorepo/go/atlassian/JiraTransition"
-	"net/http"
-)
-
 // Transition - Transition workflow step for a given ticket
 func (jira *Issue) Transition(step *string) (output []byte, err error) {
-
-	const path = "/rest/api/3/issue/%s/transitions"
-
-	var transition AtlassianTypes.JiraTransition
-	if err = transition.Init(&jira.client); err != nil {
-		return nil, err
-	}
-	if err = transition.GetTransitionIdByName(&jira.IssueKey, step); err != nil {
-		return nil, err
-	}
-
-	return jira.client.Send(
-		http.MethodPost,
-		fmt.Sprintf(path, jira.IssueKey),
-		transition.JsonMarshall())
-
+	//
+	//const path = "/rest/api/3/issue/%s/transitions"
+	//
+	//var transition AtlassianTypes.JiraTransition
+	//if err = transition.Init(&jira.client,&jira.IssueKey); err != nil {
+	//	return nil, err
+	//}
+	//if err = transition.GetTransitionIdByName(); err != nil {
+	//	return nil, err
+	//}
+	//
+	//return jira.client.Send(
+	//	http.MethodPost,
+	//	fmt.Sprintf(path, jira.IssueKey),
+	//	transition.JsonMarshall())
+	return nil, nil
 }
 
 /*
