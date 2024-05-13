f60f4f7e3f6057e2ee3b6fce5ca87d72037be834
Sam Caldwell <github@samcaldwell.net>
2024-05-12 22:41:12 -0500

    fix bug in API validation

--- go/atlassian/Client.SetApiKey.go
+++ go/atlassian/Client.SetApiKey.go
@@ -1,16 +1,11 @@
 package Atlassian
 
 import (
-	"fmt"
 	env "github.com/sam-caldwell/monorepo/go/environment"
 )
 
 // SetApiKey - Sanitize and set the api key
 func (client *Client) SetApiKey(apiKey *string) (err error) {
-
-	if (apiKey == nil) || (*apiKey == "") {
-		return fmt.Errorf("empty or nil apiKey")
-	}
 
 	*apiKey, err = env.RequireString("ATLASSIAN_TOKEN")
 
--- go/atlassian/Token.Set.go
+++ go/atlassian/Token.Set.go
@@ -8,6 +8,10 @@ import (
 // Set - set a token (after validating it)
 func (o *Token) Set(v *string) (err error) {
 
+	if (v == nil) || (*v == "") {
+		return fmt.Errorf("empty or nil apiKey")
+	}
+
 	pattern := regexp.MustCompile(atlassianTokenRegex)
 
 	if !pattern.MatchString(*v) {
