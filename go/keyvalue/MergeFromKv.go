a4e670cfa42db7fd9750429945c3a43da614fc57
Sam Caldwell <github@samcaldwell.net>
2024-05-17 09:41:49 -0500

    refactor MergeFromKv()

--- go/keyvalue/MergeFromKv.go
+++ go/keyvalue/MergeFromKv.go
@@ -3,13 +3,14 @@ package keyvalue
 /*
  * keyvalue.MergeFromKv
  * (c) 2023 Sam Caldwell.  See LICENSE.txt
- *
- * Merge a given source keyvalue into the current struct.
- * Note this will overwrite existing keys
  */
 
 // MergeFromKv - Merge a given source KeyValue into the current struct
-func (kv *KeyValue) MergeFromKv(source KeyValue) {
+func (kv *KeyValue[KeyType, ValueType]) MergeFromKv(source KeyValue[KeyType, ValueType]) {
+	kv.lock.Lock()
+	source.lock.Lock()
+	defer kv.lock.Unlock()
+	defer source.lock.Unlock()
 	for key, value := range source.data {
 		kv.data[key] = value
 	}
