3a949e5f74e5afd223a6bca1dceec6d088470f6d
Sam Caldwell <github@samcaldwell.net>
2024-05-17 00:42:36 -0500

    refactor Initialize()

--- go/keyvalue/Initialize.go
+++ go/keyvalue/Initialize.go
@@ -8,16 +8,18 @@ package keyvalue
  */
 
 // Initialize - Allocate memory for our Key-value map
-func (kv *KeyValue) Initialize(sz int, allowOverwrite OverwriteProtection) {
+func (kv *KeyValue[KeyType, ValueType]) Initialize(sz int, allowOverwrite OverwriteProtection) {
 	//
 	// Only allocate memory if not allocated or 'overwrite' flag is set.
 	//
+	kv.lock.Lock()
+	defer kv.lock.Unlock()
 	if (kv.data == nil) || allowOverwrite {
 		//A less-than-zero length memory allocation sounds like a fantasy from Dilber's Mr. PointyHead
 		//Let's not allow that.
 		if sz < 0 {
 			sz = 0
 		}
-		kv.data = make(Map, sz)
+		kv.data = make(map[KeyType]ValueType, sz)
 	}
 }
