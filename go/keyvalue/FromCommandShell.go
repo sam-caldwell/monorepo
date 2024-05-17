0acc84cb655493271377d1f40d1a0723e7f7e04d
Sam Caldwell <github@samcaldwell.net>
2024-05-17 00:44:19 -0500

    refactor FromCommandShell() method with locks

--- go/keyvalue/FromCommandShell.go
+++ go/keyvalue/FromCommandShell.go
@@ -5,11 +5,12 @@ import (
 )
 
 // FromCommandShell - Execute a command shell and parse its results into the Key-Value Map.
-func (kv *KeyValue) FromCommandShell(columnDelimiter, lineDelimiter, command string, args ...string) (err error) {
+func (kv *KeyValue[KeyType, ValueType]) FromCommandShell(columnDelimiter, lineDelimiter, command string, args ...string) (err error) {
 	var out []byte
 	if out, err = exec.Command(command, args...).Output(); err != nil {
 		return err
 	}
-	kv.FromBytes(&out, lineDelimiter, columnDelimiter)
-	return nil
+	kv.lock.Lock()
+	defer kv.lock.Unlock()
+	return kv.FromBytes(&out, lineDelimiter, columnDelimiter)
 }
