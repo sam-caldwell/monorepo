69b764bfc83c016065f87c86b366f7dde99ba05d
Sam Caldwell <github@samcaldwell.net>
2024-05-17 10:22:03 -0500

    add locks to OrderedPair

--- go/keyvalue/pair/OrderedPair.go
+++ go/keyvalue/pair/OrderedPair.go
@@ -1,6 +1,9 @@
 package pair
 
+import "sync"
+
 // OrderedPair - A Generic ordered key-value pair
 type OrderedPair[KeyType comparable, ValueType any] struct {
+	lock sync.RWMutex
 	data []Pair[KeyType, ValueType]
 }
