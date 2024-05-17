2692cadfb4d9bd271aeee99fb0177e3142c80fbc
Sam Caldwell <github@samcaldwell.net>
2024-05-16 23:37:49 -0500

    improve Count() method test

--- go/sets/simple/Count_test.go
+++ go/sets/simple/Count_test.go
@@ -3,41 +3,40 @@ package simple
 import "testing"
 
 func TestSet_Count(t *testing.T) {
-	t.Skip("debugging")
-	//Create a new set
-	var set Set[int]
 
-	// Verify that the initial count is 0
-	if count := set.Count(); count != 0 {
-		t.Errorf("Expected count 0, got: %d", count)
-	}
+	t.Run("Test Count method uninitialized", func(t *testing.T) {
+		var set Set[int]
+		// Verify that the initial count is 0
+		if count := set.Count(); count != 0 {
+			t.Errorf("Expected count 0, got: %d", count)
+		}
+	})
+	t.Run("Test Count method initialized", func(t *testing.T) {
+		var set Set[int]
+		// Verify that the initial count is 0
+		if count := set.Count(); count != 0 {
+			t.Errorf("Expected count 0, got: %d", count)
+		}
 
-	// Add items to the set
-	if err := set.Add(1); err != nil {
-		t.Fatal(err)
-	}
-	if err := set.Add(2); err != nil {
-		t.Fatal(err)
-	}
-	if err := set.Add(3); err != nil {
-		t.Fatal(err)
-	}
+		// Add items to the set
+		_ = set.Add(1)
+		_ = set.Add(2)
+		_ = set.Add(3)
+		_ = set.Add(4)
 
-	// Verify that the count is updated correctly
-	if count := set.Count(); count != 3 {
-		t.Errorf("Expected count 3, got: %d", count)
-	}
+		// Verify that the count is updated correctly
+		if count := set.Count(); count != 4 {
+			t.Errorf("Expected count 4, got: %d", count)
+		}
 
-	// Add more items to the set
-	if err := set.Add(4); err != nil {
-		t.Fatal(err)
-	}
-	if err := set.Add(5); err != nil {
-		t.Fatal(err)
-	}
+		if err := set.Add(5); err != nil {
+			t.Fatal(err)
+		}
 
-	// Verify that the count is updated correctly again
-	if count := set.Count(); count != 5 {
-		t.Errorf("Expected count 5, got: %d", count)
-	}
+		// Verify that the count is updated correctly again
+		if count := set.Count(); count != 5 {
+			t.Errorf("Expected count 5, got: %d", count)
+		}
+	})
+
 }
