e21fec6ae089c57cafe90d1996c99d81ecfc912c
Sam Caldwell <github@samcaldwell.net>
2024-05-16 19:33:52 -0500

    improve ordered.Set .Add() test

--- go/sets/ordered/Add_test.go
+++ go/sets/ordered/Add_test.go
@@ -4,36 +4,40 @@ import "testing"
 
 func TestSet_Add(t *testing.T) {
 	t.Run("integer test", func(t *testing.T) {
-		set := Set[int]{data: []int{1, 2, 3}}
+		var set Set[int]
 
 		t.Run("AddNonDuplicate", func(t *testing.T) {
-			err := set.Add(4)
+			err := set.Add(1)
 			if err != nil {
-				t.Errorf("Expected nil error, got %v", err)
+				t.Fatalf("Expected nil error, got %v", err)
 			}
+			_ = set.Add(2)
+			_ = set.Add(3)
+			_ = set.Add(4)
+			_ = set.Add(5)
+			_ = set.Add(6)
 		})
 
 		t.Run("AddDuplicate", func(t *testing.T) {
 			err := set.Add(3)
 			expectedErr := "duplicate entry"
 			if err == nil || err.Error() != expectedErr {
-				t.Errorf("Expected error '%s', got %v", expectedErr, err)
+				t.Fatalf("Expected error '%s', got %v", expectedErr, err)
 			}
 		})
 	})
 	t.Run("string test", func(t *testing.T) {
 		var set Set[string]
-
 		t.Run("AddFirstElement", func(t *testing.T) {
 			err := set.Add("1")
 			if err != nil {
-				t.Errorf("Expected nil error, got %v", err)
+				t.Fatalf("Expected nil error, got %v", err)
 			}
 		})
 		t.Run("AddNonDuplicate", func(t *testing.T) {
 			err := set.Add("2")
 			if err != nil {
-				t.Errorf("Expected nil error, got %v", err)
+				t.Fatalf("Expected nil error, got %v", err)
 			}
 		})
 
@@ -41,7 +45,7 @@ func TestSet_Add(t *testing.T) {
 			err := set.Add("1")
 			expectedErr := "duplicate entry"
 			if err == nil || err.Error() != expectedErr {
-				t.Errorf("Expected error '%s', got %v", expectedErr, err)
+				t.Fatalf("Expected error '%s', got %v", expectedErr, err)
 			}
 		})
 	})
