9f33da58a2abd6994a5210b0b066f30860813371
Sam Caldwell <github@samcaldwell.net>
2024-05-17 09:44:42 -0500

    refactor Set()

--- go/keyvalue/Set.go
+++ go/keyvalue/Set.go
@@ -3,12 +3,12 @@ package keyvalue
 /*
  * keyvalue.SetBool
  * (c) 2023 Sam Caldwell.  See LICENSE.txt
- *
- * For a given key and value, store the same in our key-value store
  */
 
-// SetBool - For a given key and value, store the same in our key-value store
-func (kv *KeyValue) SetBool(key string, value bool) {
+// Set - For a given key and value, store the same in our key-value store
+func (kv *KeyValue[KeyType, ValueType]) Set(key KeyType, value ValueType) {
 	kv.Initialize(0, preserve)
+	kv.lock.Lock()
+	defer kv.lock.Unlock()
 	kv.data[key] = value
 }
