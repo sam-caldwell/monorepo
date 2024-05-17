44099af62df150115991dd40102467a6c19f42d7
Sam Caldwell <github@samcaldwell.net>
2024-05-17 09:48:28 -0500

    Refactor ToOrderedPairList()

--- go/keyvalue/ToOrderedPairList.go
+++ go/keyvalue/ToOrderedPairList.go
@@ -4,7 +4,10 @@ import (
 	"github.com/sam-caldwell/monorepo/go/keyvalue/pair"
 )
 
-func (kv *KeyValue) ToOrderedPairList(sorted bool) (list pair.OrderedPair) {
+// ToOrderedPairList - Convert KeyValue to OrderedPair
+func (kv *KeyValue[KeyType, ValueType]) ToOrderedPairList(sorted bool) (list pair.OrderedPair[KeyType, ValueType]) {
+	kv.lock.Lock()
+	defer kv.lock.Unlock()
 	if kv.data == nil {
 		return list
 	}
