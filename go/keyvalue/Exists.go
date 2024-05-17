a690aea7a3ee389b04d25972544719a37058291e
Sam Caldwell <github@samcaldwell.net>
2024-05-17 00:42:08 -0500

    refactor Exists() method

--- go/keyvalue/Exists.go
+++ go/keyvalue/Exists.go
@@ -1,7 +1,7 @@
 package keyvalue
 
 // Exists - Return true if key exists, false if it does not.
-func (kv *KeyValue) Exists(key string) bool {
+func (kv *KeyValue[KeyType, ValueType]) Exists(key KeyType) bool {
 	_, found := kv.data[key]
 	return found
 }
