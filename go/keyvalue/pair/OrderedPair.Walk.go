d9d22c21ed25d42a79ba9343fa09f5d4161ea0f1
Sam Caldwell <github@samcaldwell.net>
2024-05-17 16:16:26 -0500

    refactor OrderedPair.Walk

--- go/keyvalue/pair/OrderedPair.Walk.go
+++ go/keyvalue/pair/OrderedPair.Walk.go
@@ -1,6 +1,6 @@
 package pair
 
-func (o *OrderedPair) Walk(fn func(key string, value interface{}) error) error {
+func (o *OrderedPair[KeyType, ValueType]) Walk(fn func(key KeyType, value ValueType) error) error {
 	for _, record := range o.data {
 		if err := fn(record.Key, record.Value); err != nil {
 			return err
