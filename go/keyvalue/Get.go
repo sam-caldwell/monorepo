546785523fbc83bc01faae10a77f46f32a1f460d
Sam Caldwell <github@samcaldwell.net>
2024-05-17 01:08:28 -0500

    Refactor Getter methods to implement generic function

---
+++ go/keyvalue/Get.go
@@ -0,0 +1,21 @@
+package keyvalue
+
+import (
+	"fmt"
+	"github.com/sam-caldwell/monorepo/go/exit/errors"
+)
+
+/*
+ * keyvalue.Get
+ * (c) 2023 Sam Caldwell.  See LICENSE.txt
+ */
+
+// Get - return the value for a given key.
+func (kv *KeyValue[KeyType, ValueType]) Get(key KeyType) (value ValueType, err error) {
+	raw, ok := kv.FindKey(key)
+	if !ok {
+		var empty ValueType
+		return empty, fmt.Errorf(errors.NotFound)
+	}
+	return raw, nil
+}
