6326b3b9e0a57fc9c040c119ea919d695f327e31
Sam Caldwell <github@samcaldwell.net>
2024-05-16 22:48:47 -0500

    fixes for simple.Set Add() method and test

--- go/sets/simple/Add_test.go
+++ go/sets/simple/Add_test.go
@@ -1,49 +1,53 @@
 package simple
 
 import (
-	"fmt"
-	"github.com/sam-caldwell/monorepo/go/exit/errors"
 	"testing"
 )
 
 func TestSet_Add(t *testing.T) {
-	func() {
-		//Simple test
-		var set Set
+	t.Run("simple test with no initialization", func(t *testing.T) {
+		var set Set[int]
+		if set.data != nil {
+			t.Fatal("expect nil data map at this point.")
+		}
 		if err := set.Add(1); err != nil {
 			t.Fatalf("simple test: %v", err)
 		}
 		if err := set.Add(2); err != nil {
 			t.Fatalf("simple test: %v", err)
 		}
-	}()
-
-	func() {
-		//Complex test
-		set := Set{}
-
-		complexTest := func(value any, expectedError error) {
-			// Add the first item (type int)
-			//t.Logf("Value: %v [expected:%v]", value, expectedError)
-			if err := set.Add(value); (err != nil) && (err.Error() != expectedError.Error()) {
-				t.Fatalf("Expected error mismatch.\n"+
-					"\t   value: '%v'\n"+
-					"\tExpected: '%v'\n"+
-					"\t     Got: '%v'\n",
-					value, expectedError, err)
+	})
+	t.Run("simple test with prior initialization", func(t *testing.T) {
+		var set Set[int]
+		set.Init()
+		if err := set.Add(1); err != nil {
+			t.Fatalf("simple test: %v", err)
+		}
+		if err := set.Add(2); err != nil {
+			t.Fatalf("simple test: %v", err)
+		}
+	})
+	t.Run("test with different types", func(t *testing.T) {
+		t.Run("test int", func(t *testing.T) {
+			var set Set[int]
+			if err := set.Add(1); err != nil {
+				t.Fatalf("simple test: %v", err)
 			}
-		}
-
-		for i := 42; i > -42; i-- {
-			complexTest(42, nil)
-		}
-
-		complexTest(true, fmt.Errorf(errors.TypeMismatch))
-		complexTest(false, fmt.Errorf(errors.TypeMismatch))
-		complexTest(3.1415, fmt.Errorf(errors.TypeMismatch))
-		complexTest(0.0, fmt.Errorf(errors.TypeMismatch))
-		complexTest("", fmt.Errorf(errors.TypeMismatch))
-		complexTest("badString", fmt.Errorf(errors.TypeMismatch))
-		complexTest(nil, nil)
-	}()
+		})
+		t.Run("test uint", func(t *testing.T) {
+			var set Set[uint]
+			if err := set.Add(99); err != nil {
+				t.Fatalf("simple test: %v", err)
+			}
+		})
+		t.Run("test string", func(t *testing.T) {
+			var set Set[string]
+			if err := set.Add("data"); err != nil {
+				t.Fatalf("simple test: %v", err)
+			}
+			if err := set.Add("data2"); err != nil {
+				t.Fatalf("simple test: %v", err)
+			}
+		})
+	})
 }
