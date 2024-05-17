4c81773525806493122efe2ba27f2972ea1e7bf1
Sam Caldwell <github@samcaldwell.net>
2024-05-17 10:22:19 -0500

    add locks to OrderedPair.Add()

--- go/keyvalue/pair/OrderedPair.Add.go
+++ go/keyvalue/pair/OrderedPair.Add.go
@@ -2,6 +2,8 @@ package pair
 
 // Add - Append a
 func (o *OrderedPair[KeyType, ValueType]) Add(key KeyType, value ValueType) {
+	o.lock.Lock()
+	defer o.lock.Unlock()
 	o.data = append(o.data, Pair[KeyType, ValueType]{
 		Key:   key,
 		Value: value,
