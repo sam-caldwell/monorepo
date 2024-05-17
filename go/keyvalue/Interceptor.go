0c525bf3012315f35c3c47a040507eadd6a574ee
Sam Caldwell <github@samcaldwell.net>
2024-05-17 09:55:33 -0500

    refactor interceptor function type

--- go/keyvalue/Interceptor.go
+++ go/keyvalue/Interceptor.go
@@ -4,7 +4,7 @@ import (
 	"github.com/sam-caldwell/monorepo/go/misc/words"
 )
 
-func Interceptor(sourceFunc func() (KeyValue, error)) (string, error) {
+func Interceptor[KeyType comparable, ValueType any](sourceFunc func() (KeyValue[KeyType, ValueType], error)) (string, error) {
 	kv, err := sourceFunc()
 	return kv.ToString(words.Colon, words.NewLine), err
 }
