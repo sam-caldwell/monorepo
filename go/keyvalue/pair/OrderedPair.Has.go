e0b59dbc7d98b69b2dcc7e75e57ac3244b352f36
Sam Caldwell <github@samcaldwell.net>
2024-05-17 10:23:02 -0500

    refactor OrderedPair.Has()

--- go/keyvalue/pair/OrderedPair.Has.go
+++ go/keyvalue/pair/OrderedPair.Has.go
@@ -1,6 +1,8 @@
 package pair
 
-func (o *OrderedPair) Has(key string) bool {
+func (o *OrderedPair[KeyType, ValueType]) Has(key KeyType) bool {
+	o.lock.Lock()
+	defer o.lock.Unlock()
 	for _, element := range o.data {
 		if element.Key == key {
 			return true
