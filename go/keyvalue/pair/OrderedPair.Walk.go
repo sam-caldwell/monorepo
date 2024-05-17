80c941a64cb9e0939ba6970ac59ab7ece196b86f
Sam Caldwell <github@samcaldwell.net>
2024-05-17 16:16:51 -0500

    refactor OrderedPair.Walk

--- go/keyvalue/pair/OrderedPair.Walk.go
+++ go/keyvalue/pair/OrderedPair.Walk.go
@@ -1,5 +1,6 @@
 package pair
 
+// Walk - Walk the ordered pair and execute the given function on each element
 func (o *OrderedPair[KeyType, ValueType]) Walk(fn func(key KeyType, value ValueType) error) error {
 	for _, record := range o.data {
 		if err := fn(record.Key, record.Value); err != nil {
