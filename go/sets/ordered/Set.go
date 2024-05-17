51b65b45206e9b0f69f2a3b154783cc0e5e52a46
Sam Caldwell <github@samcaldwell.net>
2024-05-16 19:16:56 -0500

    Refactoring ordered.Set

--- go/sets/ordered/Set.go
+++ go/sets/ordered/Set.go
@@ -3,14 +3,12 @@ package ordered
 /*
  * projects/sets/ordered/Set.go
  * (c) 2023 Sam Caldwell.  See LICENSE.txt
- *
- * See OpSys.Network.software.Memory.Disk.Cpu.README.md
  */
 
 import "sync"
 
 // Set - Create map of any to its type
-type Set struct {
+type Set[T comparable] struct {
 	lock sync.RWMutex
-	data []any
+	data []T
 }
--- go/sets/ordered/Set_test.go
+++ go/sets/ordered/Set_test.go
@@ -3,8 +3,31 @@ package ordered
 import "testing"
 
 func TestSet_Struct(t *testing.T) {
-	var s Set
-	if s.data != nil {
-		t.Fail()
+	tests := []struct {
+		name string
+		typ  interface{}
+	}{
+		{"int", int(0)},
+		{"int8", int8(0)},
+		{"int16", int16(0)},
+		{"int32", int32(0)},
+		{"int64", int64(0)},
+		{"uint", uint(0)},
+		{"uint8", uint8(0)},
+		{"uint16", uint16(0)},
+		{"uint32", uint32(0)},
+		{"uint64", uint64(0)},
+		{"rune", rune(0)},
+		{"string", ""},
+		{"bool", false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var s Set[interface{}]
+			if s.data != nil {
+				t.Fail()
+			}
+		})
 	}
 }
