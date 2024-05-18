831b336cf4036961b5881614aa30edda36cfb04c
Sam Caldwell <github@samcaldwell.net>
2024-05-18 11:52:27 -0500

    debug and fix code and tests for KeyToBytes()

--- go/keyvalue/pair/keyToBytes.go
+++ go/keyvalue/pair/keyToBytes.go
@@ -6,23 +6,118 @@ import (
 	"fmt"
 	"github.com/sam-caldwell/monorepo/go/convert"
 	"github.com/sam-caldwell/monorepo/go/exit/errors"
+	"reflect"
+	"sort"
 )
 
 // KeyToBytes converts a key of KeyType to a []byte.
 func KeyToBytes[KeyType comparable](key KeyType) ([]byte, error) {
+	const conversionError = "error converting key to bytes: %w"
 	var buf bytes.Buffer
-	switch v := any(key).(type) {
-	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64, float32, float64:
-		err := binary.Write(&buf, binary.LittleEndian, v)
-		if err != nil {
-			return nil, fmt.Errorf("error converting key to bytes: %w", err)
+
+	if err := writeValue(&buf, key); err != nil {
+		return nil, fmt.Errorf(conversionError, err)
+	}
+
+	return buf.Bytes(), nil
+}
+
+func writeValue(buf *bytes.Buffer, value any) error {
+	switch v := value.(type) {
+	case int:
+		// Decodes any int to 64-bit integer to avoid non-fixed int problem
+		if err := binary.Write(buf, binary.LittleEndian, int64(v)); err != nil {
+			return fmt.Errorf("error converting key to bytes: %w", err)
+		}
+	case int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64, float32, float64:
+		if err := binary.Write(buf, binary.LittleEndian, v); err != nil {
+			return fmt.Errorf("error converting key to bytes: %w", err)
 		}
 	case string:
 		buf.WriteString(v)
 	case bool:
 		buf.WriteByte(convert.BoolToByte(v))
+	case *int:
+		if err := binary.Write(buf, binary.LittleEndian, int64(*v)); err != nil {
+			return fmt.Errorf("error converting key to bytes: %w", err)
+		}
+	case *int8:
+		if err := binary.Write(buf, binary.LittleEndian, *v); err != nil {
+			return fmt.Errorf("error converting key to bytes: %w", err)
+		}
+	case *int16:
+		if err := binary.Write(buf, binary.LittleEndian, *v); err != nil {
+			return fmt.Errorf("error converting key to bytes: %w", err)
+		}
+	case *int32:
+		if err := binary.Write(buf, binary.LittleEndian, *v); err != nil {
+			return fmt.Errorf("error converting key to bytes: %w", err)
+		}
+	case *int64:
+		if err := binary.Write(buf, binary.LittleEndian, *v); err != nil {
+			return fmt.Errorf("error converting key to bytes: %w", err)
+		}
+	case *uint:
+		if err := binary.Write(buf, binary.LittleEndian, *v); err != nil {
+			return fmt.Errorf("error converting key to bytes: %w", err)
+		}
+	case *uint8:
+		if err := binary.Write(buf, binary.LittleEndian, *v); err != nil {
+			return fmt.Errorf("error converting key to bytes: %w", err)
+		}
+	case *uint16:
+		if err := binary.Write(buf, binary.LittleEndian, *v); err != nil {
+			return fmt.Errorf("error converting key to bytes: %w", err)
+		}
+	case *uint32:
+		if err := binary.Write(buf, binary.LittleEndian, *v); err != nil {
+			return fmt.Errorf("error converting key to bytes: %w", err)
+		}
+	case *uint64:
+		if err := binary.Write(buf, binary.LittleEndian, *v); err != nil {
+			return fmt.Errorf("error converting key to bytes: %w", err)
+		}
+	case *float32:
+		if err := binary.Write(buf, binary.LittleEndian, *v); err != nil {
+			return fmt.Errorf("error converting key to bytes: %w", err)
+		}
+	case *float64:
+		if err := binary.Write(buf, binary.LittleEndian, *v); err != nil {
+			return fmt.Errorf("error converting key to bytes: %w", err)
+		}
+	case *string:
+		buf.WriteString(*v)
+	case *bool:
+		buf.WriteByte(convert.BoolToByte(*v))
+	case []byte:
+		buf.Write(v)
+	case *[]byte:
+		buf.Write(*v)
 	default:
-		return nil, fmt.Errorf(errors.UnsupportedType)
+		rv := reflect.ValueOf(value)
+		rt := rv.Type()
+		if rv.Kind() == reflect.Ptr && rv.Elem().Kind() == reflect.Struct {
+			rv = rv.Elem()
+			rt = rt.Elem()
+		}
+		if rv.Kind() == reflect.Struct {
+			// Get and sort field names
+			fieldNames := make([]string, rv.NumField())
+			for i := 0; i < rv.NumField(); i++ {
+				fieldNames[i] = rt.Field(i).Name
+			}
+			sort.Strings(fieldNames)
+
+			// Write fields in sorted order
+			for _, fieldName := range fieldNames {
+				field := rv.FieldByName(fieldName)
+				if err := writeValue(buf, field.Interface()); err != nil {
+					return err
+				}
+			}
+			return nil
+		}
+		return fmt.Errorf(errors.UnsupportedType)
 	}
-	return buf.Bytes(), nil
+	return nil
 }
--- go/keyvalue/pair/keyToBytes_test.go
+++ go/keyvalue/pair/keyToBytes_test.go
@@ -1,35 +1,405 @@
 package pair
 
 import (
+	"bytes"
+	"encoding/binary"
 	"github.com/sam-caldwell/monorepo/go/exit/errors"
+	"github.com/sam-caldwell/monorepo/go/misc/words"
+	"math"
 	"testing"
 )
 
 func TestKeyToBytes(t *testing.T) {
-	t.Run("test with invalid (struct) type", func(t *testing.T) {
-		type invalidType struct{}
-		var lhs invalidType
-		if _, err := KeyToBytes(lhs); err == nil {
-			t.Fatalf("expected error for invalid type but got %v", err)
-		} else {
-			if err.Error() != errors.UnsupportedType {
-				t.Fatalf("expected UnsupportedType error but got %v", err)
-			}
-		}
+	t.Run("test with boolean type", func(t *testing.T) {
+		var n bool
+		t.Run("boolean value test", func(t *testing.T) {
+			t.Run("test with false", func(t *testing.T) {
+				n = false
+				v, err := KeyToBytes(n)
+				if err != nil {
+					t.Fatalf("expected error for boolean type but got %v", err)
+				}
+				if v[0] != 0x00 {
+					t.Fatalf("expected 0x00 for boolean type but got %v", v)
+				}
+			})
+			t.Run("test with true", func(t *testing.T) {
+				n = true
+				v, err := KeyToBytes(n)
+				if err != nil {
+					t.Fatalf("expected error for boolean type but got %v", err)
+				}
+				if v[0] != 0xFF {
+					t.Fatalf("expected 0xFF for boolean type but got %v", v)
+				}
+			})
+		})
+		t.Run("boolean pointer test", func(t *testing.T) {
+			t.Run("test with false", func(t *testing.T) {
+				n = false
+				v, err := KeyToBytes(&n)
+				if err != nil {
+					t.Fatalf("expected error for boolean type but got %v", err)
+				}
+				if v[0] != 0x00 {
+					t.Fatalf("expected 0x00 for boolean type but got %v", v)
+				}
+			})
+			t.Run("test with true", func(t *testing.T) {
+				n = true
+				v, err := KeyToBytes(&n)
+				if err != nil {
+					t.Fatalf("expected error for boolean type but got %v", err)
+				}
+				if v[0] != 0xFF {
+					t.Fatalf("expected 0xFF for boolean type but got %v", v)
+				}
+			})
+		})
 	})
-	t.Run("test with boolean type", func(t *testing.T) {})
-	t.Run("test with signed integer type", func(t *testing.T) {})
-	t.Run("test with unsigned integer type", func(t *testing.T) {})
-	t.Run("test with float32 type", func(t *testing.T) {})
-	t.Run("test with float64 type", func(t *testing.T) {})
-	t.Run("test with string type", func(t *testing.T) {})
-	t.Run("test with rune type", func(t *testing.T) {})
-	t.Run("test with struct pointer type", func(t *testing.T) {})
-	t.Run("test with boolean pointer type", func(t *testing.T) {})
-	t.Run("test with signed integer pointer type", func(t *testing.T) {})
-	t.Run("test with unsigned integer pointer type", func(t *testing.T) {})
-	t.Run("test with float32 pointer type", func(t *testing.T) {})
-	t.Run("test with float64 pointer type", func(t *testing.T) {})
-	t.Run("test with string pointer type", func(t *testing.T) {})
-	t.Run("test with rune pointer type", func(t *testing.T) {})
+	t.Run("test with signed integer type", func(t *testing.T) {
+		t.Run("test with int", func(t *testing.T) {
+			for expected := range []int{math.MinInt, -1024, -1, 0, 1, 1024, math.MaxInt} {
+				var actual bytes.Buffer
+				_ = binary.Write(&actual, binary.LittleEndian, int64(expected))
+				t.Run("test with value", func(t *testing.T) {
+					expectedBytes, err := KeyToBytes(expected)
+					if err != nil {
+						t.Fatalf("expected error for integer type but got '%v'", err)
+					}
+					if bytes.Compare(actual.Bytes(), expectedBytes) != 0 {
+						t.Fatalf("expected '%v' but got '%v'", expectedBytes, actual)
+					}
+				})
+				t.Run("test with pointer", func(t *testing.T) {
+					expectedBytes, err := KeyToBytes(&expected)
+					if err != nil {
+						t.Fatalf("expected error for integer type but got '%v'", err)
+					}
+					if bytes.Compare(actual.Bytes(), expectedBytes) != 0 {
+						t.Fatalf("expected '%v' but got '%v'", expectedBytes, actual)
+					}
+				})
+			}
+		})
+		t.Run("test with int8", func(t *testing.T) {
+			for expected := range []int8{-127, -1, 0, 1, 127} {
+				var actual bytes.Buffer
+				_ = binary.Write(&actual, binary.LittleEndian, int64(expected))
+				t.Run("test with value", func(t *testing.T) {
+					expectedBytes, err := KeyToBytes(expected)
+					if err != nil {
+						t.Fatalf("expected error for integer type but got '%v'", err)
+					}
+					if bytes.Compare(actual.Bytes(), expectedBytes) != 0 {
+						t.Fatalf("expected '%v' but got '%v'", expectedBytes, actual)
+					}
+				})
+				t.Run("test with pointer", func(t *testing.T) {
+					expectedBytes, err := KeyToBytes(&expected)
+					if err != nil {
+						t.Fatalf("expected error for integer type but got '%v'", err)
+					}
+					if bytes.Compare(actual.Bytes(), expectedBytes) != 0 {
+						t.Fatalf("expected '%v' but got '%v'", expectedBytes, actual)
+					}
+				})
+			}
+		})
+		t.Run("test with int16", func(t *testing.T) {
+			for expected := range []int16{math.MinInt16, -1024, -1, 0, 1, 1024, math.MaxInt16} {
+				var actual bytes.Buffer
+				_ = binary.Write(&actual, binary.LittleEndian, int64(expected))
+				t.Run("test with value", func(t *testing.T) {
+					expectedBytes, err := KeyToBytes(expected)
+					if err != nil {
+						t.Fatalf("expected error for integer type but got '%v'", err)
+					}
+					if bytes.Compare(actual.Bytes(), expectedBytes) != 0 {
+						t.Fatalf("expected '%v' but got '%v'", expectedBytes, actual)
+					}
+				})
+				t.Run("test with pointer", func(t *testing.T) {
+					expectedBytes, err := KeyToBytes(&expected)
+					if err != nil {
+						t.Fatalf("expected error for integer type but got '%v'", err)
+					}
+					if bytes.Compare(actual.Bytes(), expectedBytes) != 0 {
+						t.Fatalf("expected '%v' but got '%v'", expectedBytes, actual)
+					}
+				})
+			}
+		})
+		t.Run("test with int32", func(t *testing.T) {
+			for expected := range []int32{math.MinInt32, -1024, -1, 0, 1, 1024, math.MaxInt32} {
+				var actual bytes.Buffer
+				_ = binary.Write(&actual, binary.LittleEndian, int64(expected))
+				t.Run("test with value", func(t *testing.T) {
+					expectedBytes, err := KeyToBytes(expected)
+					if err != nil {
+						t.Fatalf("expected error for integer type but got '%v'", err)
+					}
+					if bytes.Compare(actual.Bytes(), expectedBytes) != 0 {
+						t.Fatalf("expected '%v' but got '%v'", expectedBytes, actual)
+					}
+				})
+				t.Run("test with pointer", func(t *testing.T) {
+					expectedBytes, err := KeyToBytes(&expected)
+					if err != nil {
+						t.Fatalf("expected error for integer type but got '%v'", err)
+					}
+					if bytes.Compare(actual.Bytes(), expectedBytes) != 0 {
+						t.Fatalf("expected '%v' but got '%v'", expectedBytes, actual)
+					}
+				})
+			}
+		})
+		t.Run("test with int64", func(t *testing.T) {
+			for expected := range []int64{math.MinInt64, -1024, -1, 0, 1, 1024, math.MaxInt64} {
+				var actual bytes.Buffer
+				_ = binary.Write(&actual, binary.LittleEndian, int64(expected))
+				t.Run("test with value", func(t *testing.T) {
+					expectedBytes, err := KeyToBytes(expected)
+					if err != nil {
+						t.Fatalf("expected error for integer type but got '%v'", err)
+					}
+					if bytes.Compare(actual.Bytes(), expectedBytes) != 0 {
+						t.Fatalf("expected '%v' but got '%v'", expectedBytes, actual)
+					}
+				})
+				t.Run("test with pointer", func(t *testing.T) {
+					expectedBytes, err := KeyToBytes(&expected)
+					if err != nil {
+						t.Fatalf("expected error for integer type but got '%v'", err)
+					}
+					if bytes.Compare(actual.Bytes(), expectedBytes) != 0 {
+						t.Fatalf("expected '%v' but got '%v'", expectedBytes, actual)
+					}
+				})
+			}
+		})
+	})
+	t.Run("test with unsigned integer type", func(t *testing.T) {
+		t.Run("test with uint", func(t *testing.T) {
+			for expected := range []uint{0, 1, 1024, math.MaxUint} {
+				var actual bytes.Buffer
+				_ = binary.Write(&actual, binary.LittleEndian, int64(expected))
+				t.Run("test with value", func(t *testing.T) {
+					expectedBytes, err := KeyToBytes(expected)
+					if err != nil {
+						t.Fatalf("expected error for integer type but got '%v'", err)
+					}
+					if bytes.Compare(actual.Bytes(), expectedBytes) != 0 {
+						t.Fatalf("expected '%v' but got '%v'", expectedBytes, actual)
+					}
+				})
+				t.Run("test with pointer", func(t *testing.T) {
+					expectedBytes, err := KeyToBytes(&expected)
+					if err != nil {
+						t.Fatalf("expected error for integer type but got '%v'", err)
+					}
+					if bytes.Compare(actual.Bytes(), expectedBytes) != 0 {
+						t.Fatalf("expected '%v' but got '%v'", expectedBytes, actual)
+					}
+				})
+			}
+		})
+		t.Run("test with uint8", func(t *testing.T) {
+			for expected := range []uint8{0, 1, math.MaxUint8} {
+				var actual bytes.Buffer
+				_ = binary.Write(&actual, binary.LittleEndian, int64(expected))
+				t.Run("test with value", func(t *testing.T) {
+					expectedBytes, err := KeyToBytes(expected)
+					if err != nil {
+						t.Fatalf("expected error for integer type but got '%v'", err)
+					}
+					if bytes.Compare(actual.Bytes(), expectedBytes) != 0 {
+						t.Fatalf("expected '%v' but got '%v'", expectedBytes, actual)
+					}
+				})
+				t.Run("test with pointer", func(t *testing.T) {
+					expectedBytes, err := KeyToBytes(&expected)
+					if err != nil {
+						t.Fatalf("expected error for integer type but got '%v'", err)
+					}
+					if bytes.Compare(actual.Bytes(), expectedBytes) != 0 {
+						t.Fatalf("expected '%v' but got '%v'", expectedBytes, actual)
+					}
+				})
+			}
+		})
+		t.Run("test with uint16", func(t *testing.T) {
+			for expected := range []uint16{0, 1, math.MaxUint16} {
+				var actual bytes.Buffer
+				_ = binary.Write(&actual, binary.LittleEndian, int64(expected))
+				t.Run("test with value", func(t *testing.T) {
+					expectedBytes, err := KeyToBytes(expected)
+					if err != nil {
+						t.Fatalf("expected error for integer type but got '%v'", err)
+					}
+					if bytes.Compare(actual.Bytes(), expectedBytes) != 0 {
+						t.Fatalf("expected '%v' but got '%v'", expectedBytes, actual)
+					}
+				})
+				t.Run("test with pointer", func(t *testing.T) {
+					expectedBytes, err := KeyToBytes(&expected)
+					if err != nil {
+						t.Fatalf("expected error for integer type but got '%v'", err)
+					}
+					if bytes.Compare(actual.Bytes(), expectedBytes) != 0 {
+						t.Fatalf("expected '%v' but got '%v'", expectedBytes, actual)
+					}
+				})
+			}
+		})
+		t.Run("test with uint32", func(t *testing.T) {
+			for expected := range []uint32{0, 1, math.MaxUint32} {
+				var actual bytes.Buffer
+				_ = binary.Write(&actual, binary.LittleEndian, int64(expected))
+				t.Run("test with value", func(t *testing.T) {
+					expectedBytes, err := KeyToBytes(expected)
+					if err != nil {
+						t.Fatalf("expected error for integer type but got '%v'", err)
+					}
+					if bytes.Compare(actual.Bytes(), expectedBytes) != 0 {
+						t.Fatalf("expected '%v' but got '%v'", expectedBytes, actual)
+					}
+				})
+				t.Run("test with pointer", func(t *testing.T) {
+					expectedBytes, err := KeyToBytes(&expected)
+					if err != nil {
+						t.Fatalf("expected error for integer type but got '%v'", err)
+					}
+					if bytes.Compare(actual.Bytes(), expectedBytes) != 0 {
+						t.Fatalf("expected '%v' but got '%v'", expectedBytes, actual)
+					}
+				})
+			}
+		})
+		t.Run("test with uint64", func(t *testing.T) {
+			for expected := range []uint64{0, 1, math.MaxUint64} {
+				var actual bytes.Buffer
+				_ = binary.Write(&actual, binary.LittleEndian, int64(expected))
+				t.Run("test with value", func(t *testing.T) {
+					expectedBytes, err := KeyToBytes(expected)
+					if err != nil {
+						t.Fatalf("expected error for integer type but got '%v'", err)
+					}
+					if bytes.Compare(actual.Bytes(), expectedBytes) != 0 {
+						t.Fatalf("expected '%v' but got '%v'", expectedBytes, actual)
+					}
+				})
+				t.Run("test with pointer", func(t *testing.T) {
+					expectedBytes, err := KeyToBytes(&expected)
+					if err != nil {
+						t.Fatalf("expected error for integer type but got '%v'", err)
+					}
+					if bytes.Compare(actual.Bytes(), expectedBytes) != 0 {
+						t.Fatalf("expected '%v' but got '%v'", expectedBytes, actual)
+					}
+				})
+			}
+		})
+
+	})
+	t.Run("test with floating-point type", func(t *testing.T) {
+		t.Run("test with float32", func(t *testing.T) {
+			var actual bytes.Buffer
+			expected := float32(3.14)
+			_ = binary.Write(&actual, binary.LittleEndian, expected)
+			expectedBytes, err := KeyToBytes(expected)
+			if err != nil {
+				t.Fatalf("expected error for integer type but got '%v'", err)
+			}
+			if bytes.Compare(actual.Bytes(), expectedBytes) != 0 {
+				t.Fatalf("expected '%v' but got '%v'", expectedBytes, actual)
+			}
+		})
+		t.Run("test with float64", func(t *testing.T) {
+			var actual bytes.Buffer
+			expected := float64(3.14)
+			_ = binary.Write(&actual, binary.LittleEndian, expected)
+			expectedBytes, err := KeyToBytes(expected)
+			if err != nil {
+				t.Fatalf("expected error for integer type but got '%v'", err)
+			}
+			if bytes.Compare(actual.Bytes(), expectedBytes) != 0 {
+				t.Fatalf("expected '%v' but got '%v'", expectedBytes, actual)
+			}
+		})
+
+	})
+	t.Run("test with string type", func(t *testing.T) {
+		t.Run("test with string (empty)", func(t *testing.T) {
+			var actual bytes.Buffer
+			expected := words.EmptyString
+			actual.WriteString(expected)
+			expectedBytes, err := KeyToBytes(expected)
+			if err != nil {
+				t.Fatalf("expected error for integer type but got '%v'", err)
+			}
+			if bytes.Compare(actual.Bytes(), expectedBytes) != 0 {
+				t.Fatalf("expected '%v' but got '%v'", expectedBytes, actual)
+			}
+		})
+		t.Run("test with string (non-empty)", func(t *testing.T) {
+			var actual bytes.Buffer
+			expected := "test string"
+			actual.WriteString(expected)
+			expectedBytes, err := KeyToBytes(expected)
+			if err != nil {
+				t.Fatalf("expected error for integer type but got '%v'", err)
+			}
+			if bytes.Compare(actual.Bytes(), expectedBytes) != 0 {
+				t.Fatalf("expected '%v' but got '%v'", expectedBytes, actual)
+			}
+		})
+	})
+	t.Run("test with rune type", func(t *testing.T) {
+		t.Run("test with rune", func(t *testing.T) {
+			var actual bytes.Buffer
+			expected := 'a'
+			_ = binary.Write(&actual, binary.LittleEndian, expected)
+			expectedBytes, err := KeyToBytes(expected)
+			if err != nil {
+				t.Fatalf("expected error for integer type but got '%v'", err)
+			}
+			if bytes.Compare(actual.Bytes(), expectedBytes) != 0 {
+				t.Fatalf("expected '%v' but got '%v'", expectedBytes, actual)
+			}
+		})
+		t.Run("test with rune", func(t *testing.T) {
+			var actual bytes.Buffer
+			expected := rune(0x00)
+			_ = binary.Write(&actual, binary.LittleEndian, expected)
+			expectedBytes, err := KeyToBytes(expected)
+			if err != nil {
+				t.Fatalf("expected error for integer type but got '%v'", err)
+			}
+			if bytes.Compare(actual.Bytes(), expectedBytes) != 0 {
+				t.Fatalf("expected '%v' but got '%v'", expectedBytes, actual)
+			}
+		})
+	})
+	t.Run("test with struct", func(t *testing.T) {
+		t.Run("test with struct type", func(t *testing.T) {
+			type structType struct{}
+			var lhs structType
+			if _, err := KeyToBytes(lhs); err != nil {
+				if err.Error() != errors.UnsupportedType {
+					t.Fatalf("expected UnsupportedType error but got %v", err)
+				}
+			}
+		})
+		t.Run("test with struct pointer type", func(t *testing.T) {
+			type structType struct{}
+			var lhs = &(structType{})
+			if _, err := KeyToBytes(lhs); err != nil {
+				if err.Error() != errors.UnsupportedType {
+					t.Fatalf("expected UnsupportedType error but got %v", err)
+				}
+			}
+		})
+	})
+
 }
