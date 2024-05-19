1da3095605e574a0c817f1c69ceed895ad678f82
Sam Caldwell <github@samcaldwell.net>
2024-05-19 02:07:00 -0500

    code cleanup

--- go/keyvalue/ToString.go
+++ go/keyvalue/ToString.go
@@ -1,20 +1,16 @@
 package keyvalue
 
-/*
- * keyvalue.ToString
- * (c) 2023 Sam Caldwell.  See LICENSE.txt
- *
- * Given an internal key-value data set, this method will return a string
- * table of key-value data where each row is formatted using a column delimiter
- * and line separator.
- *
- */
-
 import (
 	"strings"
 )
 
 // ToString - Return a flattened set (string) using the given rowFormat
+//
+//	     Given an internal key-value data set, this method will return a string
+//	     table of key-value data where each row is formatted using a column delimiter
+//	     and line separator.
+//
+//		 (c) 2023 Sam Caldwell.  MIT License
 func (kv *KeyValue[KeyType, ValueType]) ToString(columnDelimiter, lineEnding string) (output string) {
 	const pretty = true
 	return strings.Join(kv.ToStringArray(columnDelimiter, pretty), lineEnding)
