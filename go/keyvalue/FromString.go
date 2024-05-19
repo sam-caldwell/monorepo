141fcafe1a0dd7cbbdb32dfd55285f28f7d4a3e3
Sam Caldwell <github@samcaldwell.net>
2024-05-18 21:43:10 -0500

    code cleanup

--- go/keyvalue/FromString.go
+++ go/keyvalue/FromString.go
@@ -1,16 +1,8 @@
 package keyvalue
 
-/*
- * keyvalue.FromString
- * (c) 2023 Sam Caldwell.  See LICENSE.txt
- *
- * 	Consume string and parse by lineEnding and columnDelimiter into a keyvalue.Map
- *  internal state.
- *
- *  Note: this is destructive of any existing state
- */
-
 // FromString - Given a reference to a string, parse by lines and key-value columns, storing internally.
+//
+//	(c) 2023 Sam Caldwell.  MIT License
 func (kv *KeyValue[KeyType, ValueType]) FromString(data *string, lineEnding, columnDelimiter string) error {
 	buffer := []byte(*data)
 	return kv.FromBytes(&buffer, lineEnding, columnDelimiter)
--- go/keyvalue/FromString_test.go
+++ go/keyvalue/FromString_test.go
@@ -1,26 +1,29 @@
 package keyvalue
 
 import (
-	"reflect"
+	"github.com/sam-caldwell/monorepo/go/misc"
 	"testing"
 )
 
 func TestFromString(t *testing.T) {
-	kv := KeyValue{}
+	t.Run("Happy path", func(t *testing.T) {
+		kv := KeyValue[string, string]{}
+		data := "key1:value1\nkey2:value2\nkey3:value3"
+		lineEnding := "\n"
+		columnDelimiter := ":"
+		if err := kv.FromString(&data, lineEnding, columnDelimiter); err != nil {
+			t.Fatal(err)
+		}
+		expectedData := KeyValue[string, string]{
+			data: map[string]string{
+				"key1": "value1",
+				"key2": "value2",
+				"key3": "value3",
+			},
+		}
+		if !misc.EqualMap(&kv.data, &expectedData.data) {
+			t.Errorf("Parsed data does not match expected data")
+		}
+	})
 
-	data := "key1:value1\nkey2:value2\nkey3:value3"
-	lineEnding := "\n"
-	columnDelimiter := ":"
-
-	kv.FromString(&data, lineEnding, columnDelimiter)
-
-	expectedData := Map{
-		"key1": "value1",
-		"key2": "value2",
-		"key3": "value3",
-	}
-
-	if !reflect.DeepEqual(kv.data, expectedData) {
-		t.Errorf("Parsed data does not match expected data")
-	}
 }
