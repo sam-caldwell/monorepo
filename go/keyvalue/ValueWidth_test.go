ad751343fddc6298905c9a45ce6d90e18a57e790
Sam Caldwell <github@samcaldwell.net>
2024-05-19 18:07:50 -0500

    fixing tests for ValueWidth()

--- go/keyvalue/ValueWidth_test.go
+++ go/keyvalue/ValueWidth_test.go
@@ -43,39 +43,3 @@ func TestKeyValue_ValueWidth(t *testing.T) {
 		})
 	})
 }
-
-//
-//func TestValueWidthHappyPath(t *testing.T) {
-//	kv := KeyValue{
-//		data: Map{
-//			"key1": "short",
-//			"key2": "very long value",
-//		},
-//	}
-//
-//	width := kv.ValueWidth()
-//
-//	if width != len("very long value") {
-//		t.Fatalf("Incorrect width. Expected %d, but got %d", len("very long value"), width)
-//	}
-//}
-//
-//func TestValueWidthWithEmptyMap(t *testing.T) {
-//	kv := KeyValue{}
-//
-//	width := kv.ValueWidth()
-//
-//	if width != 0 {
-//		t.Fatalf("Incorrect width. Expected 0, but got %d", width)
-//	}
-//}
-//
-//func TestValueWidthWithNilMap(t *testing.T) {
-//	kv := KeyValue{}
-//
-//	width := kv.ValueWidth()
-//
-//	if width != 0 {
-//		t.Fatalf("Incorrect width. Expected 0, but got %d", width)
-//	}
-//}
