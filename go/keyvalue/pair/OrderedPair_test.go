8119a18e5fd2a76d08a88fe67a8c5aca723130c3
Sam Caldwell <github@samcaldwell.net>
2024-05-18 19:53:33 -0500

    add test for pair/OrderedPair

---
+++ go/keyvalue/pair/OrderedPair_test.go
@@ -0,0 +1,20 @@
+package pair
+
+import "testing"
+
+func TestOrderedPair_Struct(t *testing.T) {
+	t.Run("test bool:bool", func(t *testing.T) {
+		var o OrderedPair[bool, bool]
+		o.lock.Unlock()
+		if o.data != nil {
+			t.Fatalf("o.data should be nil at this time")
+		}
+	})
+	t.Run("test to ensure Key and Value can be different types (string:int)", func(t *testing.T) {
+		var o OrderedPair[string, int]
+		o.lock.Unlock()
+		if o.data != nil {
+			t.Fatalf("o.data should be nil at this time")
+		}
+	})
+}
