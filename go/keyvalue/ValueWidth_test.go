fa5b38f5c773e65426906ce82d3fa72920914dd7
Sam Caldwell <github@samcaldwell.net>
2024-05-19 18:07:17 -0500

    fixing tests for ValueWidth()

--- go/keyvalue/ValueWidth_test.go
+++ go/keyvalue/ValueWidth_test.go
@@ -1,6 +1,9 @@
 package keyvalue
 
-import "testing"
+import (
+	"github.com/google/uuid"
+	"testing"
+)
 
 func TestKeyValue_ValueWidth(t *testing.T) {
 	t.Run("test with nil data", func(t *testing.T) {
@@ -10,11 +13,33 @@ func TestKeyValue_ValueWidth(t *testing.T) {
 		}
 	})
 	t.Run("test with data", func(t *testing.T) {
+		var kv KeyValue[string, string]
 		t.Run("initialize data", func(t *testing.T) {
-
+			kv.data = make(map[string]string)
+			values := []string{
+				"a",
+				"ab",
+				"abc",
+				"abcd",
+				"abcde",
+				"short",
+				"medium_size",
+				"really_long_key",
+				"even_longer_key",
+				"this_key_is_the_longest",
+			}
+			for _, value := range values {
+				key := uuid.New().String()
+				kv.data[key] = value
+			}
 		})
 		t.Run("get width", func(t *testing.T) {
+			expectedWidth := len("this_key_is_the_longest")
+			width := kv.ValueWidth()
 
+			if width != expectedWidth {
+				t.Fatalf("expect %d width, got %d", expectedWidth, width)
+			}
 		})
 	})
 }
