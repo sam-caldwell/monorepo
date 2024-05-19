80f7c2ae135004d0722823fb33b29aea08ea7737
Sam Caldwell <github@samcaldwell.net>
2024-05-18 21:16:01 -0500

    code cleanup

--- go/keyvalue/constants.go
+++ go/keyvalue/constants.go
@@ -1,22 +1,23 @@
 package keyvalue
 
-/*
- * keyvalue (constants)
- * (c) 2023 Sam Caldwell.  See LICENSE.txt
- *
- */
-
+// OverwriteProtection - Used by KeyValue.Initialize() to reallocate key-value map, if already allocated
+//
+//	(c) 2023 Sam Caldwell.  MIT License
 type OverwriteProtection bool
 
-// Used by KeyValue.Initialize() to reallocate key-value map, if already allocated
 const (
+	// overwrite - if KeyValue is already allocated, reallocate the memory and call gc
 	overwrite OverwriteProtection = true
-	preserve  OverwriteProtection = false
+	// preserve - if KeyValue is already allocated, preserve the existing state.
+	preserve OverwriteProtection = false
 )
 
 // Column parsing values
 const (
+	//columnCount - number of columns we will parse data for to convert to KeyValue
 	columnCount = 2
-	keyColumn   = 0
+	//keyColumn - the index of the parsed key value
+	keyColumn = 0
+	//valueColumn - the starting index of the parsed value (1...n will be used)
 	valueColumn = 1
 )
