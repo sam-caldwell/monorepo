4045facde0bd1df449ab1b99b10aff816a54afc0
Sam Caldwell <github@samcaldwell.net>
2024-05-18 23:44:42 -0500

    fixes for tests on MergeFromKv()

--- go/keyvalue/MergeFromKv.go
+++ go/keyvalue/MergeFromKv.go
@@ -5,8 +5,13 @@ package keyvalue
  * (c) 2023 Sam Caldwell.  See LICENSE.txt
  */
 
-// MergeFromKv - Merge a given source KeyValue into the current struct
-func (kv *KeyValue[KeyType, ValueType]) MergeFromKv(source KeyValue[KeyType, ValueType]) {
+// MergeFromKv - Given the current struct and another KeyValue struct, merge the two into the current.
+//
+//	     This will lock both structs for the duration of the merge operation to avoid
+//	     integrity issues.
+//
+//			(c) 2023 Sam Caldwell.  MIT License
+func (kv *KeyValue[KeyType, ValueType]) MergeFromKv(source *KeyValue[KeyType, ValueType]) {
 	kv.lock.Lock()
 	source.lock.Lock()
 	defer kv.lock.Unlock()
---
+++ go/keyvalue/MergeFromKv_test.go
@@ -0,0 +1,45 @@
+package keyvalue
+
+import (
+	"testing"
+)
+
+func TestMergeFromKv(t *testing.T) {
+	// Initialize two keyvalue objects with some data
+	var kv1 KeyValue[string, string]
+	var kv2 KeyValue[string, string]
+	var expected KeyValue[string, string]
+
+	t.Run("Initialize data", func(t *testing.T) {
+		kv1.data = map[string]string{
+			"key1": "value1",
+			"key2": "value2",
+		}
+		kv2.data = map[string]string{
+			"key2": "value2new",
+			"key3": "value3",
+			"key4": "value4",
+		}
+		// Create a Map with the expected result after the merge
+		expected.data = map[string]string{
+			"key1": "value1",
+			"key2": "value2new",
+			"key3": "value3",
+			"key4": "value4",
+		}
+	})
+	t.Run("Call MergeFromKv()", func(t *testing.T) {
+		kv1.MergeFromKv(&kv2)
+	})
+	t.Run("Check if the data in kv1 matches the expected result", func(t *testing.T) {
+		for expectedKey, expectedValue := range expected.data {
+			if actualValue, ok := kv1.data[expectedKey]; !ok {
+				t.Errorf("key %s not found in kv1", expectedKey)
+			} else {
+				if actualValue != expectedValue {
+					t.Errorf("key %s found in kv1 but value is wrong", expectedKey)
+				}
+			}
+		}
+	})
+}
