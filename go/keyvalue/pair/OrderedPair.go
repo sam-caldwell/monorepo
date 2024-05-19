794bd839ff216a7f4275e44f30fed0d156c6ded3
Sam Caldwell <github@samcaldwell.net>
2024-05-18 21:12:37 -0500

    code cleanup

--- go/keyvalue/pair/OrderedPair.go
+++ go/keyvalue/pair/OrderedPair.go
@@ -3,6 +3,8 @@ package pair
 import "sync"
 
 // OrderedPair - A Generic ordered key-value pair
+//
+//	(c) 2023 Sam Caldwell.  MIT License
 type OrderedPair[KeyType comparable, ValueType any] struct {
 	lock sync.RWMutex
 	data []Pair[KeyType, ValueType]
