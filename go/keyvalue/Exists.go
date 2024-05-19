ea72ca2c3425efac8a28275d9c4853b571dd394a
Sam Caldwell <github@samcaldwell.net>
2024-05-18 21:21:29 -0500

    code cleanup and add Exists() test

--- go/keyvalue/Exists.go
+++ go/keyvalue/Exists.go
@@ -1,6 +1,8 @@
 package keyvalue
 
 // Exists - Return true if key exists, false if it does not.
+//
+//	(c) 2023 Sam Caldwell.  MIT License
 func (kv *KeyValue[KeyType, ValueType]) Exists(key KeyType) bool {
 	kv.lock.Lock()
 	defer kv.lock.Unlock()
---
+++ go/keyvalue/Exists_test.go
@@ -0,0 +1,25 @@
+package keyvalue
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestKeyValue_Exists(t *testing.T) {
+	var kv KeyValue[string, int]
+	kv.data = make(map[string]int)
+	kv.lock.Lock()
+	kv.lock.Unlock()
+	for i := 0; i < 10; i++ {
+		key := fmt.Sprintf("test%d", i)
+		value := i
+		kv.data[key] = value
+	}
+	for i := 0; i < 10; i++ {
+		key := fmt.Sprintf("test%d", i)
+		if !kv.Exists(key) {
+			t.Errorf("key %s not exists", key)
+		}
+	}
+
+}
