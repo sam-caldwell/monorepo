df99f1c43f67dffb4317a8631036a1f776e25f7b
Sam Caldwell <github@samcaldwell.net>
2024-05-24 09:34:24 -0500

    update fs.file to create abstract file handling object with locking

--- go/fs/file/file.go
+++ go/fs/file/file.go
@@ -1,4 +1,17 @@
 package file
 
-// File - this is a simple file/path name object with the goal of providing safe functionality.
-type File string
+import (
+	"os"
+	"sync"
+)
+
+// File - This is a wrapper around file-access logic.
+//
+//	     The goal is to create an abstraction layer so changes to golang will not require
+//	     significant effort to update all projects.
+//
+//			(c) 2023 Sam Caldwell.  MIT License
+type File struct {
+	lock   sync.Mutex
+	handle *os.File
+}
