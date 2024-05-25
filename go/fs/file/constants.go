85f044b578cc8d9e7519e0391120e9eca383dbc7
Sam Caldwell <github@samcaldwell.net>
2024-05-25 12:27:04 -0500

    add constants for file package

---
+++ go/fs/file/constants.go
@@ -0,0 +1,22 @@
+package file
+
+import (
+	"io"
+)
+
+const (
+	/*
+	   This set of constants creates an abstraction from the continuous breaking changes
+	   of golang's evolution.  Not long ago os.SEEK_CUR, etc. moved to io.SeekStart, et seq.
+
+	   This guarantees that a future change will only require me to make one change here to
+	   fix all the things that use this package...because I am lazy.
+	*/
+
+	//SeekFromStart - Abstraction from golang constants (see notes, above)
+	SeekFromStart = io.SeekStart
+	//SeekFromCurrent - Abstraction from golang constants (see notes, above)
+	SeekFromCurrent = io.SeekCurrent
+	//SeekFromEnd - Abstraction from golang constants (see notes, above)
+	SeekFromEnd = io.SeekEnd
+)
