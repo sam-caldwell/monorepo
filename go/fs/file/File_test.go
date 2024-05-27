853e6f9ddcdb3cf47c58e5f2f47ab0d029e8b006
Sam Caldwell <github@samcaldwell.net>
2024-05-26 19:29:53 -0500

    fix up File test

--- go/fs/file/File_test.go
+++ go/fs/file/File_test.go
@@ -1,13 +1,32 @@
 package file
 
-import "testing"
+import (
+	"github.com/google/uuid"
+	"os"
+	"path/filepath"
+	"testing"
+)
 
-func TestFile(t *testing.T) {
-	const expectedFileName = "/tmp/myFile.txt"
+func TestFile_struct(t *testing.T) {
+	var expectedFileName = filepath.Join("/tmp/", uuid.New().String())
 	var f File
-	if f.handle != nil {
-		t.Fatal("handle expects to be nil")
-	}
-	f.lock.Lock()
-	f.lock.Unlock()
+	t.Run("pre-test", func(t *testing.T) {
+		if f.handle != nil {
+			t.Fatal("handle expects to be nil")
+		}
+	})
+	t.Run("setup test", func(t *testing.T) {
+		var err error
+		f.handle, err = os.OpenFile(expectedFileName, os.O_CREATE|os.O_RDWR, 0600)
+		if err != nil {
+			t.Fatal("error opening file", err)
+		}
+		if err = f.handle.Close(); err != nil {
+			t.Fatal("error closing file", err)
+		}
+	})
+	t.Run("test locks", func(t *testing.T) {
+		f.lock.Lock()
+		f.lock.Unlock()
+	})
 }
