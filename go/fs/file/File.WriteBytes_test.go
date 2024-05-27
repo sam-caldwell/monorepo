3696a15ac4bc57f696122f48505a8b1d36972fa0
Sam Caldwell <github@samcaldwell.net>
2024-05-26 22:11:41 -0500

    Fix File.WriteBytes()

---
+++ go/fs/file/File.WriteBytes_test.go
@@ -0,0 +1,85 @@
+package file
+
+import (
+	"github.com/google/uuid"
+	"github.com/sam-caldwell/monorepo/go/exit/errors"
+	"github.com/sam-caldwell/monorepo/go/misc/words"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestFile_WriteBytes(t *testing.T) {
+	t.Run("test uninitialized state", func(t *testing.T) {
+		var f File
+		n, err := f.WriteBytes([]byte("test"))
+		if err == nil {
+			t.Fatal("expected error when writing to uninitialized file (nil file handle)")
+		}
+		if err.Error() != errors.NotInitialized {
+			t.Fatal("expected NotInitialized error")
+		}
+		if n != 0 {
+			t.Errorf("WriteBytes failed: expected 0, got %v", n)
+		}
+	})
+
+	t.Run("test initialized state", func(t *testing.T) {
+		var (
+			f                File
+			name             string
+			expectedFileName = filepath.Join("/tmp/", uuid.New().String())
+		)
+
+		t.Cleanup(func() {
+			_ = f.handle.Close()
+			_ = os.Remove(name)
+		})
+
+		t.Run("test create test file", func(t *testing.T) {
+			var err error
+			if err = f.Open(expectedFileName, os.O_CREATE|os.O_RDWR, 0600); err != nil {
+				t.Fatal(err)
+			}
+			if f.handle == nil {
+				t.Fatal("handle is nil")
+			}
+			name = f.handle.Name()
+			if name == words.EmptyString {
+				t.Fatal("name is empty")
+			}
+		})
+
+		t.Run("test that the file exists", func(t *testing.T) {
+			if !Existsp(&name) {
+				t.Fatal("file does not exist as expected")
+			}
+		})
+
+		t.Run("write file bytes", func(t *testing.T) {
+			const testData = "test1"
+			n, err := f.WriteBytes([]byte(testData))
+			if err != nil {
+				t.Fatal(err)
+			}
+			if n != len(testData) {
+				t.Fatal("size mismatch")
+			}
+			pos, _ := f.handle.Seek(0, SeekFromCurrent)
+			if pos != int64(len(testData)) {
+				t.Fatal("position mismatch")
+			}
+			b := make([]byte, len(testData))
+			n, err = f.handle.ReadAt(b, 0)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if n != len(testData) {
+				t.Fatal("read size mismatch")
+			}
+			if string(b[:n]) != testData {
+				t.Fatal("read data mismatch")
+			}
+		})
+	})
+}
