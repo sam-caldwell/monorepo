e56f8d3b21d40ab47b48c7fd3eb65bde4774c50b
Sam Caldwell <github@samcaldwell.net>
2024-05-26 21:08:24 -0500

    add File.Open() test

---
+++ go/fs/file/File.Open_test.go
@@ -0,0 +1,44 @@
+package file
+
+import (
+	"github.com/google/uuid"
+	"github.com/sam-caldwell/monorepo/go/misc/words"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestFile_Open(t *testing.T) {
+	var (
+		f                File
+		name             string
+		expectedFileName = filepath.Join("/tmp/", uuid.New().String())
+	)
+	t.Cleanup(func() {
+		_ = f.handle.Close()
+		_ = os.Remove(name)
+	})
+	t.Run("test uninitialized file", func(t *testing.T) {
+		if f.handle != nil {
+			t.Fatal("handle is not nil")
+		}
+	})
+	t.Run("test create test file", func(t *testing.T) {
+		var err error
+		if err = f.Open(expectedFileName, os.O_CREATE|os.O_RDWR, 0600); err != nil {
+			t.Fatal(err)
+		}
+		if f.handle == nil {
+			t.Fatal("handle is nil")
+		}
+		name = f.handle.Name()
+		if name == words.EmptyString {
+			t.Fatal("name is empty")
+		}
+	})
+	t.Run("test that the file exists", func(t *testing.T) {
+		if !Existsp(&name) {
+			t.Fatal("file does not exist as expected")
+		}
+	})
+}
