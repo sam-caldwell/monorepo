3b132eb6a481143413a9194b45bf34dcd57e6636
Sam Caldwell <github@samcaldwell.net>
2024-05-27 01:33:28 -0500

    update examples

--- go/logger/example/example2-file.go
+++ go/logger/example/example2-file.go
@@ -11,10 +11,12 @@ func main() {
 	ansi.White().Println("Test starting...")
 	// Declare the logger and specify the output target (e.g. stdout, file, http,...)
 	var log logger.Logger[LogTarget.File]
+
 	// Configure the log level and other parameters
 	log.Configure(LogTarget.ConfigureTarget{
 		"filename":   "/tmp/test.log",
 		"permission": "0600",
+		"rateLimit":  "100",
 	}).
 		SetLevel(LogLevel.Debug).
 		Debug("debug message (expect blue text)").
@@ -22,29 +24,34 @@ func main() {
 		Warning("warning message (expect yellow text)").
 		Error("error message (expect red text)").
 		Critical("critical message (expect red text)")
+
 	log.SetLevel(LogLevel.Info).
 		Debug("debug message (expect blue text)").
 		Info("info message (expect blue text)").
 		Warning("warning message (expect yellow text)").
 		Error("error message (expect red text)").
 		Critical("critical message (expect red text)")
+
 	log.SetLevel(LogLevel.Warning).
 		Debug("debug message (expect blue text)").
 		Info("info message (expect blue text)").
 		Warning("warning message (expect yellow text)").
 		Error("error message (expect red text)").
 		Critical("critical message (expect red text)")
+
 	log.SetLevel(LogLevel.Error).
 		Debug("debug message (expect blue text)").
 		Info("info message (expect blue text)").
 		Warning("warning message (expect yellow text)").
 		Error("error message (expect red text)").
 		Critical("critical message (expect red text)")
+
 	log.SetLevel(LogLevel.Critical).
 		Debug("debug message (expect blue text)").
 		Info("info message (expect blue text)").
 		Warning("warning message (expect yellow text)").
 		Error("error message (expect red text)").
 		Critical("critical message (expect red text)")
+
 	ansi.White().Println("test finishing").Reset()
 }
