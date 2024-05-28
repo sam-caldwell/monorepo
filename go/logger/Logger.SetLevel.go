0779312546964f3d76c17e5979456bb06154f919
Sam Caldwell <github@samcaldwell.net>
2024-05-27 20:51:48 -0500

    refactor Logger.SetLevel()

--- go/logger/Logger.SetLevel.go
+++ go/logger/Logger.SetLevel.go
@@ -1,11 +1,19 @@
 package logger
 
-import "github.com/sam-caldwell/monorepo/go/logger/LogLevel"
+import (
+	"github.com/sam-caldwell/monorepo/go/exit/errors"
+	"github.com/sam-caldwell/monorepo/go/logger/LogLevel"
+)
 
 // SetLevel - Define the log level
 //
 //	(c) 2023 Sam Caldwell.  MIT License
-func (log *Logger[T]) SetLevel(level LogLevel.Value) *Logger[T] {
-	log.target.SetLevel(level)
+func (log *Logger) SetLevel(level LogLevel.Value) *Logger {
+	switch level {
+	case LogLevel.Debug, LogLevel.Info, LogLevel.Warning, LogLevel.Error, LogLevel.Critical, LogLevel.Fatal:
+		log.level = level
+	default:
+		panic(errors.InvalidLogLevel)
+	}
 	return log
 }
