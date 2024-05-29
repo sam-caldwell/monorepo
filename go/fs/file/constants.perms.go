046f0ef2d89f1bb110eefcfbba7210e7514ab296
Sam Caldwell <github@samcaldwell.net>
2024-05-29 15:51:33 -0500

    cleanup

--- go/fs/file/constants.perms.go
+++ go/fs/file/constants.perms.go
@@ -1,9 +1,5 @@
 package file
 
-import (
-	"os"
-)
-
 const (
 	/*
 	   Permission flags.  Use bitwise OR to combine them.
@@ -52,32 +48,4 @@ const (
 
 	//EveryoneExecute - Everyone can execute
 	EveryoneExecute = PermExecute << AppliesToEveryone
-
-	/*
-	   File Flags
-	*/
-
-	//FlagAppend - can be appended
-	FlagAppend = os.O_APPEND
-
-	//FlagCreate - can be created if not exists
-	FlagCreate = os.O_CREATE
-
-	//FlagExclusive - opened exclusively
-	FlagExclusive = os.O_EXCL
-
-	//FlagReadOnly - open for readonly access
-	FlagReadOnly = os.O_RDONLY
-
-	//FlagReadWrite - open file for read/write random access
-	FlagReadWrite = os.O_RDWR
-
-	//FlagSync - open file for synchronous access
-	FlagSync = os.O_SYNC
-
-	//FlagTruncate - open file and truncate the content
-	FlagTruncate = os.O_TRUNC
-
-	//FlagWriteOnly - open file for write-only access
-	FlagWriteOnly = os.O_WRONLY
 )
