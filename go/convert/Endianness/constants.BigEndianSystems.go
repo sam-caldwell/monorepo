357f554d5c536354dc98cadb22fd9f85d3ec3c58
Sam Caldwell <github@samcaldwell.net>
2024-05-28 21:40:23 -0500

    document Endianness package

--- go/convert/Endianness/constants.BigEndianSystems.go
+++ go/convert/Endianness/constants.BigEndianSystems.go
@@ -3,18 +3,18 @@
 
 package endianness
 
-/*
- * projects/convert/Endianness/constants.BigEndianness.go
- * (c) 2023 Sam Caldwell.  See LICENSE.txt
- *
- * This file provides two simple constants to help us determine system endianness at
- * runtime without being unsafe or engaging in the pedantic dumbfuckery of a conversation
- * with the goLang-nuts forum of opinionated holier-than-though jack-wagons.  If this
- * offends you, consider the fact that so many of us have to create workarounds because
- * you live in perfect, pretty worlds while the rest of us write code in reality where
- * nasty stuff still exists.
- */
 const (
-	BigEndian    = true
+	//BigEndian: most significant byte is stored first (at the smallest address).
+	//Memory Address:  0   1   2   3
+	//Value (32-bit):  MSB  ..  ..  LSB
+	//
+	//	(c) 2023 Sam Caldwell.  MIT License
+	BigEndian = true
+
+	//LittleEndian: least significant byte is stored first (at the smallest address).
+	//Memory Address:  0   1   2   3
+	//Value (32-bit):  LSB  ..  ..  MSB
+	//
+	//	(c) 2023 Sam Caldwell.  MIT License
 	LittleEndian = false
 )
