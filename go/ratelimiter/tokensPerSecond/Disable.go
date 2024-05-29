5677c09845f04fd5821ea9354d8a56ee6e250fbe
Sam Caldwell <github@samcaldwell.net>
2024-05-28 21:40:58 -0500

    documentation and code cleanup

--- go/ratelimiter/tokensPerSecond/Disable.go
+++ go/ratelimiter/tokensPerSecond/Disable.go
@@ -2,10 +2,10 @@ package tokensPerSecond
 
 // Disable - disable the rate limiter
 //
-//	 This will result in all activity being allowed because no rate limiting
-//	 will be enforced.
+//	This will result in all activity being allowed because no rate limiting
+//	will be enforced.
 //
-//		(c) 2023 Sam Caldwell.  MIT License
+//	(c) 2023 Sam Caldwell.  MIT License
 func (r *RateLimiter) Disable() {
 	r.active = false
 }
--- go/ratelimiter/tokensPerSecond/NonBlockingCheck.go
+++ go/ratelimiter/tokensPerSecond/NonBlockingCheck.go
@@ -4,14 +4,14 @@ import "time"
 
 // NonBlockingCheck - If active, return boolean true if rate limit should allow an operation.
 //
-//			 If the rate limiter is not active, we will always return true to allow operations.
-//			 If the rate limit is exhausted, we will return false, indicating that an operation
-//			 should not be allowed.
+//	    If the rate limiter is not active, we will always return true to allow operations.
+//		If the rate limit is exhausted, we will return false, indicating that an operation
+//		should not be allowed.
 //
-//	         tokensConsumed - represents the number of tokens the operation will consume from the
-//	         ratelimit (number of tokens allowed per period).
+//		tokensConsumed - represents the number of tokens the operation will consume from the
+//		ratelimit (number of tokens allowed per period).
 //
-//			 (c) 2023 Sam Caldwell.  MIT License
+//		(c) 2023 Sam Caldwell.  MIT License
 func (r *RateLimiter) NonBlockingCheck(tokensConsumed int) bool {
 	if !r.active {
 		return true //Always allow because rate limiter is disabled.
