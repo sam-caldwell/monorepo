9275a4d29e79fecc273a362245dd107c24e5feb4
Sam Caldwell <github@samcaldwell.net>
2024-05-29 10:52:30 -0500

    add placeholders for persistentSecrets Set() and Delete() methods

---
+++ go/secrets/persistentSecrets/Delete.go
@@ -0,0 +1,8 @@
+package persistentSecrets
+
+import "github.com/sam-caldwell/monorepo/go/exit/errors"
+
+func (vault *Vault) Delete(name string) error {
+	panic(errors.NotImplemented)
+	return nil
+}
---
+++ go/secrets/persistentSecrets/Set.go
@@ -0,0 +1,8 @@
+package persistentSecrets
+
+import "github.com/sam-caldwell/monorepo/go/exit/errors"
+
+func (vault *Vault) Set(name string, value []byte) error {
+	panic(errors.NotImplemented)
+	return nil
+}
