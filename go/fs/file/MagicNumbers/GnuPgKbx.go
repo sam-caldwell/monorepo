8ecbed5943969ba0fd388cc003c83bb4b909b508
Sam Caldwell <github@samcaldwell.net>
2024-05-29 23:05:22 -0500

    fix kbx magic

--- go/fs/file/MagicNumbers/GnuPgKbx.go
+++ go/fs/file/MagicNumbers/GnuPgKbx.go
@@ -5,4 +5,4 @@ package MagicNumbers
 // See https://github.com/gpg/gnupg/blob/master/kbx/keybox-blob.c
 //
 //	(c) 2023 Sam Caldwell.  MIT License
-const GnuPgKbx = "\x00\x00\x00\x20\x01\x01\x00\x02\x4b\x42\x58\x66"
+const GnuPgKbx = "\x00\x00\x00\x20"
