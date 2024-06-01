2c30fcb2d84c252f2041d9471a907cddb9925c94
Sam Caldwell <github@samcaldwell.net>
2024-06-01 13:04:59 -0500

    fixes and tests for generateHuffmanCodes()

--- go/compress/huffman/generateHuffmanCodes.go
+++ go/compress/huffman/generateHuffmanCodes.go
@@ -5,6 +5,9 @@ package huffman
 //	(c) 2023 Sam Caldwell.  MIT License
 func generateHuffmanCodes(n *node, code string, codes map[byte]string) {
 	if n.Left == nil && n.Right == nil {
+		if code == "" {
+			code = "0" // Assign code "0" if it's the only node in the tree
+		}
 		codes[n.Character] = code
 		return
 	}
