09567cfdbe36d293d22b9c6b3bb10c07886bf4b3
Sam Caldwell <github@samcaldwell.net>
2024-06-02 21:09:47 -0500

    Add NewFrequency() and test

--- go/compress/huffman/NewFrequencyTable.go
+++ go/compress/huffman/NewFrequencyTable.go
@@ -1,6 +1,8 @@
 package huffman
 
 // NewFrequencyTable - Initialize the frequency table.
+//
+//	(c) 2023 Sam Caldwell.  MIT License
 func NewFrequencyTable() FrequencyTable {
 	return FrequencyTable{}
 }
