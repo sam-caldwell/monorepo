3c90398c8e50de48142520f50537ef79f50204dd
Sam Caldwell <github@samcaldwell.net>
2024-06-04 21:27:26 -0500

    fix PrettyPrint() to handle nil trees

--- go/compress/huffman/Node.PrettyPrint.go
+++ go/compress/huffman/Node.PrettyPrint.go
@@ -3,14 +3,26 @@ package huffman
 import (
 	"fmt"
 	"github.com/sam-caldwell/monorepo/go/ansi"
+	"github.com/sam-caldwell/monorepo/go/misc/words"
 )
 
 // PrettyPrint method to print the Huffman tree using ASCII art
 //
 //	(c) 2023 Sam Caldwell.  MIT License
 func (node *Node) PrettyPrint() {
-	ansi.Blue().Println("(root)").Reset()
+	ansi.Blue().
+		Line(words.Hyphen, 40).
+		Println("tree...").
+		Flush().Reset()
+	if node == nil {
+		ansi.Blue().Println("<nil>").Flush().Reset()
+		return
+	}
+	ansi.Blue().Println("(root)").Flush().Reset()
 	printTree(node, "  ", true)
+	ansi.Blue().
+		Line(words.Hyphen, 40).
+		Flush().Reset()
 }
 
 // printTree is a helper function to recursively print the tree
@@ -28,9 +40,9 @@ func printTree(node *Node, prefix string, isTail bool) {
 	}
 
 	if isTail {
-		ansi.Blue().Println(prefix + "└── " + nodeString(node)).Reset()
+		ansi.Blue().Println(prefix + "└── " + nodeString(node)).Flush().Reset()
 	} else {
-		ansi.Blue().Println(prefix + "┌── " + nodeString(node)).Reset()
+		ansi.Blue().Println(prefix + "┌── " + nodeString(node)).Flush().Reset()
 	}
 
 	if node.left != nil {
