8beaa0ba2c88212408ebc5a0658f6beed2a280ef
Sam Caldwell <github@samcaldwell.net>
2024-06-06 20:29:46 -0500

    extend test for findUniqueEof()

--- go/compress/bzip2/bzip2bwt/findUniqueEof_test.go
+++ go/compress/bzip2/bzip2bwt/findUniqueEof_test.go
@@ -9,11 +9,12 @@ func TestFindUniqueEOF(t *testing.T) {
 		input    []byte
 		expected byte
 	}{
-		{[]byte("BANANA"), byte(0x04)},     // 0x04 should not be in "BANANA"
-		{[]byte("PANAMA"), byte(0x04)},     // 0x04 should not be in "PANAMA"
-		{[]byte("APPLE"), byte(0x04)},      // 0x04 should not be in "APPLE"
-		{[]byte("BOOKKEEPER"), byte(0x04)}, // 0x04 should not be in "BOOKKEEPER"
-		{[]byte("HE$LO"), byte(0x04)},      // 0x04 should not be in "HE$LO"
+		{[]byte("BANANA"), byte(0x04)},       // 0x04 should not be in "BANANA" (return default)
+		{[]byte("PANAMA"), byte(0x04)},       // 0x04 should not be in "PANAMA" (return default)
+		{[]byte("APPLE"), byte(0x04)},        // 0x04 should not be in "APPLE" (return default)
+		{[]byte("BOOKKEEPER"), byte(0x04)},   // 0x04 should not be in "BOOKKEEPER" (return default)
+		{[]byte("HE$LO"), byte(0x04)},        // 0x04 should not be in "HE$LO" (return default)
+		{[]byte{65, 66, 04, 67}, byte(0x05)}, // find 0x04 and expect 0x05
 	}
 
 	for _, test := range tests {
