cb5b909d702795e3dddda9c3d2e04b96e00b99cc
Sam Caldwell <github@samcaldwell.net>
2024-06-16 19:00:58 -0500

    update ansi.Flush() to use exit.SafeDefer()

--- go/ansi/Flush.go
+++ go/ansi/Flush.go
@@ -1,20 +1,20 @@
 package ansi
 
 import (
-    "os"
+	"os"
 )
 
 // Flush - Flush StdOut buffer
 //
 //	(c) 2023 Sam Caldwell.  MIT License
 func (c *Color) Flush() *Color {
-    defer func() { _ = os.Stdout.Sync() }()
-    return c
+	defer func() { _ = os.Stdout.Sync() }()
+	return c
 }
 
 // Flush - Flush StdOut buffer
 //
 //	(c) 2023 Sam Caldwell.  MIT License
 func Flush() *Color {
-    return (&Color{}).Flush()
+	return (&Color{}).Flush()
 }
