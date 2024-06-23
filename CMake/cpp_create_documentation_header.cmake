98a5ac2895192916ab419c6009a902a9d4058c80
Sam Caldwell <github@samcaldwell.net>
2024-06-23 13:07:09 -0500

    fix formatting issue with dynamic documentation in c++

--- CMake/cpp_create_documentation_header.cmake
+++ CMake/cpp_create_documentation_header.cmake
@@ -5,13 +5,13 @@ function(cpp_create_documentation_header readmeFile)
     set(DOC_LINES
             "C++ Project Document Root"
             "========================="
-            "(c) Sam Caldwell.  See LICENSE.txt."
+            "(c) Sam Caldwell.  See LICENSE.txt.\n"
             ""
-            "## Objectives"
+            "## Objectives\n"
             "  This README file provides a root for all C++ project documentation"
             "  each project should be referenced (and linked if appropriate)"
-            ""
-            "## Projects")
+            "\n"
+            "## Projects\n")
 
     file(WRITE ${readmeFile})
     foreach (LINE ${DOC_LINES})
@@ -24,13 +24,13 @@ function(cpp_create_documentation_line readmeFile project_path project_name)
     if (EXISTS cpp/${project_path}/README.md)
         file(APPEND ${readmeFile} "* [${project_name}](${project_path}/README.md)\n")
     else()
-        logWARN("project (${project_name}) has no README.md (${readmeFile}) file")
-        file(APPEND ${readmeFile} "* ${project_name}\n")
+        logWARN("project (${project_name}) has no README.md (${readmeFile}) file\n")
+        file(APPEND ${readmeFile} "* ${project_name}")
     endif ()
 endfunction()
 
 function(cpp_create_documentation_footer readmeFile)
-    file(APPEND ${readmeFile} "\n\n")
+    file(APPEND ${readmeFile} "\n")
     string(TIMESTAMP TIMESTAMP "%Y-%m-%d %H:%M:%S")
     file(APPEND ${readmeFile} "\nGenerated at ${TIMESTAMP}\n")
 endfunction()
--- cpp/README.md
+++ cpp/README.md
@@ -1,14 +1,18 @@
 C++ Project Document Root
 =========================
 (c) Sam Caldwell.  See LICENSE.txt.
+
 ## Objectives
+
   This README file provides a root for all C++ project documentation
   each project should be referenced (and linked if appropriate)
+
+
 ## Projects
+
 * [AnsiColors](common/AnsiColors/README.md)
 * [Channel](common/Channel/README.md)
 * [helloworld](tools/helloworld/README.md)
 
 
-
-Generated at 2024-06-23 13:00:11
+Generated at 2024-06-23 13:06:20
